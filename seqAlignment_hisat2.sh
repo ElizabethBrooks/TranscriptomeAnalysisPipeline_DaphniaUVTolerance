3693d13c978bbd0caadbd82847c0e7f2293959b5
Mae <merimaat1@gmail.com>
2019-05-30 19:27:30 -0400

    Removed parallel processing

--- seqAlignment_hisat2.sh
+++ seqAlignment_hisat2.sh
@@ -10,15 +10,11 @@ mkdir aligned
 
 hisat2-build -f /afs/crc.nd.edu/group/hoth/echo_base/genome/Daphnia_pulex.allmasked.fa Daphnia_pulex.allmasked
 
-#Use up to 24 (256 GB) separate processes
-N=24
 #Loop through all forward and reverse paired reads and run hisat2 on each pair
-(
 for f1 in *pairedForward.fq.gz; do
 	for f2 in *pairedReverse.fq.gz; do
 		if "${f1:0:${#f1}-19}" == "${f2:0:${#f2}-19}"; then
-			hisat2 -q -x Daphnia_pulex.allmasked -1 $f1 -2 $f2 -S aligned --aligned/"${f1:0:${#f1}-19}"/summary_file.txt &
+			hisat2 -q -x Daphnia_pulex.allmasked -1 $f1 -2 $f2 -S aligned --aligned/"${f1:0:${#f1}-19}"/summary_file.txt
 		fi
 	done
 done
-)
