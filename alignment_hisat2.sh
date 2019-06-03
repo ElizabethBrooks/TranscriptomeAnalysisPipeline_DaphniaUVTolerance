bcc04d02c3a7fae1b29dfa41c993dc7ee230e094
Mae <merimaat1@gmail.com>
2019-06-03 15:56:47 -0400

    Fixed counters and folder references

--- alignment_hisat2.sh
+++ alignment_hisat2.sh
@@ -11,6 +11,6 @@ mkdir aligned
 hisat2-build -f /afs/crc.nd.edu/group/hoth/echo_base/genome/Daphnia_pulex.allmasked.fa Daphnia_pulex.allmasked
 
 #Loop through all forward and reverse paired reads and run hisat2 on each pair
-for f1 in *pairedForward.fq.gz; do
-	hisat2 -q -x Daphnia_pulex.allmasked -1 $f1 -2 "${f1:0:${#f1}-19}"pairedReverse.fq.gz -S aligned --aligned/"${f1:0:${#f1}-19}"/summary_file.txt	
+for f1 in trimmed/*pairedForward.fq.gz; do
+	hisat2 -q -x Daphnia_pulex.allmasked -1 $f1 -2 trimmed/"${f1:0:${#f1}-19}"pairedReverse.fq.gz -S aligned --aligned/"${f1:0:${#f1}-19}"/summary_file.txt	
 done
