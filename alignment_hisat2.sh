9cb1e6fa455acbc5d21525297752c0d380e0a7a9
Mae <merimaat1@gmail.com>
2019-06-07 05:38:03 -0400

    Fixed file arrays

--- alignment_hisat2.sh
+++ alignment_hisat2.sh
@@ -12,14 +12,9 @@ mkdir aligned
 module load bio/hisat2/2.1.0
 hisat2-build -f /afs/crc.nd.edu/group/hoth/echo_base/genome/Daphnia_pulex.allmasked.fa aligned/Daphnia_pulex.allmasked
 
-#Initialize counter
-COUNTER=0
-#Loop through all forward and reverse paired reads and store the file locations in arrays
-for f1 in trimmed/*pairedForward.fq.gz; do
-        FORWARDARRAY[i]="$f1, "
-        REVERSEARRAY[i]="${f1:0:${#f1}-19}pairedReverse.fq.gz, "
-        let COUNTER+=1
-done
+#Store forward and reverse reads in arrays
+FORWARDARRAY=(trimmed/*pairedForward.fq.gz)
+REVERSEARRAY=(trimmed/*pairedReverse.fq.gz)
 
-#Run hisat2 on forward and reverse reads stored in arrays
-hisat2 -p 8 -q -x aligned/Daphnia_pulex.allmasked -1 ${FORWARDARRAY[*]} -2 ${REVERSEARRAY[*]} -S aligned --aligned/summary_file.txt
+#Run hisat2 on reads stored in arrays
+hisat2 -q -p 8 -x aligned/Daphnia_pulex.allmasked -1 ${FORWARDARRAY[*]} -2 ${REVERSEARRAY[*]} -S aligned --summary-file aligned/alignedSummary.txt
