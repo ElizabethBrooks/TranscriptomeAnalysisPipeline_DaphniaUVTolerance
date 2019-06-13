5d3c6367d09e67f1c7b47fc39cbd0d3c8fcedca7
Elizabeth Brooks <ebrooks5@crcfe01.crc.nd.edu>
2019-06-13 16:02:18 -0400

    Fixed equality check

--- alignment_hisat2.sh
+++ alignment_hisat2.sh
@@ -5,15 +5,16 @@
 #$ -N alignment_hisat2
 #$ -pe smp 8
 
+#Prepare for mapping
 cd ..
 mkdir aligned_hisat2
-
-#Prepare for mapping
 module load bio/hisat2/2.1.0
+
+#Build reference genome
 hisat2-build -f /afs/crc.nd.edu/group/hoth/echo_base/genome/Daphnia_pulex.allmasked.fa aligned_hisat2/Daphnia_pulex.allmasked
 
 #Loop through all forward and reverse paired reads and run hisat2 on each pair
 # using 8 threads
 for f1 in trimmed/*pForward.fq.gz; do
-	hisat2 -q -p 8 -x aligned_hisat2/Daphnia_pulex.allmasked -1 $f1 -2 "${f1:0:${#f1}-14}"pReverse.fq.gz -S aligned_hisat2/out/"${f1:0:${#f1}-14}" --summary-file alignedSummary.txt
+	hisat2 -q -p 8 -x aligned_hisat2/Daphnia_pulex.allmasked -1 $f1 -2 "${f1:0:${#f1}-14}"pReverse.fq.gz -S aligned_hisat2/out/"${f1:0:${#f1}-14}" --summary-file aligned_hisat2/alignedSummary.txt
 done
--- trimming_trimmomaticFastqc.sh
+++ trimming_trimmomaticFastqc.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 #$ -M ebrooks5@nd.edu
 #$ -m abe
-#$ -N adapterTrimming_trimmomaticFastqc
+#$ -N trimming_trimmomaticFastqc
 #$ -pe smp 1
 
 #Prepare for adapter trimming and quality control
 #Initialize variables
 qcCountStart=0
 score=0
+
 #Load necessary modules for ND CRC servers
 module load bio
 module load bio/trimmomatic/0.32
+
 #Move to folder with .fq.gz read files
 cd ..	
 mkdir trimmed
+
 #Loop through all forward and reverse reads and run trimmomatic on each pair
 for f1 in *1.fq.gz; do
 	#Quality control using fastqc on the first read file
-	if [ qcCountStart = 0 ]; then
+	if [ "$qcCountStart" -eq 0 ]; then
 		fastqc $f1 --extract
 		#Determine phred score for trimming
 		if grep -iF "Encoding   Illumina 1.5" "${f1:0:${#f1}-7}"1_fastqc/fastqc_data.txt; then
+			echo "${f1:0:${#f1}-7} phred score is $score"
 			score=64
 		elif grep -iF "Encoding Illumina 1.9" "${f1:0:${#f1}-7}"1_fastqc/fastqc_data.txt; then
+			echo "${f1:0:${#f1}-7} phred score is $score"
 			score=33
 		else
 			echo "Illumina encoding not found"
