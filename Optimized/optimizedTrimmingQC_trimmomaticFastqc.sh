590ce2c76cfb6b8b3b520e717e5339cb97e70561
Elizabeth Brooks <ebrooks5@crcfe01.crc.nd.edu>
2019-06-13 22:19:54 -0400

    Updated location of saved QC report

--- Optimized/optimizedTrimmingQC_trimmomaticFastqc.sh
+++ Optimized/optimizedTrimmingQC_trimmomaticFastqc.sh
@@ -18,7 +18,8 @@ module load bio/trimmomatic/0.32
 
 #Move to folder with .fq.gz read files
 cd ..	
-mkdir trimmed
+cd ..
+mkdir optimizedTrimmed
 
 #Loop through all forward and reverse reads and run trimmomatic on each pair
 for f1 in *1.fq.gz; do
@@ -38,26 +39,26 @@ for f1 in *1.fq.gz; do
 		#QC the first read file
 		#...in progress...
 		if grep -iF "WARN" "${f1:0:${#f1}-7}"1_fastqc/summary.txt; then
-			grep -iF "WARN" "${f1:0:${#f1}-7}"1_fastqc/summary.txt > "${f1:0:${#f1}-7}"1_fastqc_report.txt
+			grep -iF "WARN" "${f1:0:${#f1}-7}"1_fastqc/summary.txt > optimizedTrimmed/"${f1:0:${#f1}-7}"1_fastqc_report.txt
 		fi
 		if grep -iF "FAIL" "${f1:0:${#f1}-7}"1_fastqc/summary.txt; then
-			grep -iF "FAIL" "${f1:0:${#f1}-7}"1_fastqc/summary.txt > "${f1:0:${#f1}-7}"1_fastqc_report.txt
+			grep -iF "FAIL" "${f1:0:${#f1}-7}"1_fastqc/summary.txt > optimizedTrimmed/"${f1:0:${#f1}-7}"1_fastqc_report.txt
 		fi
 		#Only QC one file
 		qcCountStart=1
 	fi
 	#Perform adapter trimming on paired reads
-	trimmomatic PE -phred"$score" $f1 "${f1:0:${#f1}-7}"2.fq.gz trimmed/"${f1:0:${#f1}-7}"pForward.fq.gz trimmed/"${f1:0:${#f1}-7}"uForward.fq.gz trimmed/"${f1:0:${#f1}-7}"pReverse.fq.gz trimmed/"${f1:0:${#f1}-7}"uReverse.fq.gz ILLUMINACLIP:/afs/crc.nd.edu/x86_64_linux/bio/Trimmomatic/0.32/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:13
+	trimmomatic PE -phred"$score" $f1 "${f1:0:${#f1}-7}"2.fq.gz optimizedTrimmed/"${f1:0:${#f1}-7}"pForward.fq.gz optimizedTrimmed/"${f1:0:${#f1}-7}"uForward.fq.gz optimizedTrimmed/"${f1:0:${#f1}-7}"pReverse.fq.gz optimizedTrimmed/"${f1:0:${#f1}-7}"uReverse.fq.gz ILLUMINACLIP:/afs/crc.nd.edu/x86_64_linux/bio/Trimmomatic/0.32/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:13
 	#Final quality control check using fastqc on the first trimmed paired read file
 	if [ qcCountEnd = 0 ]; then
 		#QC paired forward read
 		#...in progress...
-		fastqc trimmed/"${f1:0:${#f1}-7}"pForward.fq.gz --extract
-		if grep -iF "WARN" trimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt; then
-			grep -iF "WARN" trimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt > "${f1:0:${#f1}-7}"1_fastqc_report.txt
+		fastqc optimizedTrimmed/"${f1:0:${#f1}-7}"pForward.fq.gz --extract
+		if grep -iF "WARN" optimizedTrimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt; then
+			grep -iF "WARN" optimizedTrimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt > optimizedTrimmed/"${f1:0:${#f1}-7}"1_fastqc_report.txt
 		fi
 		if grep -iF "FAIL" trimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt; then
-			grep -iF "FAIL" trimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt > "${f1:0:${#f1}-7}"1_fastqc_report.txt
+			grep -iF "FAIL" optimizedTrimmed/"${f1:0:${#f1}-7}"pForward_fastqc/summary.txt > optimizedTrimmed/"${f1:0:${#f1}-7}"1_fastqc_report.txt
 		fi
 		#Only QC one file
 		qcCountEnd=1
