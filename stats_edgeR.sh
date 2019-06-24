73a33cb9ac593a40c792da1b2f8ef4524399e6a2
Mae <merimaat1@gmail.com>
2019-06-24 16:46:33 -0400

    Updated edgeR stats script

--- stats_edgeR.sh
+++ stats_edgeR.sh
@@ -14,9 +14,9 @@ cd ..
 #Loop through all forward and reverse paired reads and store the file locations in arrays
 for f1 in aligned_tophat2/out/*; do
 	echo "Sample ${f1:20:${#f1}-0} is being analyzed..."
-    #Run samtools to prepare mapped reads for counting
+	#Run samtools to prepare mapped reads for counting
 	# using 8 threads
 	#samtools sort -o stats_edgeR/sorted/${f1:20:${#f1}-0}/accepted_hits.sorted.bam -T /tmp/${f1:20:${#f1}-0}/accepted_hits.sorted.bam -@ 8 $f1/accepted_hits.bam
 	#Run htseq-count to prepare sorted reads for stats analysis in edgeR
-	#htseq-count -s no -m union -t gene -i trID $f1/accepted_hits.sorted.bam -i genomeFilePath > sample.counts
+	#htseq-count -s no -m union -t gene -i trID $f1/accepted_hits.sorted.bam -i /afs/crc.nd.edu/group/hoth/echo_base/genome/dpulex-genepredict-v11.gff > stats_edgeR/${f1:20:${#f1}-0}.counts
 done
