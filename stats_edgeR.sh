fd333f8d7226c4619fea27870b3550c604676b18
Mae <merimaat1@gmail.com>
2019-06-24 16:37:30 -0400

    Testing edgeR stats script

--- stats_edgeR.sh
+++ stats_edgeR.sh
@@ -7,19 +7,16 @@
 
 #Prepare for alignment
 cd ..
-mkdir stats_edgeR
-module load bio/python/2.7.14
-module load bio/htseq/0.11.2
-COUNTER=0
+#mkdir stats_edgeR
+#mkdir stats_edgeR/sorted
+#module load bio/python/2.7.14
+#module load bio/htseq/0.11.2
 #Loop through all forward and reverse paired reads and store the file locations in arrays
 for f1 in aligned_tophat2/out/*; do
-        READARRAY[i]="$f1/accepted_hits.bam, "
-        let COUNTER+=1
+	echo "Sample ${f1:20:${#f1}-0} is being analyzed..."
+    #Run samtools to prepare mapped reads for counting
+	# using 8 threads
+	#samtools sort -o stats_edgeR/sorted/${f1:20:${#f1}-0}/accepted_hits.sorted.bam -T /tmp/${f1:20:${#f1}-0}/accepted_hits.sorted.bam -@ 8 $f1/accepted_hits.bam
+	#Run htseq-count to prepare sorted reads for stats analysis in edgeR
+	#htseq-count -s no -m union -t gene -i trID $f1/accepted_hits.sorted.bam -i genomeFilePath > sample.counts
 done
-#Remove the last two characters (extra comma and white space) from the last element of the read file array
-READARRAY[COUNTER]="$f1/accepted_hits.bam"
-echo ${READARRAY}
-#Use samtools to prepare mapped reads for counting
-#samtools sort -T /tmp/sample/accepted_hits_sorted.bam -o aligned/sample/accepted_hits_sorted.bam aligned/sample/accepted_hits.bam
-#Use htseq-count to prepare sorted reads for stats analysis in edgeR
-#htseq-count -s no -m union -t gene -i trID aligned/sample/accepted_hits_sorted.bam -i genomeFilePath > sample.counts
