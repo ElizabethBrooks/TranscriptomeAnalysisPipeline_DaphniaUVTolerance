a38aaccf071c60ed6ce7d6f1709d704e79f61f8c
Elizabeth Brooks <ebrooks5@crcfe01.crc.nd.edu>
2019-06-24 14:42:33 -0400

    Updated to latest alignment scripts

--- alignment_tophat2.sh
+++ alignment_tophat2.sh
@@ -3,7 +3,7 @@
 #$ -m abe
 #$ -r n
 #$ -N alignment_tophat2_jobOutput
-#$ -pe smp 1
+#$ -pe smp 8
 
 #Prepare for alignment
 cd ..
@@ -13,6 +13,9 @@ module load bio
 #Build reference genome
 bowtie2-build /afs/crc.nd.edu/group/hoth/echo_base/genome/Daphnia_pulex.allmasked.fa aligned_tophat2/Daphnia_pulex.allmasked
 #Loop through all forward and reverse paired reads and run tophat2 on each pair
+# using 8 threads
 for f1 in trimmed/*pForward.fq.gz; do
-	tophat2 -G /afs/crc.nd.edu/group/hoth/echo_base/genome/dpulex-genepredict-v11.gff -o aligned_tophat2/out/"${f1:8:${#f1}-23}" aligned_tophat2/Daphnia_pulex.allmasked $f1 "${f1:0:${#f1}-14}"pReverse.fq.gz
+	echo "Sample ${f1:8:${#f1}-23} is being aligned..."
+	tophat2 -p 8 -G /afs/crc.nd.edu/group/hoth/echo_base/genome/dpulex-genepredict-v11.gff -o aligned_tophat2/out/"${f1:8:${#f1}-23}" aligned_tophat2/Daphnia_pulex.allmasked $f1 "${f1:0:${#f1}-14}"pReverse.fq.gz
+	echo "Sample ${f1:8:${#f1}-23} has been aligned!"
 done
