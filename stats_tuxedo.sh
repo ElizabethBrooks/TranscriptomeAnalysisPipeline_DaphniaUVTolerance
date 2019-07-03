17fd14b7868b9b7617df8c7af28238a4ab7826b2
Mae <merimaat1@gmail.com>
2019-07-03 15:23:48 -0400

    Added multiple input folders for stats anaylsis

--- stats_tuxedo.sh
+++ stats_tuxedo.sh
@@ -5,12 +5,13 @@
 #$ -N stats_tuxedo_jobOutput
 #$ -pe smp 8
 #Required modules for ND CRC servers
-module load bio/cufflinks/2.2.1
+#module load bio/cufflinks/2.2.1
 #Prepare for analysis
 cd ..
 dirFlag=0
 runNum=0
 COUNTER=0
+genomeFile=TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/PA42.3.0.annotation.18440.gff
 #Make a new directory for each analysis run
 while [ $dirFlag -eq 0 ]; do
 	mkdir stats_tuxedo_run"$runNum"
@@ -27,22 +28,29 @@ done
 #Retrieve folders to analyze from the input arguments
 for f1 in "$@"; do
 	if [[ $f1 == *"hisat2"* ]]; then
-
+		#Loop through all forward and reverse paired reads and store the file locations in an array
+		for f2 in "$f1"/out/*; do
+	    	READARRAY[COUNTER]="$f2.bam, "
+			let COUNTER+=1
+		done
+		#Re set the last array element to rmove the last two characters
+		# (extra comma and white space) from the last element of the read file array
+		unset 'READARRAY[${#READARRAY[@]}-1]'
+		READARRAY[COUNTER]="$f2.bam"
 	elif [[ $f1 == *"tophat2"* ]]; then	
 		#Loop through all forward and reverse paired reads and store the file locations in an array
-		for f2 in aligned_tophat2/out/*; do
-		        READARRAY[COUNTER]="$f2/*.bam, "
-		        let COUNTER+=1
+		for f2 in "f1"/out/*; do
+	    	READARRAY[COUNTER]="$f2/*.bam, "
+			let COUNTER+=1
 		done
+		#Re set the last array element to rmove the last two characters
+		# (extra comma and white space) from the last element of the read file array
+		unset 'READARRAY[${#READARRAY[@]}-1]'
+		READARRAY[COUNTER]="$f2/accepted_hits.bam"
 	else
 		echo "The $f1 folder or bam files were not found... exiting"
 		exit 1
 	done
-	#Re set the last array element to rmove the last two characters
-	# (extra comma and white space) from the last element of the read file array
-	unset 'READARRAY[${#READARRAY[@]}-1]'
-	READARRAY[COUNTER]="$f2/accepted_hits.bam"
-	genomeFile=TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/PA42.3.0.annotation.18440.gff
 	#Run cuffdiff on the aligned reads stored in the file array using 8 threads
-	cuffdiff -p 8 -o stats_tuxedo_run"$runNum" "$genomeFile" "${READARRAY[@]}"
+	#cuffdiff -p 8 -o stats_tuxedo_run"$runNum" "$genomeFile" "${READARRAY[@]}"
 done
