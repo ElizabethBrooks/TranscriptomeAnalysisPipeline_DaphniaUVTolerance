865eb9e3751d86c13f1dd70cb3398ceec254e360
Mae <merimaat1@gmail.com>
2019-07-14 14:56:44 -0400

    Partitioning reads into sets

--- stats_tuxedo.sh
+++ stats_tuxedo.sh
@@ -5,18 +5,26 @@
 #$ -N stats_tuxedo_jobOutput
 #$ -pe smp 8 
 #Required modules for ND CRC servers
-module load bio/cufflinks/2.2.1
+#module load bio/cufflinks/2.2.1
 #Prepare for analysis
 cd ..
 dirFlag=0
 runNum=0
 COUNTER=0
 genomeFile=TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/PA42.3.0.annotation.18440.gff
+repCount=0
+treCount=0
+genCount=0
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
    	echo "No folder name(s) supplied... exiting"
    	exit 1
 fi
+#Retrieve inputs for number of reads, replicates, genotypes, and treatments
+readMax=${#READARRAY[@]}-1
+repMax=${#REPARRAY[@]}-1
+treMax=${#TREARRAY[@]}-1
+genMax=${#GENARRAY[@]}-1
 #Retrieve folders to analyze from the input arguments
 for f1 in "$@"; do
 	#Determine what analysis method was used for the input folder of data
@@ -25,23 +33,69 @@ for f1 in "$@"; do
 		analysisMethod=hisat2
 		#Loop through all forward and reverse paired reads and store the file locations in an array
 		for f2 in "$f1"/out/*.bam; do
-	    	READARRAY[COUNTER]="$f2, "
-			let COUNTER+=1
+			while [ $COUNTER -lt $readMax ]; do
+				#Determine which read to add next to the set of replicates/samples
+				if [[ $f2 == *"${REPARRAY[repCounter]}_${TREARRAY[genCounter]}_${GENARRAY[treCounter]}"* ]]; then
+					if [[ $repCounter -eq $repMax && $treCounter -ne $treMax && $genCounter -ne $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2"
+						let COUNTER+=1
+						repCounter=0
+						let treCounter+=1
+					elif [[ $repCounter -eq $repMax && $treCounter -eq $treMax && $genCounter -ne $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2"
+						let COUNTER+=1
+						repCounter=0
+						treCounter=0
+						let genCounter+=1
+					elif [[ $repCounter -eq $repMax && $treCounter -eq $treMax && $genCounter -eq $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2"
+						let COUNTER+=1
+					else
+						#Add the next sample to the read array for input to cuffdiff
+						READARRAY[COUNTER]="$f2, "
+						let COUNTER+=1
+						let repCounter+=1
+					fi
+				fi
+			done
 		done
-		#Re set the last array element to remove the last two characters
-		unset 'READARRAY[${#READARRAY[@]}-1]'
-		READARRAY[COUNTER]="$f2"
 	elif [[ $f1 == *"tophat2"* ]]; then
 		#Set analysis method for folder naming
 		analysisMethod=tophat2	
 		#Loop through all forward and reverse paired reads and store the file locations in an array
-		for f2 in "$f1"/out/*; do
-	    	READARRAY[COUNTER]="$f2/accepted_hits.bam, "
-			let COUNTER+=1
+		for f2 in "$f1"/out/*.bam; do
+			while [ $COUNTER -lt $readMax ]; do
+				#Determine which read to add next to the set of replicates/samples
+				if [[ $f2 == *"${REPARRAY[repCounter]}_${TREARRAY[genCounter]}_${GENARRAY[treCounter]}"* ]]; then
+					if [[ $repCounter -eq $repMax && $treCounter -ne $treMax && $genCounter -ne $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2/accepted_hits.bam"
+						let COUNTER+=1
+						repCounter=0
+						let treCounter+=1
+					elif [[ $repCounter -eq $repMax && $treCounter -eq $treMax && $genCounter -ne $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2/accepted_hits.bam"
+						let COUNTER+=1
+						repCounter=0
+						treCounter=0
+						let genCounter+=1
+					elif [[ $repCounter -eq $repMax && $treCounter -eq $treMax && $genCounter -eq $genMax ]]; then
+						#Add the last sample to the end of the set of replicates/samples
+						READARRAY[COUNTER]="$f2/accepted_hits.bam"
+						let COUNTER+=1
+					else
+						#Add the next sample to the read array for input to cuffdiff
+						READARRAY[COUNTER]="$f2/accepted_hits.bam, "
+						let COUNTER+=1
+						let repCounter+=1
+					fi
+				fi
+			done
 		done
-		#Re set the last array element to remove the last two characters
-		unset 'READARRAY[${#READARRAY[@]}-1]'
-		READARRAY[COUNTER]="$f2/accepted_hits.bam"
 	else
 		echo "The $f1 folder or bam files were not found... exiting"
 		exit 1
@@ -61,6 +115,7 @@ for f1 in "$@"; do
 			let runNum=0
 		fi
 	done
+	echo ${READARRAY[@]}
 	#Run cuffdiff on the aligned reads stored in the file array using 8 threads
-	cuffdiff -p 8 -o stats_"$analysisMethod"Tuxedo_run"$runNum" "$genomeFile" "${READARRAY[@]}"
+	#cuffdiff -p 8 -o stats_"$analysisMethod"Tuxedo_run"$runNum" "$genomeFile" "${READARRAY[@]}"
 done
