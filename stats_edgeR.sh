4b76ae473205e73a0bd0593d8bdcbadbe69fe225
Mae <merimaat1@gmail.com>
2019-10-01 03:45:29 -0400

    Fixed file paths

--- stats_edgeR.sh
+++ stats_edgeR.sh
@@ -110,7 +110,7 @@ for f1 in "$@"; do
 		fi
 	fi
 	#Loop through all forward and reverse paired reads and store the file locations in an array
-	for f2 in $analysisFiles*; do
+	for f2 in "$analysisFiles"; do
 		echo "Sample $f2$analysisExtension is being counted..."
 		htseq-count -f bam -s no -m union -t gene -i ID -o "stats_"$analysisMethod"EdgeR_run"$runNum"/${f2:${#analysisFiles}:(${#f2}-${#analysisFiles}+${#analysisExtension}+${#analysisTag})}.counted.sam" "$f2$analysisExtension" "$genomeFile"
 		echo "Sample $f2$analysisExtension has been counted!"
