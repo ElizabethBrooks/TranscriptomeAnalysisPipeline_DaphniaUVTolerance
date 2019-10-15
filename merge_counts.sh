400ee590897c822ca2de052892bf934bf4018870
Mae <merimaat1@gmail.com>
2019-10-15 13:38:07 -0400

    Added array of file tags

--- merge_counts.sh
+++ merge_counts.sh
@@ -10,43 +10,74 @@ wordCOUNTER=0
 fileFlag=0
 #Retrieve location of gene count files to be merged
 geneCounts=$1
+#Determine if the folder name was input in the correct format
+if [[ $geneCounts == *\/* ]] || [[ $geneCounts == *\\* ]]; then
+	echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
+	exit 1
+fi	
 #Set name for merge gene counts file
 mergedCounts="geneCounts_merged.txt"
-mkdir "$mergedCounts"
+#Make a new directory for each analysis run
+while [ $dirFlag -eq 0 ]; do
+	outputFolder=counts_merged_run"$runNum"
+	mkdir "$outputFolder"
+	#Check if the folder already exists
+	if [ $? -ne 0 ]; then
+		#Increment the folder name
+		let runNum+=1
+	else
+		#Indicate that the folder was successfully made
+		dirFlag=1
+		echo "Creating folder for $runNum run of merging counts of $f1 data..."
+	fi
+done
 #Retrieve merge order list from file
 #UPDATE: similarly to statsInputs_tuxedo.txt for counting_cuffdiff.sh
 inputsFile="TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/mergeInputs.txt"
 #Merge gene counts from file based on order in mergeOrder.txt
 while IFS= read -r line; do
 	#Determine tags for current file in list
-	#UPDATE: add line tags to identify fields
 	for word in $line; do
-		if [[ COUNTER -eq 0 ]]; then
-			replicationTag=$word
+		elif [[ COUNTER -eq 0 ]]; then
+			REPARRAY[repCount]="$word"
+		   	let repCount+=1
 		elif [[ COUNTER -eq 1 ]]; then
-			genotypeTag="$word"
+		   	TREARRAY[treCount]="$word"
+		   	let treCount+=1
 		elif [[ COUNTER -eq 2 ]]; then
-		   	treatmentTag="$word"
+		   	GENARRAY[genCount]="$word"
+		   	let genCount+=1
 		else
 		   	echo "ERROR: Incorrect number of lines in mergeInputs.txt... exiting"
 		   	exit 1
 		fi
 		let wordCOUNTER+=1
 	done
-	#Merge files based on tag order
-	echo "File $currentFile is being merged..."
-	currentFile="$replicationTag"_"$genotypeTag"_"$treatmentTag"
-	if [ $fileFlag -eq 0 ]; then #Output the first column with gene IDs
-		cp "$geneCounts"/*"$currentFile"* "$mergedCounts"
+done < "$inputsFile"
+wordCOUNTER=0
+#Create array of sorted tags for search files
+for genTag in ${GENARRAY[@]}; do
+	for treTag in ${TREARRAY[@]}; do
+		for repTag in ${REPARRAY[@]}; do
+			TAGARRAY[wordCOUNTER]="$repTag"_"genTag"_"treTag"
+			let wordCOUNTER+=1
+		done
+	done
+done
+echo ${TAGARRAY[@]}
+#Merge files based on tag order
+#	currentFile="$replicationTag"_"$genotypeTag"_"$treatmentTag"
+#	echo "File $currentFile is being merged..."
+#	if [ $fileFlag -eq 0 ]; then #Output the first column with gene IDs
+#		cp "$geneCounts"/*"$currentFile"* "$mergedCounts"
 		#Insert header line
-		sed -i.bak 1i"gene0" "$mergedCounts"
-	else #Add the gene counts from the next file
-		cut -d' ' -f1 "$geneCounts"/*"$currentFile"*
-		paste -d' ' "$mergedCounts" "$geneCounts"/*"$currentFile"*
+#		sed -i.bak 1i"gene0" "$mergedCounts"
+#	else #Add the gene counts from the next file
+#		cut -d' ' -f1 "$geneCounts"/*"$currentFile"*
+#		paste -d' ' "$mergedCounts" "$geneCounts"/*"$currentFile"*
 	#else
 		#echo "ERROR: Please check that mereOrder.txt input tags are in the same order found in gene count file names... exiting!"
-	fi
+#	fi
 	#Insert current file tags to header line
-	sed -i.bak "1 s/$/ $currentFile/" "$mergedCounts"
-	let wordCOUNTER=0
-done < "$inputsFile"
+#	sed -i.bak "1 s/$/ $currentFile/" "$mergedCounts"
+#	let wordCOUNTER=0
