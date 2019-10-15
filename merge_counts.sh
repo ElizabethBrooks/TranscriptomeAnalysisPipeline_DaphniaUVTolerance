a63b509a657ea398a5f18321a18adecbcc8a5e53
Mae <merimaat1@gmail.com>
2019-10-15 15:51:47 -0400

    Testing paste and cut

--- merge_counts.sh
+++ merge_counts.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #Move out of script repository folder
 cd ..
-dirFlag=0
-runNum=0
 #Initializing file tag variables
 genotypeTag=""
 treatmentTag=""
 replicationTag=""
 #Initialize counters and flags
+dirFlag=0
+runNum=0
 wordCOUNTER=0
-fileFlag=0
 #Retrieve location of gene count files to be merged
 geneCounts=$1
 #Determine if the folder name was input in the correct format
@@ -66,18 +65,21 @@ done
 wordCOUNTER=0
 #Merge files based on tag order
 for currentFile in ${TAGARRAY[@]}; do
-	if [ $fileFlag -eq 0 ]; then #Output the first column with gene IDs
-		echo "Sample $currentFile is being merged..."
+	if [ $wordCOUNTER -eq 0 ]; then #Output the first column with gene IDs
+		echo "First sample $currentFile is being merged..."
 		cp "$geneCounts"/*"$currentFile"* "$outputFolder"/"$mergedCounts"
 		#Insert header line
 		sed -i.bak 1i"gene0" "$outputFolder"/"$mergedCounts"
-		fileFlag=1
+	elif [ $wordCOUNTER -eq ${#TAGARRAY[@]} ]; then
+		echo "Last sample $currentFile is being merged..."
+		$(paste -d' ' "$geneCounts"/*"$currentFile"* "$outputFolder"/"$mergedCounts") >> "$outputFolder"/"$mergedCounts"
 	else #Add the gene counts from the next file
-		echo "Sample $currentFile is being merged..."
+		echo "Next sample $currentFile is being merged..."
+		$(paste -d' ' "$geneCounts"/*"$currentFile"* "$outputFolder"/"$mergedCounts" | cut -d' ' -f1) >> "$outputFolder"/"$mergedCounts"
 		#cut -d' ' -f1 "$geneCounts"/*"$currentFile"* | paste -d' ' "$outputFolder"/"$mergedCounts" -
-		cat $(paste -d' ' "$outputFolder"/"$mergedCounts" <(cut -d' ' -f1 "$geneCounts"/*"$currentFile"*)) >> "$outputFolder"/"$mergedCounts"
+		#cat $(paste -d' ' "$outputFolder"/"$mergedCounts" <(cut -d' ' -f1 "$geneCounts"/*"$currentFile"*)) >> "$outputFolder"/"$mergedCounts"
 	fi
 	#Insert current file tags to header line
 	sed -i.bak "1 s/$/ $currentFile/" "$outputFolder"/"$mergedCounts"
-	let wordCOUNTER=0
+	let wordCOUNTER+=1
 done
