3b904c095f688f3abc50bf74fa784d6b824b4b44
Mae <merimaat1@gmail.com>
2019-11-02 19:09:16 -0400

    Testing tophat output

--- alignment_tophat2.sh
+++ alignment_tophat2.sh
@@ -41,7 +41,6 @@ for f1 in "$@"; do
 	buildFileNoEx=$(echo $buildFileNoPath | sed 's/\.fa//')
 	#Copy genome file to the current Tophat run folder
 	genomeFileNoPath=$(basename $genomeFile)
-	#Tophat tmp output directory
 	cp "$genomeFile" "$tophatOut"/"$genomeFileNoPath"
 	#Loop through all forward and reverse paired reads and run tophat2 on each pair
 	# using 8 threads
@@ -53,9 +52,7 @@ for f1 in "$@"; do
 		curSampleNoEx=$(echo $curFileNoPath | sed 's/.pForward\.fq\.gz//')
 		#Begin Tophat run for current sample
 		echo "Sample $curFileNoPath is being aligned..."
-		cd "$tophatOut"
-		tophat2 -p 8 -G "$genomeFileNoPath" -o "$curFileNoPath" ../"$buildOut"/"$buildFileNoEx" ../"$f2" ../"$curSample"_pReverse.fq.gz
-		cd ..
+		tophat2 -p 8 -G "$tophatOut"/"$genomeFileNoPath" -o "$tophatOut"/"$curFileNoPath" "$buildOut"/"$buildFileNoEx" "$f2" "$curSample"_pReverse.fq.gz
 		echo "Sample $curFileNoPath has been aligned!"
 	done
 done
