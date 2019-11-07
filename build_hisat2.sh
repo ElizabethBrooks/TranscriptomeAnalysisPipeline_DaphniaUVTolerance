263db714a040fdacca7bc124a10a153a52538d75
Mae <merimaat1@gmail.com>
2019-11-06 22:35:28 -0500

    Fixed naming typos

--- build_hisat2.sh
+++ build_hisat2.sh
@@ -2,7 +2,7 @@
 #$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -r n
-#$ -N build_bowtie2_jobOutput
+#$ -N build_hisat2_jobOutput
 #$ -pe smp 8
 #Required modules for ND CRC servers
 module load bio
@@ -24,12 +24,12 @@ if [ $? -eq 0 ]; then
 	cp "$buildFile" "$buildOut"/"$buildFileNoPath"
 	#Trim .fa file extension from build file
 	buildFileNoEx=$(echo $buildFileNoPath | sed 's/\.fasta//')
-	#Begin Bowtie2 build
-	echo "Beginning bowtie2 build... "
+	#Begin hisat2 build
+	echo "Beginning hisat2 build... "
 	hisat2-build -p 8 -f "$buildOut"/"$buildFileNoPath" "$buildOut"/"$buildFileNoEx"
-	echo "Bowtie2 build complete!"
+	echo "hisat2 build complete!"
 else
-	echo "Build folder reference_bowtie2_build already exists, skipping building..."
+	echo "Build folder reference_hisat2_build already exists, skipping building..."
 fi
 #Add run inputs to output summary file
-echo "bowtie2-build "$buildOut"/"$buildFileNoPath" "$buildOut"/"$buildFileNoEx >> $inputOutFile
+echo "hisat2-build -p 8 -f "$buildOut"/"$buildFileNoPath" "$buildOut"/"$buildFileNoEx >> $inputOutFile
