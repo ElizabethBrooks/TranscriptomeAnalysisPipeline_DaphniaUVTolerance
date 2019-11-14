f9c78cba463809aa63e719a04e4cb912cd316938
Mae <merimaat1@gmail.com>
2019-11-13 23:19:39 -0500

    Testing alignment summary script

--- AlignmentAnalysis/alignmentSummary_overall.sh
+++ AlignmentAnalysis/alignmentSummary_overall.sh
@@ -19,13 +19,13 @@ for f1 in $@; do
 	#Determine what analysis method was used for the input folder of data
 	if [[ "$f1" == *"hisat2"*  ]]; then
 		#Set analysis method for folder naming
-		analysisMethod="Hisat2"
+		analysisMethod="hisat2"
 	elif [[ "$f1" == *"tophat2"* ]]; then
 		#Set analysis method for folder naming
-		analysisMethod="Tophat2"
+		analysisMethod="tophat2"
 	else
 		echo "ERROR: The $f1 folder or bam files were not found... exiting"
-		exit 1
+		#exit 1
 	fi
 	#Prepare input and output file names
 	inputStats="$f1"
@@ -34,11 +34,11 @@ for f1 in $@; do
 	runNum=$(echo "$f1" | sed "s/aligned_"$analysisMethod"_//g")
 	#Retrieve summaries for each aligned sample
 	for f2 in "$f1"/*/; do
-		echo "Merging sample $f1 of $analysisMethod alignment summary..."
+		echo "Merging sample $f2 of $analysisMethod alignment summary..."
 		#Retrieve sample summary based on alignment method
 		#bash alignmentSummary_"$analysisMethod"_sample.sh "$f1" "$analysisMethod"
 		#Combine summaries into one csv file
 		#cat "$outputStats"_"$analysisMethod"_combined_"$runNum".csv >> "$outputStats"_"$analysisMethod"_allSamples_"$runNum".csv
-		echo "Sample $f1 of $analysisMethod alignment summary has been merged!"
+		echo "Sample $f2 of $analysisMethod alignment summary has been merged!"
 	done
 done
