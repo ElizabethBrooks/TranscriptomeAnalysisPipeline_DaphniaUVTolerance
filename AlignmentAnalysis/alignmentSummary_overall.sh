82237d1c14c179f37a1d79892b967e70f0f1f738
Mae <merimaat1@gmail.com>
2019-11-13 23:24:23 -0500

    Testing alignment summary script

--- AlignmentAnalysis/alignmentSummary_overall.sh
+++ AlignmentAnalysis/alignmentSummary_overall.sh
@@ -11,34 +11,36 @@ fi
 cd ../..
 #Retrieve folders to analyze from the input arguments to the script
 for f1 in $@; do
+	folderInput=$(echo "$f1")
 	#Determine if the folder name was input in the correct format
-	if [[ "$f1" == *\/* ]] || [[ "$f1" == *\\* ]]; then
+	if [[ "$folderInput" == *\/* ]] || [[ "$folderInput" == *\\* ]]; then
 		echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
 		exit 1
 	fi
 	#Determine what analysis method was used for the input folder of data
-	if [[ "$f1" == "*hisat2*"  ]]; then
+	if [[ "$folderInput" == *"hisat2"*  ]]; then
 		#Set analysis method for folder naming
 		analysisMethod="hisat2"
-	elif [[ "$f1" == "*tophat2*" ]]; then
+	elif [[ "$folderInput" == *"tophat2"* ]]; then
 		#Set analysis method for folder naming
 		analysisMethod="tophat2"
 	else
-		echo "ERROR: The $f1 folder or bam files were not found... exiting"
+		echo "ERROR: The $folderInput folder or bam files were not found... exiting"
 		#exit 1
 	fi
 	#Prepare input and output file names
-	inputStats="$f1"
+	inputStats="$folderInput"
 	outputStats=TranscriptomeAnalysisPipeline_DaphniaUVTolerance/AlignmentAnalysis/alignmentSummarized_"$analysisMethod"
 	#Retrieve run number for input alignment folder
-	runNum=$(echo "$f1" | sed "s/aligned_"$analysisMethod"_//g")
+	runNum=$(echo "$folderInput" | sed "s/aligned_"$analysisMethod"_//g")
 	#Retrieve summaries for each aligned sample
-	for f2 in "$f1/*/"; do
-		echo "Merging sample $f2 of $analysisMethod alignment summary..."
+	for f2 in "$folderInput"/*/; do
+		sampleInput=$(echo "$f2")
+		echo "Merging sample $sampleInput of $analysisMethod alignment summary..."
 		#Retrieve sample summary based on alignment method
 		#bash alignmentSummary_"$analysisMethod"_sample.sh "$f1" "$analysisMethod"
 		#Combine summaries into one csv file
 		#cat "$outputStats"_"$analysisMethod"_combined_"$runNum".csv >> "$outputStats"_"$analysisMethod"_allSamples_"$runNum".csv
-		echo "Sample $f2 of $analysisMethod alignment summary has been merged!"
+		echo "Sample $sampleInput of $analysisMethod alignment summary has been merged!"
 	done
 done
