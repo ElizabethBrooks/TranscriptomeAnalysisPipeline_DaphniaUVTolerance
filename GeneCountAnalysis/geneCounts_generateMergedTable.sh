962f49029246c18f2012e52d6a7fa59610671619
Mae <merimaat1@gmail.com>
2019-11-25 22:49:11 -0500

    Updated matrix prep

--- GeneCountAnalysis/geneCounts_generateMergedTable.sh
+++ GeneCountAnalysis/geneCounts_generateMergedTable.sh
@@ -19,11 +19,19 @@ fi
 inputsPath=$(grep "counting:" InputData/outputPaths.txt | tr -d " " | sed "s/counting://g")
 #Retrieve alignment outputs absolute path
 outputsPath=$(grep "geneCountAnalysis:" InputData/outputPaths.txt | tr -d " " | sed "s/geneCountAnalysis://g")
+#Prepare tags file for comparison
+grep "Pool_1" mergeGuideFile_"$1"_"$2".txt > tmp.txt | sed -ie 's|^|Pool_1_|' tmp.txt
+grep "Pool_2" mergeGuideFile_"$1"_"$2".txt >> tmp.txt | sed -ie 's|^|Pool_2_|' tmp.txt
+grep "Pool_3" mergeGuideFile_"$1"_"$2".txt >> tmp.txt | sed -ie 's|^|Pool_3_|' tmp.txt
 #Loop through all counted paired reads and append each sample tag
 # with the corresponding file path
+cat ../InputData/mergeCounts_tags_"$2".txt > "$outputsPath"/mergeGuideFile_"$1"_"$2".txt
 for f1 in "$inputsPath"/"$1"/*/; do
-	sed -i -e 's|^|'"$f1"'counts.txt|' "$outputsPath"/mergeGuideFile_"$1"_"$2".txt
+	currSample=$(basename "$f1" | sed "s/140327_I481_FCC3P1PACXX_L..//g")
+	currTag=$(grep "$currSample" tmp.txt | sed "s/^Pool_.//g")
+	sed -i 's/'"$currTag"'/'"$currTag"' '"$f1"'/g' "$outputsPath"/mergeGuideFile_"$1"_"$2".txt
 done
+#Merge gene counts based on generated guide file
 python merge_tables.py "$outputsPath"/mergeGuideFile_"$1"_"$2".txt
 #Rename the output merged counts file
 mv merged_counts.txt "$outputsPath"/mergedCounts_"$1"_"$2".txt
