fa1b798927975d462dbe0a33913d5ab707b30c39
Mae <merimaat1@gmail.com>
2019-11-26 01:20:41 -0500

    Testing gene count merging

--- GeneCountAnalysis/geneCounts_generateMergedTable.sh
+++ GeneCountAnalysis/geneCounts_generateMergedTable.sh
@@ -29,10 +29,10 @@ cat "$outputsPath"/tmp*.txt >> "$outputsPath"/tmp.txt
 #Loop through all counted paired reads and append each sample tag
 # with the corresponding file path
 cat ../InputData/mergeCounts_guideFile_tags_"$2".txt > "$outputsPath"/mergeCounts_guideFile_"$1"_"$2".txt
-for f1 in "$inputsPath"/"$1"/*/; do
+for f1 in "$inputsPath""$1"/*/; do
 	currSample=$(basename "$f1" | sed "s/140327_I481_FCC3P1PACXX_L..//g")
 	currTag=$(grep "$currSample" "$outputsPath"/tmp.txt | sed "s/^Pool_._//g")
-	sed -i 's/"$currTag"/"$currTag""$f1"/g' "$outputsPath"/mergeCounts_guideFile_"$1"_"$2".txt
+	sed -i 's,'"$currTag"','"$currTag"' '"$f1"',g' "$outputsPath"/mergeCounts_guideFile_"$1"_"$2".txt
 done
 head "$outputsPath"/mergeCounts_guideFile_"$1"_"$2".txt
 #Clean up
