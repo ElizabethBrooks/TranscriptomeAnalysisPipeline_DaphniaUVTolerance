cbe53628325395f93db4a9620a4c0464ddf4ff0a
Mae <merimaat1@gmail.com>
2019-12-12 02:17:34 -0500

    Fixed sample name var

--- Pipeline/counting_htseq.sh
+++ Pipeline/counting_htseq.sh
@@ -73,12 +73,11 @@ done
 inputOutFile="$outputFolder"/"$outputFolder"_summary.txt
 #Loop through all sorted forward and reverse paired reads and store the file locations in an array
 for f1 in "$inputsPath"/"$1"/*/*.bam; do
-	#Current sample
-	curSampleNoPath=$(basename $f1)
-	#Name of aligned file
+	#Name of sorted and aligned file
 	curAlignedSample="$f1"
-	#Trim file path from current file name
-	curSampleNoPath=$(echo $curSampleNoPath | sed 's/\.bam//')
+	#Trim file paths from current sample folder name
+	curSampleNoPath=$(echo $f1 | sed 's/accepted\_hits\.bam//')
+	curSampleNoPath=$(basename $curSampleNoPath)
 	#Create directory for current sample outputs
 	mkdir "$outputFolder"/"$curSampleNoPath"
 	#Count reads using htseq-count
@@ -90,22 +89,23 @@ for f1 in "$inputsPath"/"$1"/*/*.bam; do
 		#Flag to output features in sam format
 		#-o "$outputFolder"/"$curSampleNoPath"/counted.sam
 		htseq-count -f bam -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore "$curAlignedSample" "$genomeFile" > "$outputFolder"/"$curSampleNoPath"/counts.txt
-		commandInputs=htseq-count -f bam -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore "$curAlignedSample" "$genomeFile" > "$outputFolder"/"$curSampleNoPath"/counts.txt
+		#Add run inputs to output summary file
+		echo "$curSampleNoPath" >> $inputOutFile
+		echo "htseq-count -f bam -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore" "$curAlignedSample" "$genomeFile" ">" "$outputFolder"/"$curSampleNoPath"/counts.txt >> $inputOutFile
 	elif [[ "$1"  == sortedCoordinate* ]]; then
 		#Use coordinate sorted flag
 		#https://github.com/simon-anders/htseq/issues/37
 		#Flag to output features in sam format
 		#-o "$outputFolder"/"$curSampleNoPath"/counted.sam
 		htseq-count -f bam -r pos -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore "$curAlignedSample" "$genomeFile" > "$outputFolder"/"$curSampleNoPath"/counts.txt
-		commandInputs=htseq-count -f bam -r pos -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore "$curAlignedSample" "$genomeFile" > "$outputFolder"/"$curSampleNoPath"/counts.txt
+		#Add run inputs to output summary file
+		echo "$curSampleNoPath" >> $inputOutFile
+		echo "htseq-count -f bam -r pos -s no -m union -t gene -i ID --secondary-alignments ignore --supplementary-alignments ignore" "$curAlignedSample" "$genomeFile" ">" "$outputFolder"/"$curSampleNoPath"/counts.txt >> $inputOutFile
 	else
 		echo "ERROR: The "$1" folder of name or coordinate sorted files were not found... exiting"
 		exit 1
 	fi
 	echo "Sample $curSampleNoPath has been counted!"
-	#Add run inputs to output summary file
-	echo "$curSampleNoPath" >> $inputOutFile
-	echo "$commandInputs" "$curAlignedSample" "$genomeFile" ">" "$outputFolder"/"$curSampleNoPath"/counts.txt >> $inputOutFile
 done
 #Copy previous summaries
 cp "$inputsPath"/"$1"/*.txt "$outputFolder"
