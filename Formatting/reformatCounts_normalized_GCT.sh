5bc3f8d95b549829a8ab6a4a1dbaae07ee19eedf
Mae <merimaat1@gmail.com>
2020-02-11 23:34:05 -0500

    Updated GCT formatting script

--- Formatting/reformatCounts_normalized_GCT.sh
+++ Formatting/reformatCounts_normalized_GCT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/bin/bash
 #Script to re-format merged count tables to Gene Cluster Text file format (*.gct)
-#Usage: bash reformatCounts_GCT.sh countsFile
-#Usage Ex: bash reformatCounts_GCT.sh geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned/geneCountStats_cols1to6_geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned
+#Usage: bash reformatCounts_normalized_GCT.sh countsFile
+#Usage Ex: bash reformatCounts_normalized_GCT.sh geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned/geneCountStats_cols1to6_geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned
 
 #Check for input argument of file name
 if [ $# -eq 0 ]; then
@@ -17,10 +17,11 @@ inputsPath=$(grep "statistics:" ../InputData/outputPaths.txt | tr -d " " | sed "
 numRows=0
 numCols=0
 #Retrieve input filename
-inFile="$inputsPath"/"$1"/stats_exactTest.csv
+inFile="$inputsPath"/"$1"/stats_normalizedCounts.csv
 countsFile=$(basename "$inFile" | sed 's/\.txt//g' | sed 's/\.csv//g')
 #Change delimiter for csv files
 sed 's/,/\t/g' "$inFile" > tmpInFile.txt
+sed -i 's/ /\t/g' tmpInFile.txt
 #Retrieve number of rows
 wc -l tmpInFile.txt > tmpNumRows.txt
 numRows=$(cut -d ' ' -f1 tmpNumRows.txt)
@@ -28,7 +29,6 @@ numRows=$(cut -d ' ' -f1 tmpNumRows.txt)
 numCols=$(($(head -n1 tmpInFile.txt | awk '{print NF}')-1))
 #Set output file name
 outFile="$outputsPath"/"$1"/"$countsFile"_reformatted.gct
-mkdir "$outputsPath"/GeneCounts_Merged/"$1"
 #Output headers for GCT formatting
 echo "#1.2" > tmpHeader.gct
 echo -e "$numRows \t $numCols" >> tmpHeader.gct
