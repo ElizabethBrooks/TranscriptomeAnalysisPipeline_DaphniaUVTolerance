68f1a297eb128f6aff724317c52fdc04e582c00d
Mae <merimaat1@gmail.com>
2020-02-12 16:08:45 -0500

    Updated GCT scipt for norm counts

--- Formatting/reformatCounts_normalized_GCT.sh
+++ Formatting/reformatCounts_normalized_GCT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/bin/bash
 #Script to re-format merged count tables to Gene Cluster Text file format (*.gct)
-#Usage: bash reformatCounts_normalized_GCT.sh countsFile
-#Usage Ex: bash reformatCounts_normalized_GCT.sh geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned/geneCountStats_cols1to6_geneCounts_merged_counted_htseqTophat2_run1_fullset_cleaned
+#Usage: bash reformatCounts_normalized_GCT.sh countsFile sampleSet
+#Usage Ex: bash reformatCounts_normalized_GCT.sh GeneCountsAnalyzed_countedCoordinate_htseqHisat2_run1_fullset sampleSet
 
 #Check for input argument of file name
 if [ $# -eq 0 ]; then
@@ -17,8 +17,7 @@ inputsPath=$(grep "statistics:" ../InputData/outputPaths.txt | tr -d " " | sed "
 numRows=0
 numCols=0
 #Retrieve input filename
-inFile="$inputsPath"/"$1"/stats_normalizedCounts.csv
-countsFile=$(basename "$inFile" | sed 's/\.txt//g' | sed 's/\.csv//g')
+inFile="$inputsPath"/"$1"/geneCountStats_"$2"/stats_normalizedCounts.csv
 #Change delimiter for input files
 sed 's/,/\t/g' "$inFile" > tmpInFile.txt
 sed -i 's/ /\t/g' tmpInFile.txt
@@ -28,7 +27,7 @@ numRows=$(cut -d ' ' -f1 tmpNumRows.txt)
 #Retrieve number of samples
 numCols=$(($(head -n1 tmpInFile.txt | awk '{print NF}')-1))
 #Set output file name
-outFile="$outputsPath"/"$1"/"$countsFile"_reformatted.gct
+outFile="$outputsPath"/"$1"/geneCountStats_"$2"/stats_normalizedCounts_reformatted.gct
 #Output headers for GCT formatting
 echo "#1.2" > tmpHeader.gct
 echo -e "$numRows \t $numCols" >> tmpHeader.gct
