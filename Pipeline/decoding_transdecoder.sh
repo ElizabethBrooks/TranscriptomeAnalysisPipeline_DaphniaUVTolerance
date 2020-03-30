f3cd230c6eaee39cd0354d58faa87e2c070f8b24
Mae <merimaat1@gmail.com>
2020-03-30 14:24:10 -0400

    Added folder check

--- Pipeline/decoding_transdecoder.sh
+++ Pipeline/decoding_transdecoder.sh
@@ -12,13 +12,33 @@
 #Load necessary modules for ND CRC servers
 module load bio/transdecoder
 #module load bio/cufflinks
+#Check for input arguments of folder names
+if [ $# -eq 0 ]; then
+   	echo "No folder name(s) supplied... exiting"
+   	exit 1
+fi
+#Determine if the folder name was input in the correct format
+if [[ "$1" == *\/* ]] || [[ "$1" == *\\* ]]; then
+	echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
+	exit 1
+fi
+#Determine if the correct analysis folder was input
+if [[ "$1"  != trimmed*assembly_Trinity ]]; then
+	echo "ERROR: The "$1" folder of trimmed fq.gz files were not found... exiting"
+	exit 1
+fi
 #Retrieve genome reference and features paths
 multiFASTAPath=$(grep "assembling:" ../InputData/inputPaths.txt | tr -d " " | sed "s/assembling://g")
-multiFASTA="$multiFASTAPath"/"$1"/*.fasta
+multiFASTA="$multiFASTAPath"/"$1"/Trinity.fasta
 #Retrieve outputs absolute path
 outputsPath=$(grep "decoding:" ../InputData/outputPaths.txt | tr -d " " | sed "s/decoding://g")
 outFolder="$outputsPath"/decoded_"$1"
 mkdir "$outFolder"
+#Check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $outputFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
 #Move to output folder
 cd "$outFolder"
 #Generate your best candidate open rading frame (ORF) predictions
