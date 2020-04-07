962d4bea637b35a8ec5cb18d6ba801c36682e3b8
Mae <merimaat1@gmail.com>
2020-04-07 19:44:46 -0400

    Commented out modules for testing

--- Pipeline/decoding_transdecoder.sh
+++ Pipeline/decoding_transdecoder.sh
@@ -15,9 +15,9 @@
 #ex: wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam33.0/Pfam-A.full.gz
 
 #Load necessary modules for ND CRC servers
-module load bio/transdecoder
+#module load bio/transdecoder
 module load bio/blast+
-module load bio/hmmer
+#module load bio/hmmer
 #module load bio/cufflinks
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -38,7 +38,6 @@ fi
 inputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
 uniprotPath=$(grep "uniprotDBs:" ../InputData/outputPaths.txt | tr -d " " | sed "s/uniprotDBs://g")
 pfamPath=$(grep "pfamDB:" ../InputData/outputPaths.txt | tr -d " " | sed "s/pfamDB://g")
-uniprotDB="$uniprotPath"/"$2"
 multiFASTA=$(echo "$inputsPath"/"$1"/Trinity*.fasta)
 geneMap="$inputsPath"/"$1"/Trinity.fasta.gene_trans_map
 #Retrieve outputs absolute path
@@ -59,7 +58,7 @@ echo "Beginning decoding..."
 #Generate candidate ORFs
 #TransDecoder.LongOrfs -t "$multiFASTA" --gene_trans_map "$geneMap"
 #Use BlastP to search a protein database
-blastp -query "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotDB"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > "$outputFolder"/blastp.outfmt6
+blastp -query "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > "$outputFolder"/blastp.outfmt6
 #Search the peptides for protein domains using Pfam
 #hmmscan --cpu 8 --domtblout "$outputFolder"/pfam.domtblout "$pfamPath" "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep
 #Combine the Blast and Pfam search results into coding region selection
@@ -70,7 +69,7 @@ echo "Decoding finished!"
 #rm "$outputFolder"/pfam.domtblout
 #Output run commands to summary file
 echo "TransDecoder.LongOrfs -t" "$multiFASTA" "--gene_trans_map" "$geneMap" > "$inputOutFile"
-echo "blastp -query" "$outputFolder"/"Trinity.fasta.transdecoder_dir/longest_orfs.pep -db" "$uniprotDB"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 10 >" "$outputFolder"/"blastp.outfmt6" >> "$inputOutFile"
+echo "blastp -query" "$outputFolder"/"Trinity.fasta.transdecoder_dir/longest_orfs.pep -db" "$uniprotPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 10 >" "$outputFolder"/"blastp.outfmt6" >> "$inputOutFile"
 echo "hmmscan --cpu 8 --domtblout" "$outputFolder"/"pfam.domtblout" "$pfamPath" "$outputFolder"/"Trinity.fasta.transdecoder_dir/longest_orfs.pep" >> "$inputOutFile"
 echo "TransDecoder.Predict -t" "$multiFASTA" >> "$inputOutFile"
 #Copy previous summaries
