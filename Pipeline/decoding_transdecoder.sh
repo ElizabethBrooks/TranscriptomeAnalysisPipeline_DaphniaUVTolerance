4faddfc1bf42e66aa61d9b27e4e2fefab6c0adc2
Mae <merimaat1@gmail.com>
2020-04-07 23:52:07 -0400

    Commented out for hmmscan testing

--- Pipeline/decoding_transdecoder.sh
+++ Pipeline/decoding_transdecoder.sh
@@ -47,15 +47,15 @@ outputFolder="$outputsPath"/decoded_"$1"
 #Move to output folder
 cd "$outputFolder"
 #Name output file of inputs
-inputOutFile="$outputFolder"/"$1"_decoding_summary.txt
+#inputOutFile="$outputFolder"/"$1"_decoding_summary.txt
 #Generate your best candidate open rading frame (ORF) predictions
 echo "Beginning decoding..."
 #Generate candidate ORFs
 #TransDecoder.LongOrfs -t "$multiFASTA" --gene_trans_map "$geneMap"
 #Use BlastP to search a protein database
-blastp -query "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > "$outputFolder"/blastp.outfmt6
+#blastp -query "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > "$outputFolder"/blastp.outfmt6
 #Search the peptides for protein domains using Pfam
-#hmmscan --cpu 8 --domtblout "$outputFolder"/pfam.domtblout "$pfamPath" "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep
+hmmscan --cpu 8 --domtblout "$outputFolder"/pfam.domtblout "$pfamPath" "$outputFolder"/Trinity.fasta.transdecoder_dir/longest_orfs.pep
 #Combine the Blast and Pfam search results into coding region selection
 #TransDecoder.Predict -t "$multiFASTA" --retain_pfam_hits "$outputFolder"/pfam.domtblout --retain_blastp_hits "$outputFolder"/blastp.outfmt6
 echo "Decoding finished!"
@@ -65,4 +65,4 @@ echo "Decoding finished!"
 #echo "hmmscan --cpu 8 --domtblout" "$outputFolder"/"pfam.domtblout" "$pfamPath" "$outputFolder"/"Trinity.fasta.transdecoder_dir/longest_orfs.pep" >> "$inputOutFile"
 #echo "TransDecoder.Predict -t" "$multiFASTA" >> "$inputOutFile"
 #Copy previous summaries
-cp "$inputsPath"/"$1"/*.txt "$outputFolder"
+#cp "$inputsPath"/"$1"/*.txt "$outputFolder"
