f438569b04b37d427057580629785c4fe4351285
Mae <merimaat1@gmail.com>
2020-04-08 21:18:27 -0400

    Testing blastp

--- Pipeline/decoding_transdecoder.sh
+++ Pipeline/decoding_transdecoder.sh
@@ -11,8 +11,8 @@
 
 #Load necessary modules for ND CRC servers
 #module load bio/transdecoder
-#module load bio/blast+
-module load bio/hmmer
+module load bio/blast+
+#module load bio/hmmer
 #module load bio/cufflinks
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -31,8 +31,8 @@ if [[ "$1"  != trimmed*assembly_Trinity ]]; then
 fi
 #Retrieve genome reference and features paths
 inputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
-#uniprotPath=$(grep "uniprotDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/uniprotDB://g")
-pfamPath=$(grep "pfamDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/pfamDB://g")
+uniprotPath=$(grep "uniprotDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/uniprotDB://g")
+#pfamPath=$(grep "pfamDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/pfamDB://g")
 #multiFASTA=$(echo "$inputsPath"/"$1"/Trinity*.fasta)
 #geneMap="$inputsPath"/"$1"/Trinity.fasta.gene_trans_map
 #Retrieve outputs absolute path
@@ -47,22 +47,22 @@ outputFolder="$outputsPath"/decodedTransdecoder_"$1"
 #Move to output folder
 cd "$outputFolder"
 #Name output file of inputs
-inputOutFile="$outputFolder"/"$1"_decodingTransdecoder_summary.txt
+#inputOutFile="$outputFolder"/"$1"_decodingTransdecoder_summary.txt
 #Generate your best candidate open rading frame (ORF) predictions
 echo "Beginning decoding..."
 #Generate candidate ORFs
 #TransDecoder.LongOrfs -t "$multiFASTA" --gene_trans_map "$geneMap"
 #Use BlastP to search a protein database
-#blastp -query Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastp.outfmt6
+blastp -query Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastp.outfmt6
 #Search the peptides for protein domains using Pfam
-hmmscan --cpu 8 --domtblout pfam.domtblout "$pfamPath" Trinity.fasta.transdecoder_dir/longest_orfs.pep
+#hmmscan --cpu 8 --domtblout pfam.domtblout "$pfamPath" Trinity.fasta.transdecoder_dir/longest_orfs.pep
 #Combine the Blast and Pfam search results into coding region selection
 #TransDecoder.Predict -t "$multiFASTA" --retain_pfam_hits pfam.domtblout --retain_blastp_hits blastp.outfmt6
 echo "Decoding finished!"
 #Output run commands to summary file
 #echo "TransDecoder.LongOrfs -t" "$multiFASTA" "--gene_trans_map" "$geneMap" > "$inputOutFile"
 #echo "blastp -query" "Trinity.fasta.transdecoder_dir/longest_orfs.pep -db" "$uniprotPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 >" "blastp.outfmt6" >> "$inputOutFile"
-echo "hmmscan --cpu 8 --domtblout" "pfam.domtblout" "$pfamPath" "Trinity.fasta.transdecoder_dir/longest_orfs.pep" >> "$inputOutFile"
+#echo "hmmscan --cpu 8 --domtblout" "pfam.domtblout" "$pfamPath" "Trinity.fasta.transdecoder_dir/longest_orfs.pep" >> "$inputOutFile"
 #echo "TransDecoder.Predict -t" "$multiFASTA" "--retain_pfam_hits pfam.domtblout --retain_blastp_hits blastp.outfmt6" >> "$inputOutFile"
 #Copy previous summaries
 #cp "$inputsPath"/"$1"/*.txt "$outputFolder"
