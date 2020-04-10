d8979fc31d8158ad3e04df343c086e640813bd31
Mae <merimaat1@gmail.com>
2020-04-09 20:35:22 -0400

    Fixing alignment analysis pathing

--- AlignmentAnalysis/alignmentSummary_overall.sh
+++ AlignmentAnalysis/alignmentSummary_overall.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #Bash script to retrieve mapping stats
-#Usage: bash alignmentSummary_overall.sh alignmentFolder
-#Usage Ex: bash alignmentSummary_overall.sh aligned_tophat2_run2
+#Usage: bash alignmentSummary_overall.sh alignmentFolders
+#Usage Ex: bash alignmentSummary_overall.sh aligned_hisat2_run2 aligned_tophat2_run1 aligned_tophat2_run3
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
    	echo "ERROR: No folder name(s) supplied... exiting"
    	exit 1
 fi
+#Retrieve input alignment summary absolute path
+inputsPath=$(grep "aligning:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligning://g")
 #Retrieve alignment analysis outputs absolute path
 outputsPath=$(grep "alignmentAnalysis:" ../InputData/outputPaths.txt | tr -d " " | sed "s/alignmentAnalysis://g")
 #Move to outputs directory
 cd "$outputsPath"/AlignmentsAnalyzed
-#Create directory for alignment analysis
-outputAnalysis=AlignmentAnalysis
-mkdir "$outputAnalysis"
 #Retrieve folders to analyze from the input arguments to the script
 for f1 in $@; do
 	#Determine if the folder name was input in the correct format
@@ -26,7 +25,7 @@ for f1 in $@; do
 		#Set analysis method for folder naming
 		analysisMethod="hisat2"
 		#Set output folder name
-		outputStats="$outputAnalysis"/alignmentSummarized_"$analysisMethod"
+		outputStats=alignmentSummarized_"$analysisMethod"
 		#Retrieve run number for input alignment folder
 		runNum=$(echo "$f1" | sed "s/aligned_"$analysisMethod"_//g")
 		#Set header of overall summary csv file
@@ -35,7 +34,7 @@ for f1 in $@; do
 		#Set analysis method for folder naming
 		analysisMethod="tophat2"
 		#Set output folder name
-		outputStats="$outputAnalysis"/alignmentSummarized_"$analysisMethod"
+		outputStats=alignmentSummarized_"$analysisMethod"
 		#Retrieve run number for input alignment folder
 		runNum=$(echo "$f1" | sed "s/aligned_"$analysisMethod"_//g")
 		#Set header of overall summary csv file
@@ -47,11 +46,10 @@ for f1 in $@; do
 	#Retrieve summaries for each aligned sample
 	for f2 in "$f1"/*/; do
 		#Retrieve sample name
-		sampleName=$(echo "$f2")
-		sampleName=$(basename "$sampleName")
+		sampleName=$(basename "$inputsPath"/"$f2")
 		echo "Merging sample $sampleName alignment summary..."
 		#Retrieve sample summary based on alignment method
-		bash TranscriptomeAnalysisPipeline_DaphniaUVTolerance/AlignmentAnalysis/alignmentSummary_"$analysisMethod"_sample.sh "$f2" "$analysisMethod" "$runNum"
+		bash TranscriptomeAnalysisPipeline_DaphniaUVTolerance/AlignmentAnalysis/alignmentSummary_"$analysisMethod"_sample.sh "$inputsPath"/"$f2" "$analysisMethod" "$runNum"
 		#Combine summaries into one csv file
 		cat "$outputStats"_combined_"$runNum".csv >> "$outputStats"_"$runNum".csv
 		rm "$outputStats"_combined_"$runNum".csv
