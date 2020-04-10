e18bfb2a88643644ca2c4a3d1494cae42e92441c
Mae <merimaat1@gmail.com>
2020-04-10 05:28:10 -0400

    Added plot titles

--- AlignmentAnalysis/alignmentSummary_barPlot_binnedCompareRun.r
+++ AlignmentAnalysis/alignmentSummary_barPlot_binnedCompareRun.r
@@ -26,8 +26,10 @@ counts <- data.frame(fullsetNames, aStats$overall, aStats$concordant, aStats$run
 #Create matrix for multiple plots
 par(mfrow=c(2,1))
 #Generate grouped and colored bar plot
+plotTitle <- normalizePath(basename(args[1]))
 plotOverall <- ggplot(counts, aes(factor(fullsetNames), aStats.overall, fill=aStats.run)) + 
   geom_bar(stat="identity", position="stack") +
+  ggtitle(plotTitle) +
   xlab("Sample Number") +
   ylab("Overall Percent") +
   scale_fill_brewer(palette="Set1")
@@ -36,8 +38,10 @@ plotOverall <- plotOverall + guides(fill=guide_legend(title="Run Number"))
 outFile <- paste(normalizePath(dirname(args[1])), "plotOverallPercentages.jpg", sep="/")
 ggsave(outFile)
 #Generate second grouped and colored bar plot
+plotTitle <- normalizePath(basename(args[2]))
 plotConc <- ggplot(counts, aes(factor(fullsetNames), aStats.concordant, fill=aStats.run)) + 
   geom_bar(stat="identity", position="stack") + 
+  ggtitle(plotTitle) +
   xlab("Sample Number") +
   ylab("Concordant Percent") +
   scale_fill_brewer(palette="Set1")
