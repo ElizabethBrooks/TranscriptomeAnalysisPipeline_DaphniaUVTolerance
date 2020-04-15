335198aa71aed474c1b676b5941883f29e3c8a19
Mae <merimaat1@gmail.com>
2020-04-15 17:41:15 -0400

    Updated blastp database path

--- Pipeline/decoding_transdecoder.sh
+++ Pipeline/decoding_transdecoder.sh
@@ -21,24 +21,36 @@ if [ $# -eq 0 ]; then
    	exit 1
 fi
 #Determine if the folder name was input in the correct format
-if [[ "$1" == *\/* ]] || [[ "$1" == *\\* ]]; then
+if [[ "$2" == *\/* ]] || [[ "$2" == *\\* ]]; then
 	echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
 	exit 1
 fi
 #Determine if the correct analysis folder was input
-if [[ "$1"  != *assemblyTrinity ]]; then
-	echo "ERROR: The "$1" folder of trimmed fq.gz files were not found... exiting"
+if [[ "$2"  != *assemblyTrinity ]]; then
+	echo "ERROR: The "$2" folder of trimmed fq.gz files were not found... exiting"
 	exit 1
 fi
-#Retrieve genome reference and features paths
+#Determine input database for blastp
+if [[ "$1" == "ncbi" ]]; then
+	#Set slected database to ncbi
+	blastpPath=$(grep "ncbiDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/ncbiDB://g")
+elif [[ "$1" == "uniprot" ]]; then
+	#Set slected database to uniprot
+	blastpPath=$(grep "uniprotDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/uniprotDB://g")
+else
+	#Error message
+	echo "Invalid database selection entered (ncbi or uniprot only)... exiting!"
+	exit 1
+fi
+#Retrieve input assembly path
 inputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
-uniprotPath=$(grep "uniprotDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/uniprotDB://g")
+#Retrieve genome reference and features paths
 pfamPath=$(grep "pfamDB:" ../InputData/inputPaths.txt | tr -d " " | sed "s/pfamDB://g")
-multiFASTA=$(echo "$inputsPath"/"$1"/Trinity*.fasta)
-geneMap="$inputsPath"/"$1"/Trinity.fasta.gene_trans_map
+multiFASTA=$(echo "$inputsPath"/"$2"/Trinity*.fasta)
+geneMap="$inputsPath"/"$2"/Trinity.fasta.gene_trans_map
 #Retrieve outputs absolute path
 outputsPath=$(grep "decoding:" ../InputData/outputPaths.txt | tr -d " " | sed "s/decoding://g")
-outputFolder="$outputsPath"/decodedTransdecoder_"$1"
+outputFolder="$outputsPath"/decodedTransdecoder_"$2"
 mkdir "$outputFolder"
 #Check if the folder already exists
 if [ $? -ne 0 ]; then
@@ -54,7 +66,7 @@ echo "Beginning decoding..."
 #Generate candidate ORFs
 TransDecoder.LongOrfs -t "$multiFASTA" --gene_trans_map "$geneMap"
 #Use BlastP to search a protein database
-blastp -query Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$uniprotPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastp.outfmt6
+blastp -query Trinity.fasta.transdecoder_dir/longest_orfs.pep -db "$blastpPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastp.outfmt6
 #Search the peptides for protein domains using Pfam
 hmmscan --cpu 8 --domtblout pfam.domtblout "$pfamPath" Trinity.fasta.transdecoder_dir/longest_orfs.pep
 #Combine the Blast and Pfam search results into coding region selection
@@ -62,7 +74,7 @@ TransDecoder.Predict -t "$multiFASTA" --retain_pfam_hits pfam.domtblout --retain
 echo "Decoding finished!"
 #Output run commands to summary file
 echo "TransDecoder.LongOrfs -t" "$multiFASTA" "--gene_trans_map" "$geneMap" > "$inputOutFile"
-echo "blastp -query" "Trinity.fasta.transdecoder_dir/longest_orfs.pep -db" "$uniprotPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 >" "blastp.outfmt6" >> "$inputOutFile"
+echo "blastp -query" "Trinity.fasta.transdecoder_dir/longest_orfs.pep -db" "$blastpPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 >" "blastp.outfmt6" >> "$inputOutFile"
 echo "hmmscan --cpu 8 --domtblout" "pfam.domtblout" "$pfamPath" "Trinity.fasta.transdecoder_dir/longest_orfs.pep" >> "$inputOutFile"
 echo "TransDecoder.Predict -t" "$multiFASTA" "--retain_pfam_hits pfam.domtblout --retain_blastp_hits blastp.outfmt6" >> "$inputOutFile"
 #Copy previous summaries
