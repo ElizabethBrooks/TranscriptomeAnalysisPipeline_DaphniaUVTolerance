8ec10cd2964095fabc6ea54f80ad83cd26ebd4a9
Mae <merimaat1@gmail.com>
2020-04-23 01:50:53 -0400

    Fixed typos

--- Pipeline/searchProteins_blastx.sh
+++ Pipeline/searchProteins_blastx.sh
@@ -2,7 +2,7 @@
 #$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -r n
-#$ -N decoding_transdecoder_jobOutput
+#$ -N searchProteins_blastx_jobOutput
 #$ -pe smp 8
 #Script to use blastx to translate the nucleotide sequences of a reference genome
 # for searching a protein database
@@ -45,9 +45,7 @@ cd "$outputFolder"
 inputOutFile="$outputFolder"/"$1"_searchedProteins_summary.txt
 #Use blastx to search a protein database
 echo "Beginning blastx protein database search..."
-blastx -query "$searchFile" -db "$dbPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastx.outfmt6
+blastx -query "$inputsPath" -db "$dbPath"  -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 > blastx.outfmt6
 echo "Finished blastx protein database search!"
 #Output run commands to summary file
-echo "blastx -query" "$searchFile" "-db" "$dbPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 >" "blastx.outfmt6" >> "$inputOutFile"
-#Copy previous summaries
-cp "$inputsPath"/"$1"/*.txt "$outputFolder"
+echo "blastx -query" "$inputsPath" "-db" "$dbPath"  "-max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 8 >" "blastx.outfmt6" >> "$inputOutFile"
