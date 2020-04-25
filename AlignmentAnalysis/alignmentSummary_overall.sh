b98140de6e043feb4cefabdb832809a4806127ad
Mae <merimaat1@gmail.com>
2020-04-24 23:53:38 -0400

    Adding assembly alignment summarization

--- AlignmentAnalysis/alignmentSummary_overall.sh
+++ AlignmentAnalysis/alignmentSummary_overall.sh
@@ -8,8 +8,35 @@ if [ $# -eq 0 ]; then
    	echo "ERROR: No folder name(s) supplied... exiting"
    	exit 1
 fi
+#Determine which analysis folder was input
+if [[ "$1"  == *assembly* ]]; then
+	analysisInput="assembly"
+	#Retrieve reads input absolute path
+	assemblyPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
+	#Retrieve build transcriptome files absolute path
+	buildInputsPath="$assemblyPath"/"$1"
+	#Retrieve transcriptome reference absolute path for alignment
+	buildFile="$assemblyPath"/"$1"/"Trinity.fasta"
+	#Retrieve alignment outputs absolute path
+	outputsPath="$assemblyPath"/"$1"
+	#Retrieve trimmed run folder name used for assembly
+	assemblyFolder=$(echo $1 | sed 's/trimmed_run.//')
+	trimmedFolder=$(echo $1 | sed "s/$assemblyFolder//")
+elif [[ "$1"  == trimmed* ]]; then
+	analysisInput="trimmed"
+	#Retrieve build genome files absolute path
+	buildInputsPath=$(grep "buildingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/building://g")
+	#Retrieve genome reference absolute path for alignment
+	buildFile=$(grep "genomeReference:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeReference://g")
+	#Retrieve alignment outputs absolute path
+	outputsPath=$(grep "aligningGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligning://g")
+	trimmedFolder="$1"
+else
+	echo "ERROR: The input folder of trimmed or assembled files were not found... exiting"
+	exit 1
+fi
 #Retrieve input alignment summary absolute path
-inputsPath=$(grep "aligning:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligning://g")
+#inputsPath=$(grep "aligning:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligning://g")
 #Retrieve alignment analysis outputs absolute path
 outputsPath=$(grep "alignmentAnalysis:" ../InputData/outputPaths.txt | tr -d " " | sed "s/alignmentAnalysis://g")
 #Set outputs directory
