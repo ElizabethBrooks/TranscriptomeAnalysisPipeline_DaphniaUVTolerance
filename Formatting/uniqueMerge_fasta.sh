194db992bb754ad12653599cfab7afdd3f5bfcda
Mae <merimaat1@gmail.com>
2020-05-06 16:11:10 -0400

    Added script for unique merging of fasta files

---
+++ Formatting/uniqueMerge_fasta.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N uniqueMerge_fasta_jobOutput
+#$ -pe smp 8
+#Script to perform merge multifasta files and retain only
+#the specified unique data (by sequence, ID, or both)
+#Usage: qsub uniqueMerge_fasta.sh sortedFolder genotype mergeBy
+#Usage Ex: qsub uniqueMerge_fasta.sh sortedCoordinate_samtoolsHisat2_run1 Sierra sequence
+
+#Required modules for ND CRC servers
+module load bio/2.0
+module load bio/samtools
+#Check for input arguments of folder names
+if [ $# -eq 0 ]; then
+   	echo "No folder name(s) supplied... exiting"
+   	exit 1
+fi
+#Determine if the folder name was input in the correct format
+if [[ "$1" == *\/* ]] || [[ "$1" == *\\* ]]; then
+	echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
+	exit 1
+fi
+#Determine if the correct analysis folder was input
+if [[ "$1"  != sortedCoordinate* ]]; then
+	echo "ERROR: The "$1" folder of aligned bam files were not found... exiting"
+	exit 1
+fi
+#Determine what analysis method was used for the input folder of data
+if [[ "$1" == *"Hisat2"*  ]]; then
+	#Set analysis method for folder naming
+	analysisMethod="Hisat2"
+elif [[ "$1" == *"Tophat2"* ]]; then
+	#Set analysis method for folder naming
+	analysisMethod="Tophat2"
+else
+	echo "ERROR: The sorted "$1" folder of bam files were not found... exiting"
+	exit 1
+fi
+#Retrieve aligned reads input absolute path
+inputsPath=$(grep "sorting:" ../InputData/outputPaths.txt | tr -d " " | sed "s/sorting://g")
+#Retrieve genome reference absolute path for alignment
+genomeFile=$(grep "genomeReference:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeReference://g")
+#Retrieve assembly outputs absolute path
+outputsPath=$(grep "assemblingWithGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingWithGenome://g")
+#Create output directory
+outputFolder="$outputsPath"/"$1""$2"_assemblyGenomeTrinity
+mkdir "$outputFolder"
+#Check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $outputFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
+#Move to outputs directory
+cd "$outputFolder"
+#Name output file of inputs
+inputOutFile="$outputFolder"/"$1""$2"_assemblyGenomeTrinity_summary.txt
+#Merge and re-coordinate sort the set of bam files
+readFiles=$(echo "$inputsPath"/"$1"/*_"$2"_*/*.bam)
+echo "Beginning merging..."
+samtools merge -@ 8 merged.bam $readFiles
+echo "Merging complete! Beginning sorting..."
+samtools sort -@ 8 -o sorted.bam merged.bam
+echo "Sorting complete!"
+rm merged.bam
