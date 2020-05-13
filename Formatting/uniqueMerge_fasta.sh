ae9bba0e8165b1d28657f74216dffdb482ef672d
Mae <merimaat1@gmail.com>
2020-05-13 02:13:03 -0400

    Full testing unique fasta merging

--- Formatting/uniqueMerge_fasta.sh
+++ Formatting/uniqueMerge_fasta.sh
@@ -34,8 +34,6 @@ for i in "$@"; do
 	fi
 	counter=$(($counter+1))
 done
-echo "Fastas : $fastaList"
-echo "Genotypes : $genotypeList"
 #Create output directory
 outputFolder="$outputsPath"
 #Move to outputs directory
@@ -43,28 +41,28 @@ cd "$outputFolder"
 #Name output file and folder
 multiFastaFile="$outputFolder/$2$genotypeList""_assemblyGenomeTrinity_multiFasta.fasta"
 #Merge the set of fasta files
-#echo "Beginning merging..."
+echo "Beginning merging..."
 #Determine which method to merge fasta files by
-#if [[ "$1" == sequence ]]; then
+if [[ "$1" == sequence ]]; then
 	#Sequence identical merge
-	#awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		#(FNR==1){next}
-		#{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		#!(seen[seq]++){ print ">" $0 }' $fastaList > $multiFastaFile
-#elif [[ "$1" == sequenceName ]]; then
+	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+		(FNR==1){next}
+		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+		!(seen[seq]++){ print ">" $0 }' $fastaList > $multiFastaFile
+elif [[ "$1" == sequenceName ]]; then
 	#First part of sequence name identical merge
-	#awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		#(FNR==1){next}
-		#{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		#{ key=substr(name,1,index(s,"|")) }
-		#!(seen[key]++){ print ">" $0 }' $fastaList > $multiFastaFile
-#elif [[ "$1" == sequenceAndName ]]; then
+	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+		(FNR==1){next}
+		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+		{ key=substr(name,1,index(s,"|")) }
+		!(seen[key]++){ print ">" $0 }' $fastaList > $multiFastaFile
+elif [[ "$1" == sequenceAndName ]]; then
 	#Sequence name and sequence identical merge
-	#awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		#(FNR==1){next}
-		#{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		#!(seen[name,seq]++){ print ">" $0 }' $fastaList > $multiFastaFile
-#else
-	#echo "Selected merge format for fasta files not valid... exiting!"
-	#exit 1
-#fi
+	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+		(FNR==1){next}
+		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+		!(seen[name,seq]++){ print ">" $0 }' $fastaList > $multiFastaFile
+else
+	echo "Selected merge format for fasta files not valid... exiting!"
+	exit 1
+fi
