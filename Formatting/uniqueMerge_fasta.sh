df142d96cf3d464b7792b7eac69396f1cb96eff0
Mae <merimaat1@gmail.com>
2020-05-13 03:49:33 -0400

    Testing unique fasta merging

--- Formatting/uniqueMerge_fasta.sh
+++ Formatting/uniqueMerge_fasta.sh
@@ -37,31 +37,33 @@ done
 #Create output directory
 outputFolder="$outputsPath"
 multiFastaFile="$outputFolder/$2$genotypeList""_assemblyGenomeTrinity_multiFasta.fasta"
+echo "$multiFastaFile"
 #Move to outputs directory
 cd "$outputFolder"
 #Merge the set of fasta files
-echo "Beginning merging..."
+echo "Beginning fasta file merging..."
 #Determine which method to merge fasta files by
-if [[ "$1" == sequence ]]; then
+#if [[ "$1" == sequence ]]; then
 	#Sequence identical merge
-	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		(FNR==1){next}
-		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		!(seen[seq]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
-elif [[ "$1" == sequenceName ]]; then
+#	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+#		(FNR==1){next}
+#		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+#		!(seen[seq]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
+#elif [[ "$1" == sequenceName ]]; then
 	#First part of sequence name identical merge
-	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		(FNR==1){next}
-		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		{ key=substr(name,1,index(s,"|")) }
-		!(seen[key]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
-elif [[ "$1" == sequenceAndName ]]; then
+#	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+#		(FNR==1){next}
+#		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+#		{ key=substr(name,1,index(s,"|")) }
+#		!(seen[key]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
+#elif [[ "$1" == sequenceAndName ]]; then
 	#Sequence name and sequence identical merge
-	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
-		(FNR==1){next}
-		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
-		!(seen[name,seq]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
-else
-	echo "Selected merge format for fasta files not valid... exiting!"
-	exit 1
-fi
+#	awk 'BEGIN{RS=">"; FS="\n"; ORS=""}
+#		(FNR==1){next}
+#		{ name=$1; seq=$0; gsub(/(^[^\n]*|)\n/,"",seq) }
+#		!(seen[name,seq]++){ print ">$0" >> "$multiFastaFile" }' $fastaList
+#else
+#	echo "Selected merge format for fasta files not valid... exiting!"
+#	exit 1
+#fi
+echo "Fasta file merging complete!"
