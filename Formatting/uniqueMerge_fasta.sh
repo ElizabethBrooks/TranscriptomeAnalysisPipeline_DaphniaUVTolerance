c97a68352b7b00454f499ac51e2f85bfb036d47e
Mae <merimaat1@gmail.com>
2020-05-13 03:58:22 -0400

    Fixed file nameing for fasta merging

--- Formatting/uniqueMerge_fasta.sh
+++ Formatting/uniqueMerge_fasta.sh
@@ -30,7 +30,7 @@ for i in "$@"; do
 		genotypeList="$genotypeList$i"
 	elif [[ $counter -ge 3 ]]; then
 		fastaList="$fastaList$inputsPath/$2$i""_assemblyGenomeTrinity/Trinity.fasta "
-		genotypeList="$genotypeList$i_"
+		genotypeList="$genotypeList$i""_"
 	fi
 	counter=$(($counter+1))
 done
