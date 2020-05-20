63ceb6d318446d0ec061432531581e219356a075
Mae <merimaat1@gmail.com>
2020-05-20 00:13:03 -0400

    Testing on servers

--- AlignmentAnalysis/generateBarPlotsDriver_binnedCompare.sh
+++ AlignmentAnalysis/generateBarPlotsDriver_binnedCompare.sh
@@ -6,7 +6,7 @@
 #Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh alignmentSummarized_sortedCoordinate_samtoolsHisat2_run1E05_assemblyGenomeTrinity_hisat2_run1 alignmentSummarized_hisat2_run1
 
 #Load module necessary for crc servers
-#module load bio/R/
+module load bio/R/
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
    	echo "ERROR: No folder name(s) supplied... exiting"
@@ -20,25 +20,22 @@ echo "Plotting $1 and $2 alignment summaries..."
 #Determine what software was used for inputs
 if [[ "$1" == *"Trinity"* && "$2" != *"Trinity"* ]]; then
 	#Plot alignment data using target (assembly vs aligned) binned bar plots
-	echo "Target binned alignment summaries for $1 and $2 have been plotted!"
 	Rscript alignmentSummary_barPlot_binnedCompareTarget.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
 	Rscript alignmentSummary_barPlotMedians_binnedCompareTarget.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
 elif [[ "$1" == *"tophat2"* && "$2" == *"tophat2"* ]]; then
 	#Plot alignment data using run binned bar plots
-	echo "Run binned alignment summaries for $1 and $2 have been plotted!"
 	Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
 	Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
 elif [[ "$1" == *"hisat2"* && "$2" == *"hisat2"* ]]; then
 	#Plot alignment data using run binned bar plots
-	echo "Run binned alignment summaries for $1 and $2 have been plotted!"
 	Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
 	Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
 else
 	#Plot alignment data using software binned bar plots
-	echo "Software binned alignment summaries for $1 and $2 have been plotted!"
 	Rscript alignmentSummary_barPlot_binnedCompareSoftware.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
 	Rscript alignmentSummary_barPlotMedians_binnedCompareSoftware.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
 fi
+echo "Software binned alignment summaries for $1 and $2 have been plotted!"
 #Rename produced pdf of plots
 outFile1=$(echo "$1" | sed 's/alignmentSummarized_//')
 outFile2=$(echo "$2" | sed 's/alignmentSummarized_//')
