aa6859a061e58760969dbde4ce93cde1a0a077ba
Mae <merimaat1@gmail.com>
2020-05-20 05:10:00 -0400

    Updated bar plot driver

--- AlignmentAnalysis/alignmentSummary_barPlotMedians_binnedCompareGenotype.r
+++ AlignmentAnalysis/alignmentSummary_barPlotMedians_binnedCompareGenotype.r
@@ -12,11 +12,6 @@ library(ggplot2)
 library(stringr)
 #Retrieve input file name of gene counts
 args=commandArgs(trailingOnly=TRUE)
-numArgs=length(args)
-#Test if there is one input argument
-if (length(args)!=2) {
-  stop("Two file names must be supplied.n", call.=FALSE)
-}
 #Retrieve alignment stats
 aStats <- do.call(rbind,lapply(args,read.csv))
 #Create data frame of compined alignment stats
@@ -24,25 +19,19 @@ counts <- data.frame(aStats$genotype, aStats$overallMedian, aStats$concordantMed
 #Create matrix for multiple plots
 par(mfrow=c(2,1))
 #Set the plot titles
-plotTitle1 <- basename(args[1])
-plotTitle1 <- str_remove(plotTitle1, "alignmentSummarized_")
-plotTitle1 <- str_remove(plotTitle1, "_medians.csv")
-plotTitle2 <- basename(args[2])
-plotTitle2 <- str_remove(plotTitle2, "alignmentSummarized_")
-plotTitle2 <- str_remove(plotTitle2, "_medians.csv")
-plotTitle <- paste(plotTitle1, plotTitle2, sep=" vs ")
+plotTitle <- "Comparison of Alignments to Genotypes"
 #Generate grouped and colored bar plot
 plotOverall <- ggplot(counts, aes(x=aStats.genotype, y=aStats.overallMedian, fill=aStats.targetGenotype)) +
   geom_bar(stat="identity", position="dodge") +
   geom_errorbar(aes(x=aStats.genotype, ymin=aStats.overallMedian-aStats.overallSd, ymax=aStats.overallMedian+aStats.overallSd), width=.2, position=position_dodge(.9)) +
   ggtitle(plotTitle) +
   xlab("Genotype") +
-  ylab("Overall Median Percent") +
+  ylab("Median Overall Percent") +
   theme(plot.title = element_text(hjust = 0.5)) +
   scale_fill_brewer(palette="Set1")
 plotOverall <- plotOverall + guides(fill=guide_legend(title="Target Genotype"))
 #Save overall percentages plot as a jpg
-outFile <- paste(normalizePath(dirname(args[1])), "plotOverallMedianPercentages.jpg", sep="/")
+outFile <- paste(normalizePath(dirname(args[1])), "plotMedianOverallPercentages.jpg", sep="/")
 ggsave(outFile)
 #Generate second grouped and colored bar plot
 plotConc <- ggplot(counts, aes(x=aStats.genotype, y=aStats.concordantMedian, fill=aStats.targetGenotype)) +
@@ -50,10 +39,10 @@ plotConc <- ggplot(counts, aes(x=aStats.genotype, y=aStats.concordantMedian, fil
   geom_errorbar(aes(x=aStats.genotype, ymin=aStats.concordantMedian-aStats.concordantSd, ymax=aStats.concordantMedian+aStats.concordantSd), width=.2, position=position_dodge(.9)) +
   ggtitle(plotTitle) +
   xlab("Genotype") +
-  ylab("Overall Median Percent") +
+  ylab("Median Concordant Percent") +
   theme(plot.title = element_text(hjust = 0.5)) +
   scale_fill_brewer(palette="Set1")
 plotConc <- plotConc + guides(fill=guide_legend(title="Target Genotype"))
 #Save concordant percentages plot as a jpg
-outFile <- paste(normalizePath(dirname(args[1])), "plotConcordantMedianPercentages.jpg", sep="/")
+outFile <- paste(normalizePath(dirname(args[1])), "plotMedianConcordantPercentages.jpg", sep="/")
 ggsave(outFile)
--- AlignmentAnalysis/generateBarPlotsDriver_binnedCompare.sh
+++ AlignmentAnalysis/generateBarPlotsDriver_binnedCompare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Script to generate venn diagrams from edgeR stats
 #Usage: bash generateBarPlotsDriver_binnedCompare.sh twoAlingmentSummaryFiles 
-#Usage Ex: bash generateBarPlotsDriver_binnedCompare.sh alignmentSummarized_hisat2_run1 alignmentSummarized_tophat2_run2
-#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh alignmentSummarized_trimmed_run1E05_hisat2_run1 alignmentSummarized_hisat2_run1
-#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh alignmentSummarized_sortedCoordinate_samtoolsHisat2_run1E05_hisat2_run1 alignmentSummarized_hisat2_run1
-#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh alignmentSummarized_sortedCoordinate_samtoolsHisat2_run1 hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
-
+#Usage Ex: bash generateBarPlotsDriver_binnedCompare.sh hisat2_run1 tophat2_run2
+#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh trimmed_run1E05_hisat2_run1 hisat2_run1
+#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh sortedCoordinate_samtoolsHisat2_run1E05_hisat2_run1 hisat2_run1
+#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh PA42_hisat2_run1 sortedCoordinate_samtoolsHisat2_run1 hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
+#Alternate usage Ex: bash generateBarPlotsDriver_binnedCompare.sh sortedCoordinate_samtoolsHisat2_run1 hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
 
 #Load module necessary for crc servers
 #module load R
@@ -23,49 +23,64 @@ echo "Plotting alignment summaries..."
 counter=1
 fileList=""
 if [ $# -ge 3 ]; then
-	#Generate file list
-	for i in "$@"; do
-		#Skip first two arguments
-		if [ $counter -ge 3 ]; then
-			currFile="$1"_"$i"_"$2"_medians.csv
-			fileList="$fileList$inOutDir/$currFile "
-			printList="$fileList$currFile "
-			#Plot alignment data using software binned bar plots
-			Rscript alignmentSummary_barPlotMedians_binnedCompareGenotype.r $fileList
-		fi
-		counter=$((counter+1))
-	done
+	#Determine comparison to be performed
+	if [[ "$1" == PA42* ]]; then
+		#Generate file list
+		for i in "$@"; do
+			#Skip first 3 arguments
+			if [ $counter -ge 4 ]; then
+				currFile=alignmentSummarized_"$2$i"_"$3"_medians.csv
+				fileList="$fileList$inOutDir/$currFile "
+				printList="$fileList$currFile "
+			fi
+			counter=$((counter+1))
+		done
+		addAnalysis=$inOutDir/alignmentSummarized_"$1"_medians.csv
+		fileList="$fileList$addAnalysis"
+		outFile="$1"_vs_"$2"TargetGenotypes_"$3"
+	else
+		#Generate file list
+		for i in "$@"; do
+			#Skip first 2 arguments
+			if [ $counter -ge 3 ]; then
+				currFile=alignmentSummarized_"$1$i"_"$2"_medians.csv
+				fileList="$fileList$inOutDir/$currFile "
+				printList="$fileList$currFile "
+			fi
+			counter=$((counter+1))
+		done
+		outFile="$1"TargetGenotypes_"$2"
+	fi
+	#Plot alignment data using software binned bar plots
+	Rscript alignmentSummary_barPlotMedians_binnedCompareGenotype.r $fileList
 	#Rename produced pdf of plots
-	outFile="$1"_targetGenotypes
-	mv "$inOutDir"/plotOverallMedianPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_overallMedianPercentages.jpg
-	mv "$inOutDir"/plotConcordantMedianPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_concordantMedianPercentages.jpg
+	mv "$inOutDir"/plotMedianOverallPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_medianOverallPercentages.jpg
+	mv "$inOutDir"/plotMedianConcordantPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_medianConcordantPercentages.jpg
 else
-	printList="$1" "$2"
+	printList="alignmentSummarized_"$1" alignmentSummarized_"$2
 	#Determine what software was used for inputs
 	if [[ "$1" == *"Trinity"* && "$2" != *"Trinity"* ]]; then
 		#Plot alignment data using target (assembly vs aligned) binned bar plots
-		Rscript alignmentSummary_barPlot_binnedCompareTarget.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
-		Rscript alignmentSummary_barPlotMedians_binnedCompareTarget.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
+		Rscript alignmentSummary_barPlot_binnedCompareTarget.r "$inOutDir"/alignmentSummarized_"$1"_formatted.csv "$inOutDir"/alignmentSummarized_"$2"_formatted.csv
+		Rscript alignmentSummary_barPlotMedians_binnedCompareTarget.r "$inOutDir"/alignmentSummarized_"$1"_medians.csv "$inOutDir"/alignmentSummarized_"$2"_medians.csv
 	elif [[ "$1" == *"tophat2"* && "$2" == *"tophat2"* ]]; then
 		#Plot alignment data using run binned bar plots
-		Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
-		Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
+		Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/alignmentSummarized_"$1"_formatted.csv "$inOutDir"/alignmentSummarized_"$2"_formatted.csv
+		Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/alignmentSummarized_"$1"_medians.csv "$inOutDir"/alignmentSummarized_"$2"_medians.csv
 	elif [[ "$1" == *"hisat2"* && "$2" == *"hisat2"* ]]; then
 		#Plot alignment data using run binned bar plots
-		Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
-		Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
+		Rscript alignmentSummary_barPlot_binnedCompareRun.r "$inOutDir"/alignmentSummarized_"$1"_formatted.csv "$inOutDir"/alignmentSummarized_"$2"_formatted.csv
+		Rscript alignmentSummary_barPlotMedians_binnedCompareRun.r "$inOutDir"/alignmentSummarized_"$1"_medians.csv "$inOutDir"/alignmentSummarized_"$2"_medians.csv
 	else
 		#Plot alignment data using software binned bar plots
-		Rscript alignmentSummary_barPlot_binnedCompareSoftware.r "$inOutDir"/"$1"_formatted.csv "$inOutDir"/"$2"_formatted.csv
-		Rscript alignmentSummary_barPlotMedians_binnedCompareSoftware.r "$inOutDir"/"$1"_medians.csv "$inOutDir"/"$2"_medians.csv
+		Rscript alignmentSummary_barPlot_binnedCompareSoftware.r "$inOutDir"/alignmentSummarized_"$1"_formatted.csv "$inOutDir"/alignmentSummarized_"$2"_formatted.csv
+		Rscript alignmentSummary_barPlotMedians_binnedCompareSoftware.r "$inOutDir"/alignmentSummarized_"$1"_medians.csv "$inOutDir"/alignmentSummarized_"$2"_medians.csv
 	fi
 	echo "Software binned alignment summaries for $printList have been plotted!"
 	#Rename produced pdf of plots
-	outFile1=$(echo "$1" | sed 's/alignmentSummarized_//')
-	outFile2=$(echo "$2" | sed 's/alignmentSummarized_//')
-	outFile="$outFile1"_"$outFile2"
+	outFile="$1"_"$2"
 	mv "$inOutDir"/plotOverallPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_overallPercentages.jpg
 	mv "$inOutDir"/plotConcordantPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_concordantPercentages.jpg
-	mv "$inOutDir"/plotOverallMedianPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_overallMedianPercentages.jpg
-	mv "$inOutDir"/plotConcordantMedianPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_concordantMedianPercentages.jpg
+	mv "$inOutDir"/plotMedianOverallPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_medianOverallPercentages.jpg
+	mv "$inOutDir"/plotMedianConcordantPercentages.jpg "$inOutDir"/alignmentSummarized_"$outFile"_medianConcordantPercentages.jpg
 fi
