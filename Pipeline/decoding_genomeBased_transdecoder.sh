fe55480b5c692533a22a345375fca4acae4dbb9c
Mae <merimaat1@gmail.com>
2020-05-20 22:47:47 -0400

    Updated testing genome based transdecoder

--- Pipeline/decoding_genomeBased_transdecoder.sh
+++ Pipeline/decoding_genomeBased_transdecoder.sh
@@ -5,8 +5,8 @@
 #$ -N decoding_genomeBased_transdecoder_jobOutput
 #Script to predict coding regions from a transcript fasta file
 # using Transdecoder with genome reference and features files
-#Usage: qsub decoding_genomeBased_transdecoder.sh alignedSequencesFolder genomeVersion
-#Usage Ex: qsub decoding_genomeBased_transdecoder.sh aligned_tophat2_run2 PA42_v3.0
+#Usage: qsub decoding_genomeBased_transdecoder.sh alignedSequencesFolder
+#Usage Ex: qsub decoding_genomeBased_transdecoder.sh aligned_tophat2_run2
 
 #Load necessary modules for ND CRC servers
 module load bio/2.0
@@ -18,12 +18,17 @@ multiFASTA=$(grep "genomeReference:" ../InputData/inputPaths.txt | tr -d " " | s
 softPath=$(grep "transdecoder:" ../InputData/inputPaths.txt | tr -d " " | sed "s/transdecoder://g")
 #Retrieve outputs absolute path
 inOutputsPath=$(grep "aligning:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligning://g")
-outFolder="$inOutputsPath"/"$2"/decoded_transdecoder_genomeBased
-mkdir "$outFolder"
+outputFolder="$inOutputsPath"/"$1"/decoded_transdecoder_genomeBased
+mkdir "$outputFolder"
+#Check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $outputFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
 #Move to output folder
-cd "$outFolder"
+cd "$outputFolder"
 #Clean up genome features file
-#sed -e "s/\r//g" "$genomeFeat" > "$outFolder"/genomeFeat_"$2".gff
+#sed -e "s/\r//g" "$genomeFeat" > "$outputFolder"/genomeFeat_"$2".gff
 #Generate your best candidate open rading frame (ORF) predictions
 TransDecoder.LongOrfs -t "$multiFASTA"
 #Optionally, identify peptides with homology to known proteins
