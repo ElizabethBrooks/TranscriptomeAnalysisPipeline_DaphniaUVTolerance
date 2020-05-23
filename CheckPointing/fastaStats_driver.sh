a3db86b7a1dc781b1da4c643d62f21eac36ffedf
Mae <merimaat1@gmail.com>
2020-05-23 16:52:38 -0400

    Testing stats driver

--- CheckPointing/fastaStats_driver.sh
+++ CheckPointing/fastaStats_driver.sh
@@ -26,78 +26,64 @@ inputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "
 #Determine assembly target
 if [[ "$1" == sorted* ]]; then
 	#Set output directory
-	outputFolder="$inputsPath/$1""_assemblyGenomeTrinity_mergedFasta"
+	outputFolder="$inputsPath"/"$1"_assemblyGenomeTrinity_mergedFasta
 	#Set merged fasta file name
-	mergedFastaFile="$outputFolder/assemblyGenomeTrinity_mergedFasta.fasta"
-	summaryFile="$outputFolder/$1""_assemblyGenomeTrinity_mergedFasta_summary.txt"
+	mergedFastaFile="$outputFolder"/assemblyGenomeTrinity_mergedFasta.fasta
+	summaryFile="$outputFolder"/"$1"_assemblyGenomeTrinity_mergedFasta_summary.txt
 	#Retrieve selected fasta files
 	#Loop through all input genotypes and merge fasta files
 	for i in "$@"; do
 		#Skip first two arguments
 		if [[ $counter -eq $# ]]; then
 			#Add fasta file to list
-			fastaFile="$inputsPath/$1$i""_assemblyGenomeTrinity/Trinity.fasta"
+			fastaFile="$inputsPath"/"$1$i"_assemblyGenomeTrinity/Trinity.fasta
 			fastaList="$fastaList$fastaFile"
 		elif [[ $counter -ge 3 ]]; then
 			#Add fasta file to list
-			fastaFile="$inputsPath/$1$i""_assemblyGenomeTrinity/Trinity.fasta "
+			fastaFile="$inputsPath"/"$1$i""_assemblyGenomeTrinity/Trinity.fasta "
 			fastaList="$fastaList$fastaFile "
 		fi
 		counter=$(($counter+1))
 	done
 elif [[ "$1" == trimmed* ]]; then
 	#Set output directory
-	outputFolder="$inputsPath/$1""_assemblyTrinity_mergedFasta"
+	outputFolder="$inputsPath"/"$1"_assemblyTrinity_mergedFasta
 	#Set merged fasta file name
-	mergedFastaFile="$outputFolder/assemblyTrinity_mergedFasta.fasta"
-	summaryFile="$outputFolder/$1""_assemblyTrinity_mergedFasta_summary.txt"
+	mergedFastaFile="$outputFolder"/assemblyTrinity_mergedFasta.fasta
+	summaryFile="$outputFolder"/"$1"_assemblyTrinity_mergedFasta_summary.txt
 	#Retrieve selected fasta files
 	#Loop through all input genotypes and merge fasta files
 	for i in "$@"; do
 		#Skip first two arguments
 		if [[ $counter -eq $# ]]; then
 			#Add fasta file to list
-			fastaFile="$inputsPath/$1$i""_assemblyTrinity/Trinity.fasta"
+			fastaFile="$inputsPath"/"$1$i"_assemblyTrinity/Trinity.fasta
 			fastaList="$fastaList$fastaFile"
 		elif [[ $counter -ge 3 ]]; then
 			#Add fasta file to list
-			fastaFile="$inputsPath/$1$i""_assemblyTrinity/Trinity.fasta "
+			fastaFile="$inputsPath"/"$1$i""_assemblyTrinity/Trinity.fasta "
 			fastaList="$fastaList$fastaFile "
 		fi
 		counter=$(($counter+1))
 	done
-else #Default accept a list of full file paths
-	#Set output directory
-	outputFolder="$inputsPath/$1_mergedFasta"
-	#Set merged fasta file name
-	mergedFastaFile="$outputFolder/$1""_mergedFasta.fasta"
-	summaryFile="$outputFolder/$1""_mergedFasta_summary.txt"
-	#Retrieve selected fasta files
-	#Loop through all input genotypes and merge fasta files
-	for i in "$@"; do
-		#Skip first two arguments
-		if [[ $counter -eq $# ]]; then
-			fastaList="$fastaList$i"
-		elif [[ $counter -ge 3 ]]; then
-			fastaList="$fastaList$i "
-		fi
-		counter=$(($counter+1))
-	done
+else 
+	echo "Invalid $1 entered... exiting!"
+	exit 1
 fi
 
 #Create output directory
 mkdir $outputFolder
 
 #Write list of fasta files to the summary file
-echo "File list:" > $summaryFile
-printf '%s\n' "${fastaList[@]}" >> $summaryFile
+echo "File list:" #> $summaryFile
+printf '%s\n' "${fastaList[@]}" #>> $summaryFile
 
 #Write fasta stats to the summary file
-bash fastaStats.sh $fastaList $mergedFastaFile >> $summaryFile
+#bash fastaStats.sh $fastaList $mergedFastaFile >> $summaryFile
 
 #Write fasta stats to the summary file
-summaryFileCSV=$(echo $summaryFile | sed 's/\.txt/\.csv/g')
-bash fastaStats_csvFormatted.sh $fastaList $mergedFastaFile >> $summaryFileCSV
+#summaryFileCSV=$(echo $summaryFile | sed 's/\.txt/\.csv/g')
+#bash fastaStats_csvFormatted.sh $fastaList $mergedFastaFile >> $summaryFileCSV
 
 #Plot fasta stats from summary file
-Rscript fastaStats_barPlot.r $summaryFileCSV
+#Rscript fastaStats_barPlot.r $summaryFileCSV
