153def60baabae91d506b753dcae6e29ebd9f1ec
Mae <merimaat1@gmail.com>
2020-06-01 19:49:45 -0400

    Updated annotation merging script

--- CheckPointing/mergeAnnotations_GOID.sh
+++ CheckPointing/mergeAnnotations_GOID.sh
@@ -1,31 +1,32 @@
 #Bash script to merge two GO annotation files
 # and remove duplicate entries
-bash mergeAnnotations_GOID.sh /home/mae/Documents/RNASeq_Workshop_ND/PA42_annotations/Frozen 3.0 Genome_Annotaton/PA42_3.0_annotation_go_terms_enzyme_code_by_line_18440.txt /home/mae/Documents/RNASeq_Workshop_ND/PA42_annotations/PANNZER_proteins_new/GO.out.txt
+#bash mergeAnnotations_GOID.sh /home/mae/Documents/RNASeq_Workshop_ND/PA42_annotations/Frozen3.0Genome_Annotaton/PA42_3.0_annotation_go_terms_enzyme_code_by_line_18440.txt /home/mae/Documents/RNASeq_Workshop_ND/PA42_annotations/PANNZER_proteins_new/GO.out.txt
 
 #Merge the input files
 outPath=$(dirname "$2")
-outFile="$outPath"/"GO_merged.txt"
-cat "$1" "$2" > "$outFile"
+outFileMerged="$outPath"/"GO_merged.txt"
+outFileCleaned="$outPath"/"GO_noDuplicates.txt"
+cut -f 1,2 "$1" | sed 's/GO://g' > "$outFileCleaned"
+tail -n +2 "$2" | cut -f 1,3 >> "$outFileCleaned"
 
-#Remove extra tags
-sed -i 's/GO://g' "$outFile"
-tr -s ' ' < "$outFile" 
-tr -s '\t' < "$outFile"
+#Remove extra tabs
+unexpand -a "$outFileCleaned" > "$outFileMerged"
 
 #Sort and remove duplicate lines
-sort "$outFile" | uniq -u
-
-#Sort by gene ID
-sort -k1 -n "$outFile"
+sort "$outFileMerged" | uniq -u > "$outFileCleaned"
 
 #Check number of lines
-echo "Number of lines:"
-wc -l "$1"
-wc -l "$2"
-wc -l "$outFile"
+echo "Number of lines..."
+genes1=$(wc -l "$1" | cut -d ' ' -f 1); echo "File1: $genes1"
+genes2=$(wc -l "$2" | cut -d ' ' -f 1); echo "File2: $genes2"
+genes3=$(wc -l "$outFileMerged" | cut -d ' ' -f 1); echo "Merged: $genes3"
+genes4=$(wc -l "$outFileCleaned" | cut -d ' ' -f 1); echo "No Duplicates: $genes4"
+genes5=$(($genes3-$genes4)); echo "Number of Duplicates: $genes5"
 
 #Check number of uniqe geneIDs with GOIDs
-echo "Number of gene IDs:"
+printf "\nNumber of gene IDs...\n"
 genes1=$(sort -k1 -n "$1" | uniq -u | wc -l); echo "File1: $genes1"
 genes2=$(sort -k1 -n "$2" | uniq -u | wc -l); echo "File2: $genes2"
-genes3=$(sort -k1 -n "$outFile" | uniq -u | wc -l); echo "Merged: $genes3"
+genes3=$(sort -k1 -n "$outFileMerged" | uniq -u | wc -l); echo "Merged: $genes3"
+genes4=$(sort -k1 -n "$outFileCleaned" | uniq -u | wc -l); echo "No Duplicates: $genes4"
+genes5=$(($genes3-$genes4)); echo "Number of Duplicates: $genes5"
