71fe60b5c8e3b6405899f8c2538cf2f6419b6d1b
Mae <merimaat1@gmail.com>
2020-06-03 10:32:57 -0400

    Added search subsetting driver

---
+++ CheckPointing/searchDriver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Script to perform t-test analysis of all samples in an input set
+#Usage: bash searchDriver.sh assembledFolder sampleList
+#Usage Ex: bash searchDriver.sh trimmed_run1 Y05 Y023_5 E05 R2 PA Sierra
+#Check for input arguments of folder names
+if [ $# -eq 0 ]; then
+   	echo "ERROR: No folder name(s) supplied... exiting"
+   	exit 1
+fi
+#Initialize variables
+counter=0
+#Loop through all input sets of treatments and perform t-test analsysis
+for i in "$@"; do
+	#Determine what type of data folder was input
+	if [[ "$1" == trimmed* ]]; then
+		inputFolder=$(echo "$1""$i"_assemblyTrinity)
+	elif [[ "$1" == sorted* ]]; then
+		inputFolder=$(echo "$1""$i"_assemblyGenomeTrinity)
+	else
+		echo "ERROR: Input folder for analysis is not a valid option... exiting!"
+		exit 1
+	fi
+	#Skip first argument
+	if [ $counter -ge 1 ]; then
+		#Usage: qsub search_blastp.sh transcriptomeFasta
+		qsub search_blastp.sh "$inputFolder"
+	fi
+	counter=$(($counter+1))
+done
