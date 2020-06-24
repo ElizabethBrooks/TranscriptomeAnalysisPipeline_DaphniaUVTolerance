df1b395fc3d74dffbd4f191e5248060532b4d137
Mae <merimaat1@gmail.com>
2020-06-24 02:49:08 -0400

    Updated luserting script

--- Filtering/cluster_cdhit.sh
+++ Filtering/cluster_cdhit.sh
@@ -25,10 +25,16 @@ if [[ "$1" == *assembly* ]]; then
 	inputsPath="$assemblyPath"/"$1"
 	#Set outputs absolute path
 	outputFolder="$assemblyPath"/"$1"/clustered_cdhit_"$2"
+	#Set DBs of transcriptome
+	inputNucleotideDB="$inputsPath"/Trinity.fasta
+	inputProteinDB="$inputsPath"/decoded_transdecoder/Trinity.fasta.transdecoder.pep
 elif [[ "$1" == PA42 ]]; then
 	#Retrieve genome reference absolute path for querying
 	inputsPath=$(grep "transcriptomeDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/transcriptomeDB://g")
 	inputsPath=$(dirname "$inputsPath")
+	#Set inputs absolut paths
+	inputNucleotideDB=$(grep "codingSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/codingSequencesDB://g")
+	inputProteinDB=$(grep "transcriptomeDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/transcriptomeDB://g")
 	#Set outputs absolute path
 	outputFolder="$inputsPath"/clustered_cdhit_"$2"
 else
@@ -48,8 +54,8 @@ cd "$outputFolder"
 #Name output file of inputs
 inputOutFile="$outputFolder"/clustered_"$2"_cdhit_"$1"_summary.txt
 #Run cd-hit to cluster proteins
-"$softsPath"/cd-hit -o cdhit -c $2 -i "$inputsPath"/Trinity.fasta.transdecoder.pep -p 1 -n 5 -M 16000 –d 0 -T 8
-echo "$softsPath"/cd-hit -o cdhit -c $2 -i "$inputsPath"/Trinity.fasta.transdecoder.pep -p 1 -n 5 -M 16000 –d 0 -T 8 >> "$inputOutFile"
+"$softsPath"/cd-hit -o cdhit -c $2 -i "$inputProteinDB" -p 1 -n 5 -M 16000 –d 0 -T 8
+echo "$softsPath"/cd-hit -o cdhit -c $2 -i "$inputProteinDB" -p 1 -n 5 -M 16000 –d 0 -T 8 >> "$inputOutFile"
 #Run cd-hit to cluster nucleotides
-"$softsPath"/cd-hit-est -o cdhitEst -c $2 -i "$inputsPath"/Trinity.fasta -p 1 -n 10 -d 0 -M 16000 -T 8
-echo "$softsPath"/cd-hit-est -o cdhitEst -c $2 -i "$inputsPath"/Trinity.fasta -p 1 -n 10 -d 0 -M 16000 -T 8 > "$inputOutFile"
+"$softsPath"/cd-hit-est -o cdhitEst -c $2 -i "$inputNucleotideDB" -p 1 -n 10 -d 0 -M 16000 -T 8
+echo "$softsPath"/cd-hit-est -o cdhitEst -c $2 -i "$inputNucleotideDB" -p 1 -n 10 -d 0 -M 16000 -T 8 > "$inputOutFile"
