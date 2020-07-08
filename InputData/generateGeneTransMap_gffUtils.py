84150f7f0230b3c7b1a58533575bfcb6630b0858
Mae <merimaat1@gmail.com>
2020-07-07 20:34:29 -0400

    Adding gene to trans mapping script

---
+++ InputData/generateGeneTransMap_gffUtils.py
@@ -0,0 +1,21 @@
+library(reticulate)
+
+# check which python I am using
+py_discover_config()
+import sys
+print(sys.version)
+import gffutils
+import itertools
+import os
+os.listdir()
+db = gffutils.create_db("GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.gff.gz", ":memory:", force = True,merge_strategy="merge", id_spec={'gene': 'Dbxref'})
+list(db.featuretypes())
+tx_and_gene=[]
+## instead, loop over all features in the database
+with open("tx2gene_NCBI.tsv", "w") as f:
+        for feature in db.all_features():
+                transcript = feature.attributes.get('transcript_id', [None])[0]
+                gene = feature.attributes.get('gene', [None])[0]
+                if gene and transcript and ([transcript, gene] not in tx_and_gene):
+                        tx_and_gene.append([transcript, gene])
+                        f.write(transcript + "\t" + gene + "\n")
