1103c45542c5597d4464cb5b775ca6aefc13e31b
Mae <merimaat1@gmail.com>
2020-07-14 23:51:45 -0400

    Updating annotation analysis script

--- TranscriptAnnotation/compareAnnotations.sh
+++ TranscriptAnnotation/compareAnnotations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Script to compare annotation files
 
 #Remove header
 #tail -n +2 "$1" > tmpDataIn.csv
@@ -6,13 +7,17 @@
 #Loop over annotations
 while IFS=, read -r f1 f2
 do
-	#Check annotation sets
-	if grep -iF "$f1,$f2" $2; then
+	#Determine annotation sets
+	if grep -iF "$f1,$f2" $2; then #Matching
 		echo "$f1,$f2" >> "$1"_"$2"_matching.csv
-	else
+	else #Unique
 		echo "$f1,$f2" >> "$1"_unique.csv
 	fi
 done < $1
+
+#Output the number of annotations in each subset
+wc -l "$1"_"$2"_matching.csv
+wc -l "$1"_unique.csv
 
 #Clean up
 #rm tmp*.csv
