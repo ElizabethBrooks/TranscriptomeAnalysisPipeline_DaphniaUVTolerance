7436b700e5cde95f5bac6a26d1ccf9ac27358cdc
Mae <merimaat1@gmail.com>
2020-07-23 01:10:38 -0400

    Updated featureCounts scripts

--- GeneCountStatistics/countingDriver_featureCounts.sh
+++ GeneCountStatistics/countingDriver_featureCounts.sh
@@ -5,8 +5,8 @@
 #$ -N counting_featureCounts_jobOutput
 #Script to perform freatureCounts counting of aligned or sorted
 # paired end reads
-#Usage: qsub counting_featureCounts.sh sortedNameFolder
-#Usage Ex: qsub counting_featureCounts.sh sortedName_samtoolsTophat2_run1
+#Usage: qsub counting_featureCounts.sh assemblyFolder sortedNameFolder
+#Usage Ex: qsub counting_featureCounts.sh trimmed_run1E05_assemblyTrinity sortedName_samtoolsTophat2_run1
 
 #Required modules for ND CRC servers
 module load R/3.5.3
@@ -24,62 +24,27 @@ if [[ "$1" == *\/* ]] || [[ "$1" == *\\* ]]; then
 	echo "ERROR: Please enter folder names without a trailing forward slash (/)... exiting"
 	exit 1
 fi
-#Determine if the correct analysis folder was input
-if [[ "$1"  == sortedName* ]]; then
-	#Set name sorted flag (default) with file type flag
-	#flags="-f bam"
-	sortType="Name"
-elif [[ "$1"  == sortedCoordinate* ]]; then
-	#Set coordinate sorted flag with file type flag
-	#flags="-f bam -r pos"
-	sortType="coordinate"
+#Determine input path
+if [[ "$1" == *assembly* ]]; then
+	#Retrieve reads input absolute path
+	outputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
+	outputsPath="$outputsPath"/"$1"/"$2"
 else
-	echo "ERROR: The "$1" folder of name or coordinate sorted files were not found... exiting"
+	#Error message
+	echo "Invalid assembly directory entered... exiting!"
 	exit 1
 fi
-#Determine what analysis method was used for the input folder of data
-if [[ "$1" == *"Hisat2"*  ]]; then
-	#Set analysis method for folder naming
-	analysisMethod="Hisat2"
-elif [[ "$1" == *"Tophat2"* ]]; then
-	#Set analysis method for folder naming
-	analysisMethod="Tophat2"
-else
-	echo "ERROR: The sorted "$1" folder or bam files were not found... exiting"
-	exit 1
-fi
-#Retrieve sorted reads input absolute path
-inputsPath=$(grep "sorting:" ../InputData/outputPaths.txt | tr -d " " | sed "s/sorting://g")
 #Retrieve genome features absolute path for alignment
 genomeFile=$(grep "genomeFeatures:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeFeatures://g")
-#Retrieve alignment outputs absolute path
-outputsPath=$(grep "counting:" ../InputData/outputPaths.txt | tr -d " " | sed "s/counting://g")
-#Prepare for analysis
-dirFlag=0
-runNum=1
-COUNTER=0
-analysisTag=".sorted.bam"
-#Make a new directory for each analysis run
-while [ $dirFlag -eq 0 ]; do
-	outputFolder=counted"$sortType"_featureCounts"$analysisMethod"_run"$runNum"
-	mkdir "$outputFolder"
-	#Check if the folder already exists
-	if [ $? -ne 0 ]; then
-		#Increment the folder name
-		let runNum+=1
-	else
-		#Indicate that the folder was successfully made
-		dirFlag=1
-		echo "Creating folder for run $runNum of featureCounts counting of "$1" data..."
-	fi
-done
 #Move to outputs directory
-cd "$outputsPath"/"$outputFolder"
+outputFolder="$outputsPath"/counted_featureCounts
+mkdir "$outputFolder"
+cd "$outputFolder"
 #Name output file of inputs
-inputOutFile="$outputFolder"/"$outputFolder"_summary.txt
+inputOutFile="$outputFolder"/counted_summary.txt
 #Count reads using featureCounts via an R script
 Rscript generateCounts_featureCounts.r "$inputsPath"/"$1" "$genomeFile"
 #Add run to summary file
 echo Rscript generateCounts_featureCounts.r > "$inputOutFile"
 #Copy previous summaries
-cp "$inputsPath"/"$1"/*.txt "$outputsPath"/"$outputFolder"
+cp "$inputsPath"/"$1"/*.txt "$outputFolder"
