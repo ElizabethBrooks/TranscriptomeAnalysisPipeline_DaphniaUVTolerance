b907b2d7bb46135222d0b075fe330c91bf6c4b5d
Mae <merimaat1@gmail.com>
2020-08-22 04:52:29 -0400

    Updated orthoFinder script and input paths

--- Annotation/search_orthoFinder.sh
+++ Annotation/search_orthoFinder.sh
@@ -7,9 +7,8 @@
 #Script to use OrthoFinder to find orthogroups and orthologs, 
 # infers rooted gene trees for all orthogroups and identifies 
 # all of the gene duplication events in those gene trees
-#Usage: qsub search_orthoFinder.sh proteomeFasta
-#Usage Ex: qsub search_orthoFinder.sh trimmed_run1E05_assemblyTrinity
-#Alternate usage Ex: qsub search_orthoFinder.sh PA42_proteins
+#Usage: qsub search_orthoFinder.sh proteomeFastaList
+#Usage Ex: qsub search_orthoFinder.sh trimmed_run1E05_assemblyTrinity trimmed_run1Y05_assemblyTrinity trimmed_run1R2_assemblyTrinity trimmed_run1Y023_5_assemblyTrinity trimmed_run1PA_assemblyTrinity trimmed_run1Sierra_assemblyTrinity PA42_proteins
 
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -18,37 +17,41 @@ if [ $# -eq 0 ]; then
 fi
 #Set software path
 softwarePath=$(grep "orthoFinder:" ../InputData/softwarePaths.txt | tr -d " " | sed "s/orthoFinder://g")
-#Determine input query transcriptome for orthoFinder
-if [[ "$1" == *assembly* ]]; then
-	#Retrieve reads input absolute path
-	assemblyPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
-	inputsPath="$assemblyPath"/"$1"/decoded_transdecoder/Trinity.fasta.transdecoder.pep
-	#Set outputs absolute path
-	outputFolder="$assemblyPath"/"$1"/searched_orthoFinder_"$2"
-elif [[ "$1" == PA42_proteins ]]; then
-	#Retrieve genome reference absolute path for querying
-	inputsPath=$(grep "proteinSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/proteinSequencesDB://g")
-	#Set outputs absolute path
-	outputPath=$(dirname "$inputsPath")
-	outputFolder="$outputPath"/searched_orthoFinder_"$2"	
-else
-	#Error message
-	echo "Invalid fasta entered (species proteome expected)... exiting!"
-	exit 1
-fi
+#Set outputs absolute path
+outputPath=$(grep "orthoFinder:" ../InputData/outputPaths.txt | tr -d " " | sed "s/orthoFinder://g")
+outputFolder="$outputPath"/searched_orthoFinder
 #Make output directory
 mkdir "$outputFolder"
+#Move to output folder
+cd "$outputFolder"
 #Check if the folder already exists
 if [ $? -ne 0 ]; then
 	echo "The $outputFolder directory already exsists... please remove before proceeding."
 	exit 1
 fi
-#Move to output folder
-cd "$outputFolder"
+#Loop through all input proteomes and build directory of inputs
+inputsDir="InputProteomes"
+for i in "$@"; do
+	#Determine input proteome
+	if [[ "$i" == *assembly* ]]; then
+		#Retrieve reads input absolute path
+		assemblyPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
+		inputsPath="$assemblyPath"/"$i"/decoded_transdecoder/Trinity.fasta.transdecoder.pep
+	elif [[ "$i" == PA42_proteins ]]; then
+		#Retrieve genome reference absolute path for querying
+		inputsPath=$(grep "proteinSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/proteinSequencesDB://g")
+	else
+		#Error message
+		echo "Invalid fasta entered (species proteome expected)... exiting!"
+		exit 1
+	fi
+	cp "$inputsPath" "$outputFolder"/"$inputsDir"
+done
 #Name output file of inputs
 inputOutFile="$outputFolder"/searched_orthoFinder_"$1"_summary.txt
 #Use OrthoFinder to find orthologs
 echo "Beginning OrthoFinder search..."
-"$softwarePath"/orthofinder -f
+"$softwarePath"/orthofinder -f "$outputFolder"/"$inputsDir"
 echo "OrthoFinder search complete!"
 #Output run commands to summary file
+echo "$softwarePath"/"orthofinder -f" "$outputFolder"/"$inputsDir" > "$inputOutFile"
