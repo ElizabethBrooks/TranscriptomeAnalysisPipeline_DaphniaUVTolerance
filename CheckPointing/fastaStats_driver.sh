cf819228d9c8b899314fd4844798b6ad0e55742b
Mae <merimaat1@gmail.com>
2020-09-02 22:58:36 -0400

    Updated fasta checkpoint script

--- CheckPointing/fastaStats_driver.sh
+++ CheckPointing/fastaStats_driver.sh
@@ -5,7 +5,7 @@
 #Usage Ex: bash fastaStats_driver.sh sequenceAssembled sortedCoordinate_samtoolsHisat2_run1 Y05 Y023_5 E05 R2 PA Sierra
 #Usage Ex: bash fastaStats_driver.sh sequenceAssembled sortedCoordinate_samtoolsTophat2_run1 Y05 Y023_5 E05 R2 PA Sierra
 #Usage Ex: bash fastaStats_driver.sh sequenceDecoded trimmed_run1 Y05 Y023_5 E05 R2 PA Sierra
-#Usage Ex: bash fastaStats_driver.sh sequenceDecoded trimmed_run1 Y05 Y023_5 E05 R2 PA Sierra PA42_proteins PA42_cds PA42_transcripts
+#Usage Ex: bash fastaStats_driver.sh sequenceDecoded trimmed_run1 Y05 Y023_5 E05 R2 PA Sierra PA42_proteins
 #Default usage Ex: bash fastaStats_driver.sh sequence assemblyTrinity_all
 
 #Load necessary modules
@@ -132,38 +132,38 @@ else #Default accept a list of full file paths
 	done
 fi
 #Check if the folder already exists
-#mkdir "$outputFolder"
-#if [ $? -ne 0 ]; then
-#	echo "The $outputFolder directory already exsists... please remove before proceeding."
-#	exit 1
-#fi
+mkdir "$outputFolder"
+if [ $? -ne 0 ]; then
+	echo "The $outputFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
 
 #Merge the set of fasta files
-#echo "Beginning fasta file merging..."
-#bash fastaMerge.sh $1 $mergedFastaFile $fastaList
+echo "Beginning fasta file merging..."
+bash fastaMerge.sh $1 $mergedFastaFile $fastaList
 
 #Write fasta stats to the summary file
-#echo "Beginning file statistics summarizing..."
-#bash fastaStats.sh $mergedFastaFile $fastaList > $summaryFile
+echo "Beginning file statistics summarizing..."
+bash fastaStats.sh $mergedFastaFile $fastaList > $summaryFile
 
 #Write fasta stats to the csv formatted summary file
-#echo "Beginning file statistics formatting..."
+echo "Beginning file statistics formatting..."
 summaryFileCSV=$(echo "$summaryFile" | sed 's/\.txt/\.csv/g')
-#bash fastaStats_csvFormatted.sh $mergedFastaFile $fastaList > $summaryFileCSV
+bash fastaStats_csvFormatted.sh $mergedFastaFile $fastaList > $summaryFileCSV
 #Fix file tags
-#genotypeList=$(echo "${@:3}")
-#genotypeList="Merged "$genotypeList
-#counter=1
-#for i in $genotypeList; do
-#	genotypeTag="file"$counter
-#	replaceTag=$genotypeTag","$i
-#	sed -i "s/$genotypeTag/$replaceTag/g" $summaryFileCSV
-#	counter=$(($counter+1))
-#done
-#sed -i 's/fileTotal,/fileTotal,Total,/g' $summaryFileCSV
-#sed -i 's/fileDuplicates,/fileDuplicates,Duplicates,/g' $summaryFileCSV
+genotypeList=$(echo "${@:3}")
+genotypeList="Merged "$genotypeList
+counter=1
+for i in $genotypeList; do
+	genotypeTag="file"$counter
+	replaceTag=$genotypeTag","$i
+	sed -i "s/$genotypeTag/$replaceTag/g" $summaryFileCSV
+	counter=$(($counter+1))
+done
+sed -i 's/fileTotal,/fileTotal,Total,/g' $summaryFileCSV
+sed -i 's/fileDuplicates,/fileDuplicates,Duplicates,/g' $summaryFileCSV
 #Re-set header
-#sed -i 's/file,/file,genotype,/g' $summaryFileCSV
+sed -i 's/file,/file,genotype,/g' $summaryFileCSV
 
 #Plot fasta stats from summary file
 echo "Beginning file statistics plotting..."
