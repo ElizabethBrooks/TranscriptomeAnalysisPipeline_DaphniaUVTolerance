5da10c1dfc3e0f716fc5f10fd3b62c9814f7a21d
Mae <merimaat1@gmail.com>
2020-09-09 16:24:32 -0400

    Udated exact test script comments

--- Quantification/glm_edgeR.r
+++ Quantification/glm_edgeR.r
@@ -20,54 +20,82 @@ countsTable <- read.csv(file=args[1], row.names="gene")
 head(countsTable)
 #Import grouping factor
 targets <- read.csv(file=args[2], row.names="sample")
+
 #Setup a design matrix
-group <- factor(paste(targets$Treatment,targets$Tolerance,sep="."))
+group <- factor(paste(targets$Treatment,targets$Genotype,sep="."))
 #cbind(targets,Group=group)
 #Create DGE list object
 list <- DGEList(counts=countsTable,group=group)
+colnames(list) <- targets$Sample
 
-#There is no purpose in analysing genes that are not expressed in either 
-# experimental condition, so genes are first filtered on expression levels
+#Retain genes only if it is expressed at a minimum level
 keep <- filterByExpr(list)
-table(keep)
+summary(keep)
 list <- list[keep, , keep.lib.sizes=FALSE]
-#Calculate normalized factors
+
+#Use TMM normalization to eliminate composition biases
+# between libraries
 list <- calcNormFactors(list)
-#Write normalized counts to file
-normList <- cpm(list, normalized.lib.sizes=TRUE)
-write.table(normList, file="stats_normalizedCounts.csv", sep=",", row.names=TRUE)
-#View normalization factors
 list$samples
-dim(list)
 
-#Produce a matrix of pseudo-counts
-#Estimate common dispersion and tagwise dispersions
-list <- estimateDisp(list)
-list$common.dispersion
-#View dispersion estimates and biological coefficient of variation
-jpeg("plotBCV.jpg")
-plotBCV(list)
-dev.off()
+#Verify TMM normalization using a MD plot
+plotMD(cpm(list, log=TRUE), column=1)
+abline(h=0, col="red", lty=2, lwd=2)
 
-#Groups
-#UV.TolerantY05 - C.G1
-#UV.TolerantE05 - C.G2
-#UV.NotTolerantR2 - C.G3
-#UV.NotTolerantY023 - C.G4
-#VIS.TolerantY05 - T.G1
-#VIS.TolerantE05 - T.G2
-#VIS.NotTolerantR2 - T.G3
-#VIS.NotTolerantY023 - T.G4
+#Use a MDS plot to visualizes the differences
+# between the expression profiles of different samples
+points <- c(0,1,2,3,15,16,17,18)
+colors <- rep(c("blue", "darkgreen", "red", "black"), 2)
+plotMDS(y, col=colors[group], pch=points[group])
+legend("topleft", legend=levels(group), pch=points, col=colors, ncol=2)
 
-#Create the design matrix
-#Each tolerance for each treatment is a group
-design <- model.matrix(~0+group)
+#The experimental design is parametrized with a one-way layout, 
+# where one coefficient is assigned to each group
+design <- model.matrix(~ 0 + group)
 colnames(design) <- levels(group)
-tested <- glmQLFit(list, design)
+design
+attr(,"assign")
+attr(,"contrasts")
+attr(,"contrasts")$group
 
-#View top DE genes
-topTags(tested)
-#Create results table of DE genes
-resultsTbl <- topTags(tested, n=nrow(tested$table))$table
-#Output resulting table
-write.table(resultsTbl, file="stats_glm.csv", sep=",", row.names=TRUE)
+#Next, the NB dispersion is estimated
+list <- estimateDisp(y, design, robust=TRUE)
+list$common.dispersion
+#Visualize the dispersion estimates with a BCV plot
+plotBCV(list)
+
+#Now, estimate and plot the QL dispersions
+fit <- glmQLFit(list, design, robust=TRUE)
+head(fit$coefficients)
+plotQLDisp(fit)
+
+#Grouping factors
+#UV.E05
+#UV.R2
+#UV.Y023
+#UV.Y05
+#VIS.E05
+#VIS.R2
+#VIS.Y023
+#VIS.Y05
+
+#Define a matrix of contrasts, where each column
+# represents a contrast between two groups of interest
+con <- makeContrasts(
+	#Control
+	UV.E05vsR2 = UV.E05 - UV.R2, #A-B
+	UV.E05vsY05 = UV.E05 - UV.Y05, #A-D
+	UV.R2vsY023 = UV.R2 - UV.Y023, #B-C
+	UV.Y023vsY05 = UV.Y023 - UV.Y05, #C-D
+	#Treatment
+	VIS.E05vsR2 = VIS.E05 - VIS.R2, #A-B
+	VIS.E05vsY05 = VIS.E05 - VIS.Y05, #A-D
+	VIS.R2vsY023 = VIS.R2 - VIS.Y023, #B-C
+	VIS.Y023vsY05 = VIS.Y023 - VIS.Y05, #C-D
+	#Pairwise
+	E05.UVvsVIS = UV.E05 - VIS.E05,
+	R2.UVvsVIS = UV.R2 - VIS.R2,
+	Y023.UVvsVIS = UV.Y023 - VIS.Y023,
+	Y05.UVvsVIS = UV.Y05 - VIS.Y05,
+	
+	levels=design)
