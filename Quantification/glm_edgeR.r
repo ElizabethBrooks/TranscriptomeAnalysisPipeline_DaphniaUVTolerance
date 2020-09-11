2819f440bc12ded626422789aea3c4d0c9a0f618
Mae <merimaat1@gmail.com>
2020-09-11 13:00:19 -0400

    Updated comments for glm script

--- Quantification/glm_edgeR.r
+++ Quantification/glm_edgeR.r
@@ -3,29 +3,30 @@
 #Usage Ex: Rscript glm_edgeR.r cleaned.csv 1 24 expDesign_Olympics_GRP1.csv
 #R script to perform statistical analysis of gene count tables using edgeR two way ANOVA
 
+countsTable <- read.csv(file="/home/mae/Documents/RNASeq_Workshop_ND/GeneCountsAnalyzed/genome_sortedName_samtoolsHisat2_run2_counted_htseq_run1/cleaned.csv", row.names="gene")[ ,1:24]
+targets <- read.csv(file="/home/mae/Documents/RNASeq_Workshop_ND/TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/expDesign_Olympics_GRP1.csv", row.names="sample")
+
 #Install edgeR, this should only need to be done once
 #Since edgeR is already installed on the CRC this can be skipped if using the module
 #bioLite("edgeR")
 #install.packages("statmod")
 
-#Retrieve input file name of gene counts
-args = commandArgs(trailingOnly=TRUE)
-#Test if there is one input argument
-if (length(args)!=3) {
-  stop("One file name and a range of columns must be supplied.n", call.=FALSE)
-}
-
 #Load the edgeR library
 library("edgeR")
 library("statmod")
 
+#Retrieve input file name of gene counts
+#args = commandArgs(trailingOnly=TRUE)
+#Test if there is one input argument
+#if (length(args)!=3) {
+#  stop("One file name and a range of columns must be supplied.n", call.=FALSE)
+#}
+
 #Import gene count data
-#/home/mae/Documents/RNASeq_Workshop_ND/GeneCountsAnalyzed/genome_sortedName_samtoolsHisat2_run2_counted_htseq_run1/cleaned.csv
-countsTable <- read.csv(file="/home/mae/Documents/RNASeq_Workshop_ND/GeneCountsAnalyzed/genome_sortedName_samtoolsHisat2_run2_counted_htseq_run1/cleaned.csv", row.names="gene")[ ,1:24]
+#countsTable <- read.csv(file=args[1], row.names="gene")[ ,args[2]:args[3]]
 #head(countsTable)
 #Import grouping factor
-#/home/mae/Documents/RNASeq_Workshop_ND/TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/expDesign_Olympics_GRP1.csv
-targets <- read.csv(file="/home/mae/Documents/RNASeq_Workshop_ND/TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/expDesign_Olympics_GRP1.csv", row.names="sample")
+#targets <- read.csv(file=args[4], row.names="sample")
 
 #Setup a design matrix
 group <- factor(paste(targets$treatment,targets$genotype,sep="."))
@@ -42,7 +43,7 @@ list <- list[keep, , keep.lib.sizes=FALSE]
 #Use TMM normalization to eliminate composition biases
 # between libraries
 list <- calcNormFactors(list)
-list$samples
+#list$samples
 
 #Verify TMM normalization using a MD plot
 plotMD(cpm(list, log=TRUE), column=1)
@@ -60,7 +61,7 @@ plotMDS(list, col=colors[group], pch=points[group])
 # where one coefficient is assigned to each group
 design <- model.matrix(~ 0 + group)
 colnames(design) <- levels(group)
-design
+#design
 
 #Next, the NB dispersion is estimated
 list <- estimateDisp(list, design, robust=TRUE)
@@ -70,18 +71,8 @@ plotBCV(list)
 
 #Now, estimate and plot the QL dispersions
 fit <- glmQLFit(list, design, robust=TRUE)
-head(fit$coefficients)
+#head(fit$coefficients)
 plotQLDisp(fit)
-
-#Grouping factors
-#UV.E05
-#UV.R2
-#UV.Y023
-#UV.Y05
-#VIS.E05
-#VIS.R2
-#VIS.Y023
-#VIS.Y05
 
 #Define a matrix of contrasts, where each column
 # represents a contrast between two groups of interest
@@ -98,6 +89,7 @@ test.allPairs <- glmLRT(fit, contrast=con.allPairs)
 #topTags(test.allPairs)
 summary(decideTests(test.allPairs))
 plotMD(test.allPairs)
+abline(h=c(-1, 1), col="blue")
 
 #Pairwise E05.UVvsVIS
 #qlf.E05.UVvsVIS <- glmQLFTest(fit, contrast=con[,"E05.UVvsVIS"])
@@ -106,6 +98,7 @@ treat.E05.UVvsVIS <- glmTreat(fit, contrast=con.allPairs[,"E05.UVvsVIS"], lfc=lo
 #topTags(treat.E05.UVvsVIS)
 summary(decideTests(treat.E05.UVvsVIS))
 plotMD(treat.E05.UVvsVIS)
+abline(h=c(-1, 1), col="blue")
 
 #Pairwise R2.UVvsVIS
 #Test whether the differential expression is significant
@@ -113,13 +106,15 @@ treat.R2.UVvsVIS <- glmTreat(fit, contrast=con.allPairs[,"R2.UVvsVIS"], lfc=log2
 #topTags(treat.R2.UVvsVIS)
 summary(decideTests(treat.R2.UVvsVIS))
 plotMD(treat.R2.UVvsVIS)
+abline(h=c(-1, 1), col="blue")
 
 #Pairwise Y023.UVvsVIS
 #Test whether the differential expression is significant
-treat.Y023.UVvsVIS <- glmTreat(fit, contrast=con.allPairs[,"R2.UVvsVIS"], lfc=log2(1.2))
+treat.Y023.UVvsVIS <- glmTreat(fit, contrast=con.allPairs[,"Y023.UVvsVIS"], lfc=log2(1.2))
 #topTags(treat.Y023.UVvsVIS)
 summary(decideTests(treat.Y023.UVvsVIS))
 plotMD(treat.Y023.UVvsVIS)
+abline(h=c(-1, 1), col="blue")
 
 #Pairwise Y05.UVvsVIS
 #Test whether the differential expression is significant
@@ -127,6 +122,7 @@ treat.Y05.UVvsVIS <- glmTreat(fit, contrast=con.allPairs[,"Y05.UVvsVIS"], lfc=lo
 #topTags(treat.Y05.UVvsVIS)
 summary(decideTests(treat.Y05.UVvsVIS))
 plotMD(treat.Y05.UVvsVIS)
+abline(h=c(-1, 1), col="blue")
 
 #ANOVA like comparisons of UV
 anov.UV <- makeContrasts(UV.R2 - UV.E05,
@@ -137,6 +133,8 @@ test.anov.UV <- glmLRT(fit, contrast=anov.UV)
 #topTags(test.anov.UV)
 summary(decideTests(test.anov.UV))
 plotMD(test.anov.UV)
+abline(h=c(-1, 1), col="blue")
+
 #ANOVA like comparisons of VIS
 anov.VIS <- makeContrasts(VIS.R2 - VIS.E05,
   VIS.Y023 - VIS.E05,
@@ -146,6 +144,7 @@ test.anov.VIS <- glmLRT(fit, contrast=anov.VIS)
 #topTags(test.anov.VIS)
 summary(decideTests(test.anov.VIS))
 plotMD(test.anov.VIS)
+abline(h=c(-1, 1), col="blue")
 
 #Test whether the average across all UV groups is equal to the average across
 #all VIS groups, to examine the overall effect of treatment
@@ -153,11 +152,14 @@ con.UVvsVIS <- makeContrasts(UVvsVIS = (UV.E05 + UV.R2 + UV.Y023 + UV.Y05)/4
   - (VIS.E05 + VIS.R2 + VIS.Y023 + VIS.Y05)/4,
   levels=design)
 test.anov.VIS <- glmLRT(fit, contrast=con.UVvsVIS)
-topTags(test.anov.VIS)
+#topTags(test.anov.VIS)
 summary(decideTests(test.anov.VIS))
 plotMD(test.anov.VIS)
-#Look at genes with significant expression
+abline(h=c(-1, 1), col="blue")
+
+#Look at genes with significant expression across all UV groups
 treat.anov.VIS <- glmTreat(fit, contrast=con.UVvsVIS, lfc=log2(1.2))
-topTags(treat.anov.VIS)
+#topTags(treat.anov.VIS)
 summary(decideTests(treat.anov.VIS))
 plotMD(treat.anov.VIS)
+abline(h=c(-1, 1), col="blue")
