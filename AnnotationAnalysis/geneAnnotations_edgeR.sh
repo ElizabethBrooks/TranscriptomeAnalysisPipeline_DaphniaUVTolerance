e785fe557d50fd87c94b586df3f3ba594e85a411
Mae <merimaat1@gmail.com>
2020-09-17 00:26:49 -0400

    Updated annotation analysis scripts

--- AnnotationAnalysis/geneAnnotations_edgeR.sh
+++ AnnotationAnalysis/geneAnnotations_edgeR.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Script to run Rscripts that retieve annotations for DE analysis results
-#Usage: bash geneAnnotations_edgeR.sh analysisResultsFile
-#Usage Ex: bash geneAnnotations_edgeR.sh glmQLF_2WayANOVA_topTags_filtered.csv
+#Usage: bash geneAnnotations_edgeR.sh analysisType analysisResultsFile
+#Usage Ex: bash geneAnnotations_edgeR.sh DE glmQLF_2WayANOVA_topTags_filtered.csv
+#Usage Ex: bash geneAnnotations_edgeR.sh GSEA daphniaOlympics_leading_edge_matrix_for_results.1.gmx
 
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -11,20 +12,28 @@ fi
 
 #Retrieve gene ontology data path
 ontologyPath=$(grep "geneOntology:" ../InputData/inputPaths.txt | tr -d " " | sed "s/geneOntology://g")
-#Retrieve analysis inputs path
-outDir=$(grep "DEAnalysis:" ../InputData/outputPaths.txt | tr -d " " | sed "s/DEAnalysis://g")
+
+#Determine input type
+if [[ "$1" == "DE" ]]; then
+	#Retrieve DE analysis inputs path
+	outDir=$(grep "DEAnalysis:" ../InputData/outputPaths.txt | tr -d " " | sed "s/DEAnalysis://g")
+	#Remove file extension
+	inputResults=$(basename "$2" | sed 's/\.csv//g')
+elif [[ "$1" == "DE" ]]; then
+	#Retrieve GSEA inputs path
+	inputPath=$(grep "geneSets:" ../InputData/inputPaths.txt | tr -d " " | sed "s/geneSets://g")
+	#Remove file extension
+	inputResults=$(basename "$2" | sed 's/\.gmx//g')
+fi
 
 #Move to current outputs folder
 cd "$outDir"
-
-#Remove file extension
-inputResults=$(basename "$1" | sed 's/\.csv//g')
 
 #Retrieve all GO IDs
 cut -d"," -f7 "$ontologyPath" | sed "s/\"//g" | cut -d"^" -f1 | sed -n '/\./!p' > sprotIDs_GO.txt
 
 #Make table of GO data for the input DE analysis results
-tail -n+2 "$1" | cut -f1 -d ',' | sed "s/\"//g" > tmpIDs.txt
+tail -n+2 "$2" | cut -f1 -d ',' | sed "s/\"//g" > tmpIDs.txt
 
 #Rertieve GO annotations for DE analysis results
 head -1 "$ontologyPath" > GO_"$inputResults".csv
