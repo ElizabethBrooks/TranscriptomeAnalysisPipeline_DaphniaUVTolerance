7fc54126a059c9acf736af96e3ac322ae378915d
Mae <merimaat1@gmail.com>
2021-01-30 16:25:22 -0500

    Testing decoding script updates

--- Annotation/decoding_transdecoder.sh
+++ Annotation/decoding_transdecoder.sh
@@ -6,12 +6,12 @@
 #$ -pe smp 1
 #Script to predict coding regions from a de novo assembled transcriptome fasta file
 # using Transdecoder
-#Usage: qsub decoding_transdecoder.sh deNovoAssembledTranscriptomeFolder
-#Usage Ex: qsub decoding_transdecoder.sh trimmed_run1Sierra_assemblyTrinity
-#Usage Ex: qsub decoding_transdecoder.sh sortedCoordinate_samtoolsHisat2_run1Sierra_assemblyGenomeTrinity
-#Usage Ex: qsub decoding_transdecoder.sh sortedCoordinate_samtoolsTophat2_run1Sierra_assemblyGenomeTrinity
-#Alternate usage Ex: qsub decoding_transdecoder.sh PA42_cds
-#Alternate usage Ex: qsub decoding_transdecoder.sh PA42_transcripts
+#Usage: qsub decoding_transdecoder.sh deNovoAssembledTranscriptomeFolder target
+#Usage Ex: qsub decoding_transdecoder.sh trimmed_run1E05_assemblyTrinity clusteredNucleotides_cdhit_0.98
+#Usage Ex: qsub decoding_transdecoder.sh sortedCoordinate_samtoolsHisat2_run1Sierra_assemblyGenomeTrinity assembly
+#Usage Ex: qsub decoding_transdecoder.sh sortedCoordinate_samtoolsTophat2_run1Sierra_assemblyGenomeTrinity assembly
+#Alternate usage Ex: qsub decoding_transdecoder.sh PA42_cds genome
+#Alternate usage Ex: qsub decoding_transdecoder.sh PA42_transcripts genome
 
 #Load necessary modules for ND CRC servers
 module load bio/2.0
@@ -29,36 +29,62 @@ if [[ "$1" == *\/* ]] || [[ "$1" == *\\* ]]; then
 	exit 1
 fi
 #Determine input query transcriptome for blastp
-if [[ "$1" == *assembly* ]]; then
+if [[ "$1" == *assemblyTrinity* ]]; then
 	#Retrieve input assembly path
 	inputsPath=$(grep "assembling:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assembling://g")
-	#Retrieve genome reference and features paths
-	multiFASTA=$(echo "$inputsPath"/"$1"/Trinity.fasta)
-	geneMap=$inputsPath/$1/"Trinity.fasta.gene_trans_map"
 	#Set outputs absolute path
 	outputsPath=$inputsPath/$1
-	outputFolder=$outputsPath/"decoded_transdecoder"
+elif [[ "$1" == *assemblyGenome* ]]; then
+	#Retrieve reads input absolute path
+	assemblyPath=$(grep "assemblingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingGenome://g")
+	#Set outputs absolute path
+	outputsPath=$inputsPath/$1
 elif [[ "$1" == PA42_cds ]]; then
 	#Retrieve genome reference absolute path for querying
 	inputsPath=$(grep "codingSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/codingSequencesDB://g")
 	#Retrieve genome reference and features paths
 	multiFASTA=$(grep "codingSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/codingSequencesDB://g")
-	geneMap=$(grep "geneTransMap:" ../InputData/inputPaths.txt | tr -d " " | sed "s/geneTransMap://g")
 	#Set outputs absolute path
 	outputsPath=$(dirname $inputsPath)
-	outputFolder=$outputsPath/"decoded_transdecoder"
 elif [[ "$1" == PA42_transcripts ]]; then
 	#Retrieve genome reference absolute path for querying
 	inputsPath=$(grep "transcriptSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/transcriptSequencesDB://g")
 	#Retrieve genome reference and features paths
 	multiFASTA=$(grep "transcriptSequencesDB:" ../InputData/databasePaths.txt | tr -d " " | sed "s/transcriptSequencesDB://g")
-	geneMap=$(grep "geneTransMap:" ../InputData/inputPaths.txt | tr -d " " | sed "s/geneTransMap://g")
 	#Set outputs absolute path
 	outputsPath=$(dirname $inputsPath)
-	outputFolder=$outputsPath/"decoded_transdecoder"
 else
 	#Error message
 	echo "Invalid fasta entered (assembled transcriptome expected)... exiting!"
+	exit 1
+fi
+#Determine input file type
+if [[ $2 == assembly ]]; then
+	#Retrieve genome reference and features paths
+	multiFASTA=$(echo "$inputsPath"/"$1"/Trinity.fasta)
+	geneMap=$inputsPath/$1/"Trinity.fasta.gene_trans_map"
+	#Set output path
+	outputFolder=$outputsPath/"decoded_transdecoder"
+elif [[ $2 == clusterNucleotide* ]]; then
+	#Retrieve genome reference and features paths
+	multiFASTA=$(echo "$inputsPath"/"$2"/cdhitEst)
+	geneMap=$inputsPath/$1/"Trinity.fasta.gene_trans_map"
+	#Set output path
+	outputFolder=$outputsPath/"$2"/"decoded_transdecoder"
+elif [[ $2 == clusterProtein* ]]; then
+	#Retrieve genome reference and features paths
+	multiFASTA=$(echo "$inputsPath"/"$2"/cdhit)
+	geneMap=$inputsPath/$1/"Trinity.fasta.gene_trans_map"
+	#Set output path
+	outputFolder=$outputsPath/"$2"/"decoded_transdecoder"
+elif [[ $2 == genome ]]; then
+	#Retrieve genome reference and features paths
+	geneMap=$(grep "geneTransMap:" ../InputData/inputPaths.txt | tr -d " " | sed "s/geneTransMap://g")
+	#Set output path
+	outputFolder=$outputsPath/"decoded_transdecoder"
+else 
+	#Error message
+	echo "Invalid analysis target entered... exiting!"
 	exit 1
 fi
 #Make output folder
