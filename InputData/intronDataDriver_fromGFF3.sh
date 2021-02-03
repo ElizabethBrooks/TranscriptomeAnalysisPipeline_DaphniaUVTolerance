991082aa6144b4bbfb997583fb375a04144dcfd1
Mae <merimaat1@gmail.com>
2021-02-02 22:20:19 -0500

    Updating pipeline inputs for latest genome

--- InputData/intronDataDriver_fromGFF3.sh
+++ InputData/intronDataDriver_fromGFF3.sh
@@ -3,14 +3,9 @@
 #Usage: bash intronDataDriver_fromGFF3.sh
 #Usage Ex: bash intronDataDriver_fromGFF3.sh
 
-#Check for input arguments of folder names
-if [ $# -eq 0 ]; then
-   	echo "ERROR: No folder name(s) supplied... exiting"
-   	exit 1
-fi
 #Set paths for r script
 export R_LIBS=/afs/crc.nd.edu/user/e/ebrooks5/R/x86_64-pc-linux-gnu-library/3.5:$R_LIBS
 #Retrieve analysis inputs path
-inputsPath=$(grep "genomeFeatures:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeFeatures://g")
+inputsPath=$(grep "genomeFeatures:" inputPaths.txt | tr -d " " | sed "s/genomeFeatures://g")
 #Generate intron data using GenomicFeatures in R
 Rscript generateIntronData_fromGFF3.r "$inputsPath"
