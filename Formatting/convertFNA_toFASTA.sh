c1fc8ac6be2b38793cbd254826c2f2153a9a3b22
Mae <merimaat1@gmail.com>
2021-02-05 13:43:58 -0500

    Updated formatting scripts

--- Formatting/convertFNA_toFASTA.sh
+++ Formatting/convertFNA_toFASTA.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-#Script to convert a Genbank fna file to fasta format
+#Script to convert a genbank fna file to fasta format
+#Usage: bash convertFNA_toFASTA.sh fnaFilePath
+#Usage ex: bash convertFNA_toFASTA.sh /home/mae/Documents/RNASeq_Workshop_ND/genomicResources_PA42_v4.1/GCA_900092285.2_PA42_4.1_genomic.fna
 
-cat /afs/crc.nd.edu/group/pfrenderlab/bateson/ebrooks/rnaseq/genomicResources_PA42_v4.1/GCA_900092285.2_PA42_4.1_genomic.fna | sed "s/^>.*scaffold/>scaffold/g" | sed "s/,.*//g" > /afs/crc.nd.edu/group/pfrenderlab/bateson/ebrooks/rnaseq/genomicResources_PA42_v4.1/GCA_900092285.2_PA42_4.1_genomic.fasta
+#Remove the extension from the input file
+fileName=$(echo "$1" | sed 's/\.fna//g')
+
+#First, the fna file is read into std out
+#Then, for each sequence the genbank tag and string is removed up to the scaffold tag
+#Finally, the string after the scaffold number to the end of the sequence name line is removed
+cat "$1" | sed "s/^>.*scaffold/>scaffold/g" | sed "s/,.*//g" > "$fileName".fasta
