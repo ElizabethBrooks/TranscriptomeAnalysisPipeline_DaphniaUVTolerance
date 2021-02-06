65f805e4a6e6c299ec9f4b9f50903ebe80e7d53c
Mae <merimaat1@gmail.com>
2021-02-05 19:29:03 -0500

    Running orthofinder on v3.0

--- Annotation/search_orthoFinder.sh
+++ Annotation/search_orthoFinder.sh
@@ -8,8 +8,9 @@
 # infers rooted gene trees for all orthogroups and identifies 
 # all of the gene duplication events in those gene trees
 #Usage: qsub search_orthoFinder.sh analysisType proteomeFastaList
-#Usage Ex: qsub search_orthoFinder.sh MSA trimmed_run1E05_assemblyTrinity trimmed_run1Y05_assemblyTrinity trimmed_run1R2_assemblyTrinity trimmed_run1Y023_5_assemblyTrinity trimmed_run1PA_assemblyTrinity trimmed_run1Sierra_assemblyTrinity PA42_v4.1_proteins
-#Usage Ex: qsub search_orthoFinder.sh default trimmed_run1E05_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Y05_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1R2_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Y023_5_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1PA_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Sierra_assemblyTrinity/clusteredNucleotides_cdhit_0.98 PA42_v4.1_proteins
+#Usage ex: qsub search_orthoFinder.sh MSA trimmed_run1E05_assemblyTrinity trimmed_run1Y05_assemblyTrinity trimmed_run1R2_assemblyTrinity trimmed_run1Y023_5_assemblyTrinity trimmed_run1PA_assemblyTrinity trimmed_run1Sierra_assemblyTrinity PA42_v4.1_proteins
+#Usage ex: qsub search_orthoFinder.sh default trimmed_run1E05_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Y05_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1R2_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Y023_5_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1PA_assemblyTrinity/clusteredNucleotides_cdhit_0.98 trimmed_run1Sierra_assemblyTrinity/clusteredNucleotides_cdhit_0.98 PA42_v4.1_proteins
+#Usage ex: qsub search_orthoFinder.sh default sortedCoordinate_samtoolsHisat2_run2E05_assemblyPA42_v3.0Trinity sortedCoordinate_samtoolsHisat2_run2Y05_assemblyPA42_v3.0Trinity sortedCoordinate_samtoolsHisat2_run2Y023_5_assemblyPA42_v3.0Trinity sortedCoordinate_samtoolsHisat2_run2R2_assemblyPA42_v3.0Trinity sortedCoordinate_samtoolsHisat2_run2PA_assemblyPA42_v3.0Trinity sortedCoordinate_samtoolsHisat2_run2Sierra_assemblyPA42_v3.0Trinity PA42_v3.0_proteins
 
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -41,20 +42,20 @@ done
 inputsDir="$outputFolder"
 for i in "${@:2}"; do #Skip first two argument
 	#Determine input proteome
-	if [[ "$i" == *assemblyTrinity* ]]; then
+	if [[ "$1" == *assemblyTrinity* || "$1" == *assemblyStringtie* ]]; then
 		#Retrieve reads input absolute path
-		assemblyPath=$(grep "assemblingFree:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingFree://g")
-	elif [[ "$i" == *assemblyGenome* ]]; then
+		inputsPath=$(grep "assemblingFree:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingFree://g")
+	elif [[ "$1" == *assembly*Trinity* || "$1" == *assembly*Stringtie* ]]; then
 		#Retrieve reads input absolute path
-		assemblyPath=$(grep "assemblingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingGenome://g")
+		inputsPath=$(grep "assemblingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingGenome://g")
 	elif [[ "$i" == *proteins ]]; then
 		#Retrieve genome reference absolute path for querying
-		inputsPath=$(grep "proteinSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/proteinSequences://g")
+		inputsPath=$(grep "proteinSequences:" ../Archived/inputPaths.txt | tr -d " " | sed "s/proteinSequences://g")
 		#Copy input fasta files
 		cp "$inputsPath" "$inputsDir"
 	elif [[ "$i" == *cds ]]; then
 		#Retrieve genome reference absolute path for querying
-		inputsPath=$(grep "codingSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/codingSequences://g")
+		inputsPath=$(grep "codingSequences:" ../Archived/inputPaths.txt | tr -d " " | sed "s/codingSequences://g")
 		inputsPath=$(dirname "$inputsPath")
 		inputsPath=$(echo "$inputsPath"/decoded_transdecoder/*transdecoder.pep)
 		#Copy input fasta files
