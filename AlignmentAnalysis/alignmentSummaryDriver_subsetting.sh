191bbdec6067a9c84bd29f0464a38f8c7ee9fe2f
Mae <merimaat1@gmail.com>
2021-02-15 13:30:16 -0500

    Updating alignment analysis inputs

--- AlignmentAnalysis/alignmentSummaryDriver_subsetting.sh
+++ AlignmentAnalysis/alignmentSummaryDriver_subsetting.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Bash script to retrieve mapping stats
 #Usage: bash alignmentSummaryDriver_subsetting.sh alignmentFolder optionalAssemblyFolder genotypeList optionalRunList
-#Usage Ex: bash alignmentSummaryDriver_subsetting.sh aligned_hisat2 PA42_v4.1 run4
-#Usage Ex: bash alignmentSummaryDriver_subsetting.sh aligned_tophat2 PA42_v4.1 run1 run2 run3
+#Usage Ex: bash alignmentSummaryDriver_subsetting.sh aligned_hisat2 run4
+#Usage Ex: bash alignmentSummaryDriver_subsetting.sh aligned_tophat2 run1 run2 run3
 #Alternate usage Ex: bash alignmentSummaryDriver_subsetting.sh trimmed_run1 aligned_hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
-#Alternate usage Ex: bash alignmentSummaryDriver_subsetting.sh sortedCoordinate_samtoolsHisat2_run1 aligned_hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
-#Alternate usage Ex: bash alignmentSummaryDriver_subsetting.sh sortedCoordinate_samtoolsTophat2_run1 aligned_hisat2_run1 E05 Y05 R2 Y023_5 PA Sierra
+#Alternate usage Ex: bash alignmentSummaryDriver_subsetting.sh sortedCoordinate_samtoolsHisat2_run1 aligned_hisat2_run1 PA42_v4.1 E05 Y05 R2 Y023_5 PA Sierra
+#Alternate usage Ex: bash alignmentSummaryDriver_subsetting.sh sortedCoordinate_samtoolsTophat2_run1 aligned_hisat2_run1 PA42_v4.1 E05 Y05 R2 Y023_5 PA Sierra
 
 #Check for input arguments of folder names
 if [ $# -eq 0 ]; then
@@ -24,6 +24,8 @@ elif [[ "$1"  == sorted* || "$1"  == trimmed* ]]; then
 	if [[ "$1" == trimmed* ]]; then
 		#Retrieve reads input absolute path
 		inputsPath=$(grep "assemblingFree:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingFree://g")
+		#Set genome tag
+		genomeTag="$3"
 	elif [[ "$1" == sorted* ]]; then
 		#Retrieve reads input absolute path
 		inputsPath=$(grep "assemblingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingGenome://g")
@@ -40,24 +42,39 @@ fi
 #Initialize variables
 counter=1
 #Loop through all input sets of treatments and perform selected analsysis
+analysisFlag=-1
 for i in "$@"; do
-	#Skip first two arguments
-	if [ $counter -ge 3 ]; then
-		#Determine what type of data folder was input
-		if [[ "$1" == trimmed* ]]; then
-			inputFolder="$inputsPath"/"$1""$i"_assemblyTrinity/"$fileName"
-			genotype="$i"
-		elif [[ "$1" == sorted* ]]; then
-			inputFolder="$inputsPath"/"$1""$i"_assemblyGenomeTrinity/"$fileName"
-			genotype="$i"
-		elif [[ "$1" == aligned* ]]; then
+	if [[ "$1" == aligned* ]]; then
+		#Skip first argument
+		if [ $counter -ge 2 ]; then
+			#Set input data folder
 			inputFolder="$inputsPath"/"$1"_"$i"
 			genotype="$2"
 			fileName="$1"_"$i"
-		else
-			echo "ERROR: Input folder for analysis is not a valid option... exiting!"
-			exit 1
+			#Set analysis flag
+			analysisFlag=1
 		fi
+	elif [[ "$1" == trimmed*  ]]; then
+		#Skip first two arguments
+		if [ $counter -ge 3 ]; then
+			#Set input data folder
+			inputFolder="$inputsPath"/"$1""$i"_assemblyTrinity/"$fileName"
+			genotype="$i"
+			#Set analysis flag
+			analysisFlag=1
+		fi
+	else
+		#Skip first three arguments
+		if [ $counter -ge 4 ]; then
+			#Set input data folder
+			inputFolder="$inputsPath"/"$1""$i"_assembly"$genomeTag"Trinity/"$fileName"
+			genotype="$i"
+			#Set analysis flag
+			analysisFlag=1
+		fi
+	fi
+	#Determine if analysis should begin
+	if [[ "$analysisFlag" == 1 ]]; then
 		#Set outputs directory
 		outputsPath=$(dirname "$inputFolder")
 		outDir="$outputsPath"/AlignmentsAnalyzed
