f67e755b999e80358a8af199ee280c7f6bed39c2
Mae <merimaat1@gmail.com>
2021-03-20 17:05:38 -0400

    Updating genome wide MSA script

--- SelectionAnalysis/generateMSA_muscle.sh
+++ SelectionAnalysis/generateMSA_muscle.sh
@@ -8,30 +8,29 @@ refPath=$(grep "codingSequences:" ../InputData/inputPaths.txt | tr -d " " | sed 
 grep ">" "$refPath" | sed "s/-CDS//g" | sed "s/>//g" > col1.txt
 while IFS= read -r line; do
 	#Prepare multiline cds fasta to retrieve seqs
-	#cat "$refPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmpPA42_v4.1.fasta
+	cat "$refPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmpPA42_v4.1.fasta
 	#Retrieve selected coding sequences and convert back to multiline fasta format
 	gTag=$line"-CDS"
-	echo"$gTag"
-	#gFile=$line"_cds_allDaphnia.fasta"
-	#grep "^>$gTag" tmpPA42_v4.1.fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>PA42_v4.1_$gTag/g" > "$gFile"
+	gFile=$line"_cds_allDaphnia.fasta"
+	grep "^>$gTag" tmpPA42_v4.1.fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>PA42_v4.1_$gTag/g" > "$gFile"
 	
 	#Loop over all input samples
-	#for i in "$@"; do
+	for i in "$@"; do
 		#Prepare multiline cds fasta to retrieve seqs
-		#cat "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/decoded_transdecoder/Trinity.fasta.transdecoder.cds | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmp"$i".fasta
+		cat "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/decoded_transdecoder/Trinity.fasta.transdecoder.cds | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmp"$i".fasta
 		#Retrieve gene associated with RBHB
-		#pTag=$line"-pep"
-		#rbhTag=$(grep "$pTag" "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/reciprocalSearched_blastp_PA42_v4.1_proteins/blastp_RBH.txt | cut -d ',' -f 1)
+		pTag=$line"-pep"
+		rbhTag=$(grep "$pTag" "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/reciprocalSearched_blastp_PA42_v4.1_proteins/blastp_RBH.txt | cut -d ',' -f 1)
 		#Retrieve selected coding sequences and convert back to multiline fasta format
-		#grep "^>$rbhTag" tmp"$i".fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$rbhTag.*/>$i_$rbhTag/g" >> "$gFile"
+		grep "^>$rbhTag" tmp"$i".fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$rbhTag.*/>$i_$rbhTag/g" >> "$gFile"
 		#Create MSA
-		#mFile=$line"_cds_allDaphnia_aligned.fasta"
-		#muscle -in "$gFile" -out "$mFile"
+		mFile=$line"_cds_allDaphnia_aligned.fasta"
+		muscle -in "$gFile" -out "$mFile"
 		#Clean up
-		#rm tmp*.fasta
-		#rm "$gFile"
-	#done
+		rm tmp*.fasta
+		rm "$gFile"
+	done
 done < col1.txt
 
 #Clean up
-#rm col1.txt
+rm col1.txt
