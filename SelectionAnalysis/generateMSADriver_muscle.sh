ef3173de94430ae8b1d02538b356e8efb1752b86
Mae <merimaat1@gmail.com>
2021-03-21 02:10:38 -0400

    Updated MSA script outputs path

--- SelectionAnalysis/generateMSADriver_muscle.sh
+++ SelectionAnalysis/generateMSADriver_muscle.sh
@@ -4,6 +4,13 @@
 refPath=$(grep "codingSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/codingSequences://g")
 #Set outputs path
 outPath=$(grep "MSA:" ../InputData/outputPaths.txt | tr -d " " | sed "s/MSA://g")
+outPath="$outPath"/daphniaMSA_PA42_v4.1
+#Check if the folder already exists
+mkdir "$outPath"
+if [ $? -ne 0 ]; then
+	echo "The $outPath directory already exsists... please remove before proceeding."
+	exit 1
+fi
 
 #Loop over all genes in the reference
 grep ">" "$refPath" | sed "s/-CDS//g" | sed "s/>//g" > "$outPath"/col1.txt
