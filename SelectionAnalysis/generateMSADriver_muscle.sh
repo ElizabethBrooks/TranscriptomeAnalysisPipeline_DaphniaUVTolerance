93b6cfcfa87a76e07de09b126f8ad5ec4515f166
Mae <merimaat1@gmail.com>
2021-03-21 02:04:54 -0400

    Added batching to MSA scripts

---
+++ SelectionAnalysis/generateMSADriver_muscle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Retrieve inputs path
+refPath=$(grep "codingSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/codingSequences://g")
+#Set outputs path
+outPath=$(grep "MSA:" ../InputData/outputPaths.txt | tr -d " " | sed "s/MSA://g")
+
+#Loop over all genes in the reference
+grep ">" "$refPath" | sed "s/-CDS//g" | sed "s/>//g" > "$outPath"/col1.txt
+
+#Split the reference gene set into segments
+split -l 1500 "$outPath"/col1.txt "$outPath"/colRef
+
+#Generate MSAs for each segment
+for i in "$outPath"/colRef*; do
+	qsub generateMSA_muscle.sh "$i" "$@"
+done
+
+#Clean up
+rm "$outPath"/col1.txt
--- SelectionAnalysis/generateMSA_muscle.sh
+++ SelectionAnalysis/generateMSA_muscle.sh
@@ -11,30 +11,30 @@ module load bio
 inputPath=$(grep "assemblingGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/assemblingGenome://g")
 refPath=$(grep "codingSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/codingSequences://g")
 #Set outputs path
-outPath=$(dirname $refPath)
+outPath=$(dirname "$1")
 
 #Loop over all genes in the reference
-grep ">" "$refPath" | sed "s/-CDS//g" | sed "s/>//g" > col1.txt
+colRefFile="$1"
 while IFS= read -r line; do
 	#Prepare multiline cds fasta to retrieve seqs
-	cat "$refPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmpPA42_v4.1.fasta
+	cat "$refPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > "$outPath"/tmpPA42_v4.1.fasta
 	#Retrieve selected coding sequences and convert back to multiline fasta format
 	gTag=$line"-CDS"
-	gFile=$line"_cds_allDaphnia.fasta"
-	grep "^>$gTag" tmpPA42_v4.1.fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>PA42_v4.1_$gTag/g" > "$gFile"
+	gFile="$outPath"/"$line"_cds_allDaphnia.fasta
+	grep "^>$gTag" "$outPath"/tmpPA42_v4.1.fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>PA42_v4.1_$gTag/g" > "$gFile"
 	
 	#Output Status message
 	echo "Generating MSA for $line..."
 	#Loop over all input samples
-	for i in "$@"; do
+	for i in "${@:2}"; do
 		#Prepare multiline cds fasta to retrieve seqs
-		cat "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/decoded_transdecoder/Trinity.fasta.transdecoder.cds | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > tmp"$i".fasta
+		cat "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/decoded_transdecoder/Trinity.fasta.transdecoder.cds | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > "$outPath"/tmp"$i".fasta
 		#Retrieve gene associated with RBHB
 		pTag=$line"-pep"
 		rbhTag=$(grep "$pTag" "$inputPath"/sortedCoordinate_samtoolsHisat2_run1"$i"_assemblyPA42_v4.1Trinity/reciprocalSearched_blastp_PA42_v4.1_proteins/blastp_RBH.txt | cut -d ',' -f 1)
 		#Retrieve selected coding sequences and convert back to multiline fasta format
 		sTag="$i"_"$rbhTag"
-		grep "^>$rbhTag" tmp"$i".fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$rbhTag.*/>$sTag/g" >> "$gFile"
+		grep "^>$rbhTag" "$outPath"/tmp"$i".fasta | sed 's/NEWLINE/\n/g' | sed "s/^>$rbhTag.*/>$sTag/g" >> "$gFile"
 	done
 
 	#Create MSA
@@ -44,9 +44,9 @@ while IFS= read -r line; do
 	echo "$line MSA created: $mFile"
 
 	#Clean up
-	rm tmp*.fasta
+	rm "$outPath"/tmp*.fasta
 	rm "$gFile"
-done < col1.txt
+done < "$colRefFile"
 
 #Clean up
-rm col1.txt
+rm "$colRefFile"
