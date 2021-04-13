b9afc6e04dd4c7b7a5c21f06b4e28a88011d1551
Mae <merimaat1@gmail.com>
2021-04-13 01:10:43 -0400

    Updated script to merge gene IDs with pValues

--- AnnotationAnalysis/mergeRBH_pValues.sh
+++ AnnotationAnalysis/mergeRBH_pValues.sh
@@ -3,22 +3,38 @@
 #Script to merge list of gene IDs and pvalues with RBH gene IDs
 
 #RBH file
-rbhFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_uvResponsiveGenes_blastp_PA42_v4.1.csv"
+#rbhFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_uvResponsiveGenes_blastp_PA42_v4.1.csv"
+rbhFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/DEGs_UVBvsCntrl_Tcast_blastp_PA42_v4.1.csv"
 #PValue file
-pvalueFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_uvResponsiveGenes_pvalues.csv"
+#pvalueFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_uvResponsiveGenes_pvalues.csv"
+pvalueFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/DEGs_UVBvsCntrl_Tcast_pvalues.csv"
 
+#Output file
+#outFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_genePValues.csv"
+#outUniqFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Dmel_Svetec_2016_genePValues_uniq.csv"
+outFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Tcast_Guo_2019_genePValues.csv"
+outUniqFile="/home/mae/Documents/RNASeq_Workshop_ND/CandidateGeneLists/Tcast_Guo_2019_genePValues_uniq.csv"
+
+#Output file
 #Remove headers
-tail -n +2 $queryFileRBH > tmp1.txt
-tail -n +2 $dbFileRBH > tmp2.txt
+tail -n +2 $rbhFile > tmp1.csv
+tail -n +2 $pvalueFile > tmp2.csv
 
 #Pre-clean up
-echo "queryHit,dbHit" > $outFileRBH
+echo "geneOG,gene,PValue" > $outFile
+echo "geneOG,PValue" > $outUniqFile
+#sed -i "s/FBpp/FBgn/g" tmp1.csv
 
 #Loop over first set of annotations
-while IFS=, read -r f1 f2
-do
-	#Determine annotation sets
-	if grep -q ",$f2" tmp2.txt; then #consensus RBH
-		echo "$f1,$f2" >> $outFileRBH
+while IFS=, read -r f1 f2; do
+	#Match gene IDs with PValues
+	if grep -q $f1"," tmp1.csv; then
+		line=$(grep $f1"," tmp1.csv | head -1)
+		echo $line","$f2 >> $outFile
+	else
+		echo $f1","$f2 >> $outUniqFile
 	fi
-done < tmp1.txt
+done < tmp2.csv
+
+#Clean up
+rm tmp*.csv
