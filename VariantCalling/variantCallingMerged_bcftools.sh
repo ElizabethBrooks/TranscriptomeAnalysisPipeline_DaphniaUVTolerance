c45aa3573ed46dfde1db0b01ee612b8cf7095512
Mae <merimaat1@gmail.com>
2021-06-01 23:40:21 -0400

    Added variant filtering check points

--- VariantCalling/variantCallingMerged_bcftools.sh
+++ VariantCalling/variantCallingMerged_bcftools.sh
@@ -57,6 +57,9 @@ fi
 #Name output file of inputs
 inputOutFile="$outFolder"/variantCalling_summary.txt
 
+#Name output file of filtering info
+outputsFile="$outFolder"/variantCalling_filteredVariantNums.txt
+
 #Select lines associated with input genotype
 #genotype=_"$4"_
 #grep "$genotype" "$inputBamList" > tmpList_genotype.txt
@@ -84,30 +87,54 @@ echo bcftools mpileup --threads 8 -Ob -o "$outFolder"/"$type"_raw.bcf -f "$genom
 #Detect the single nucleotide polymorphisms 
 bcftools call --threads 8 -mv -Oz -o "$outFolder"/"$type"_calls.vcf.gz "$outFolder"/"$type"_raw.bcf 
 echo bcftools call --threads 8 -mv -Oz -o "$outFolder"/"$type"_calls.vcf.gz "$outFolder"/"$type"_raw.bcf >> "$inputOutFile"
+
 #Index vcf file
 bcftools index --threads 8 "$outFolder"/"$type"_calls.vcf.gz
 echo bcftools index --threads 8 "$outFolder"/"$type"_calls.vcf.gz >> "$inputOutFile"
+echo "Total variants: " > "$outputsFile"
+bcftools filter -i '%QUAL<1001' "$outFolder"/"$type"_calls.vcf.gz | grep "^scaffold" | wc -l >> "$outputsFile"
 
 #Include sites with quality > 20 
-# and average read depth > 10
-bcftools filter --threads 8 -i '%QUAL>20 && INFO/DP>10' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf
-echo bcftools filter --threads 8 -i '%QUAL>20 && INFO/DP>10' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf >> "$inputOutFile"
+bcftools filter --threads 8 -i '%QUAL>20' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf
+echo bcftools filter --threads 8 -i '%QUAL>20' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf >> "$inputOutFile"
+echo "Sites with quality > 20: " >> "$outputsFile"
+bcftools filter --threads 8 -i '%QUAL>20' "$outFolder"/"$type"_calls.vcf.gz | grep "^scaffold" | wc -l >> "$outputsFile"
+
+#Include sites with average read depth > 10
+bcftools filter --threads 8 -i 'INFO/DP>10' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf
+echo bcftools filter --threads 8 -i 'INFO/DP>10' "$outFolder"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf >> "$inputOutFile"
+echo "Sites with average read depth > 10: " >> "$outputsFile"
+bcftools filter --threads 8 -i 'INFO/DP>10' "$outFolder"/"$type"_calls.vcf.gz | grep "^scaffold" | wc -l >> "$outputsFile"
+
 #Exclude hetoerozygous sites
 bcftools filter --threads 8 -e 'GT="het"' "$outFolder"/"$type"_calls.flt-qualDP.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf
 echo bcftools filter --threads 8 "-e 'GT=\"het\"'" "$outFolder"/"$type"_calls.flt-qualDP.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf >> "$inputOutFile"
+echo "Excluding hetoerozygous sites: " >> "$outputsFile"
+bcftools filter --threads 8 -e 'GT="het"' "$outFolder"/"$type"_calls.flt-qualDP.bcf | grep "^scaffold" | wc -l >> "$outputsFile"
+
 #Exclude sites homozygous for the reference
 bcftools filter --threads 8 -e 'GT="RR"' "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf
 echo bcftools filter --threads 8 "-e 'GT=\"RR\"'" "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf >> "$inputOutFile"
+echo "Excluding sites homozygous for the reference: " >> "$outputsFile"
+bcftools filter --threads 8 -e 'GT="RR"' "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf | grep "^scaffold" | wc -l >> "$outputsFile"
+
 #Filter adjacent indels within 2bp
 bcftools filter --threads 8 -G 2 "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf -Ob -o "$outFolder"/"$type"_calls.flt-indels.bcf
 echo bcftools filter --threads 8 -G 2 "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf -Ob -o "$outFolder"/"$type"_calls.flt-indels.bcf >> "$inputOutFile"
+echo "Excluding adjacent indels within 2bp: " >> "$outputsFile"
+bcftools filter --threads 8 -G 2 "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf | grep "^scaffold" | wc -l >> "$outputsFile"
+
 #Filter adjacent SNPs within 2bp
 bcftools filter --threads 8 -g 2 "$outFolder"/"$type"_calls.flt-indels.bcf -Ob -o "$outFolder"/"$type"_calls.flt-SNPs.bcf
 echo bcftools filter --threads 8 -g 2 "$outFolder"/"$type"_calls.flt-indels.bcf -Ob -o "$outFolder"/"$type"_calls.flt-SNPs.bcf >> "$inputOutFile"
+echo "Excluding adjacent SNPs within 2bp: " >> "$outputsFile"
+bcftools filter --threads 8 -g 2 "$outFolder"/"$type"_calls.flt-indels.bcf | grep "^scaffold" | wc -l >> "$outputsFile"
 
 #Turn on left alignment, normalize indels, and collapse multi allelic sites
 bcftools norm --threads 8 -m +any -f "$genomeFile" "$outFolder"/"$type"_calls.flt-SNPs.bcf -Ob -o "$outFolder"/"$type"_calls.normCollapse.bcf
 echo bcftools norm --threads 8 -m +any -f "$genomeFile" "$outFolder"/"$type"_calls.flt-SNPs.bcf -Ob -o "$outFolder"/"$type"_calls.normCollapse.bcf >> "$inputOutFile"
+echo "Turn on left alignment, normalize indels, and collapse multi allelic sites: " >> "$outputsFile"
+bcftools filter -i '%QUAL<1001' "$outFolder"/"$type"_calls.flt-SNPs.bcf | grep "^scaffold" | wc -l >> "$outputsFile"
 
 #Clean up
 rm tmpList*.txt
