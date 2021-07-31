841f407b91e731f9fdc27f4347b682f85136cf47
Elizabeth Brooks <bamflappy@Elizabeths-MacBook-Air.local>
2021-07-31 14:11:25 -0400

    Updating scripts for tests for selection

---
+++ SelectionAnalysis/testSelectionDriver_muscle.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N testSelection_jobOutput
+#Script to generate MSAs for each gene in the reference set of peptide sequences
+#Usage: bash testSelectionDriver_muscle.sh sampleSet variantCallingDir
+#Usage ex: bash testSelectionDriver_muscle.sh sortedCoordinate_samtoolsHisat2_run3 variantCallingBcftools_filteredMapQ
+
+#Load necessary modules
+module load bio
+
+#Retrieve input absolute path
+inputsPath=$(grep "aligningGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligningGenome://g")
+inputsPath="$inputsPath"/"$1"/"$2"
+inputsPath="$inputsPath"/"decoded_transdecoder"/transcripts_cufflinks.fa.transdecoder.pep
+#Retrieve genome reference absolute path
+refPath=$(grep "genomeReference:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeReference://g")
+refPath=$(dirname $refPath)
+refPath="$refPath"/"decoded_transdecoder"/transcripts_cufflinks.fa.transdecoder.pep
+
+#Set outputs path
+outDir=$(grep "MSA:" ../InputData/outputPaths.txt | tr -d " " | sed "s/MSA://g")
+outPath="$outDir"/daphniaMSA_PA42_v4.1_pep
+#Check if the folder already exists
+mkdir "$outPath"
+if [ $? -ne 0 ]; then
+	echo "The $outPath directory already exsists... please remove before proceeding."
+	exit 1
+fi
+
+#Set results path
+resultsDir="$outDir"/daphniaKaks_PA42_v4.1_pep
+#Check if the folder already exists
+mkdir "$resultsDir"
+if [ $? -ne 0 ]; then
+	echo "The $resultsDir directory already exsists... please remove before proceeding."
+	exit 1
+fi
+
+#Generate a list of all genes in the reference
+colRefFile="$outPath"/col1.txt
+grep ">" "$refPath" | cut -d" " -f1 | sed "s/-/DASH/g" | sed "s/\./PERIOD/g" | sed "s/>//g" > "$colRefFile"
+
+#Split the reference gene set into segments
+#split -l 2000 "$outPath"/col1.txt "$outPath"/colRef
+
+#Generate MSAs for each segment
+#for i in "$outPath"/colRef*; do
+#	qsub generateMSA_pep_muscle.sh "$i" "$@"
+#done
+
+#Prepare reference multiline pep fasta to retrieve seqs
+tmpRef="$colRefFile"_tmpPA42_v4.1.fasta
+cat "$refPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > "$tmpRef"
+
+#Prepare input multiline pep fasta
+tmpSample="$outPath"/"$colRefFile"_tmpInput.fasta
+cat "$inputsPath" | sed ':a;N;$!ba;s/\n/NEWLINE/g' | sed 's/NEWLINE>/\n>/g' > "$tmpSample"
+
+#Save ka ks values to final results file
+resultsFile="$resultsDir"/PA42_v4.1_kaksResults.txt
+echo "geneID  t  S  N  dN/dS  dN  dS" > "$resultsFile"
+
+#Loop over all genes in the reference
+outPath=$(dirname $colRefFile)
+while IFS= read -r line; do
+	#Retrieve selected peptide sequences and convert back to multiline fasta format
+	gTag=$(echo $line | sed "s/DASH/-/g" | sed "s/PERIOD/\./g")
+	gFile="$outPath"/tmp_pep_allDaphnia_"$line".fasta
+	grep "^>$gTag" "$tmpRef" | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>PA42_v4.1_$gTag/g" > "$gFile"
+	
+	#Output Status message
+	echo "Generating MSA for $line..."
+
+	#Prepare multiline pep fasta to retrieve seqs
+	grep "^>$gTag" "$tmpSample" | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>Olympics_$gTag/g" >> "$gFile"
+
+	#Create MSA
+	mFile="$outPath"/"$gTag"_pep_allDaphnia_aligned.fasta
+	muscle -in "$gFile" -out "$mFile"
+	
+	#Output status message
+	echo "MSA created for $line: $mFile"
+
+	#Clean up
+	rm "$gFile"
+
+	#Generate and save ka ks values
+	bash testSelection_pal2nalCodeml.sh "$gTag" "$1" "$2"
+	kaks=$(tail -1 "$gTag".codeml)
+	echo "$gTag  $kaks" >> "$resultsFile"
+done < "$colRefFile"
+
+#Clean up
+rm "$tmpSample"
+rm "$tmpRef"
+rm "$colRefFile"
