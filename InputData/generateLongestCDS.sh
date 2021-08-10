de97a28e0ec10a5943102a6935cacc9c3f3e6a4f
Elizabeth Brooks <merimaat1@gmail.com>
2021-08-09 23:06:23 -0400

    Updated script to generate longest CDS

--- InputData/generateLongestCDS.sh
+++ InputData/generateLongestCDS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script to generate the longest CDS from the PA42 v4.1 gff and reference fasta
-#Usage: bash generateLongestCDS.sh
+#Usage: qsub generateLongestCDS.sh
 
 #Load necessary module
 module load bio
@@ -13,6 +13,8 @@ inputRef=$(grep "genomeReference:" ../InputData/inputPaths.txt | tr -d " " | sed
 outDir=$(dirname "$inputRef")
 outCDS="$outDir"/PA42_v4.1_CDS.fa
 
+#Retrieve CDS
+echo "Retrieving CDS..."
 #Usage: gffread <input_gff> [-g <genomic_seqs_fasta> | <dir>][-s <seq_info.fsize>] [-o <outfile.gff>] [-t <tname>] [-r #[[<strand>]<chr>:]<start>..<end> [-R]] [-CTVN‐ JMKQAFGUBHZWTOLE] [-w <exons.fa>] [-x <cds.fa>] [-y <tr_cds.fa>] [-i <maxintron>]
 gffread "$inputFeat" -g "$inputRef" -x "$outCDS" -W -F
 
@@ -29,30 +31,33 @@ cat "$colRefIn" | cut -f1 > "$tmpList"
 outLongCDS="$outDir"/PA42_v4.1_longestCDS.fa
 [ -f $outLongCDS ] && rm $outLongCDS
 cLen=0
+echo "Writing longest CDS to file: $outLongCDS"
 while IFS= read -r line; do
     #Check if current gene has multiple CDS ORF
     gTag=">$line"
+    gLong="$gTag"
     gLen=0
     numCDS=$(grep -w "$gTag" "$tmpCDS" | wc -l)
     if [ $numCDS -gt 1 ]; then
-        #Get the length of each CDS
+        #Get the longest CDS
         for i in $(seq 1 $numCDS); do
             #Initialize variables
             cTag=$(grep -w "$gTag" "$tmpCDS" | head -$i | tail -1 | cut -d" " -f1)
-            cStart=$(grep -w "$cTag" "$tmpCDS" | head -$i | tail -1 | cut -d" " -f3 | cut -d ")" -f2 | cut -d "-" -f2)
-            cEnd=$(grep -w "$cTag" "$tmpCDS" | head -$i | tail -1 | cut -d" " -f3 | cut -d ")" -f2 | cut -d "-" -f2)
+            cStart=$(grep -w "$gTag" "$tmpCDS" | head -$i | tail -1 | cut -d" " -f3 | cut -d ")" -f2 | cut -d "-" -f2)
+            cEnd=$(grep -w "$gTag" "$tmpCDS" | head -$i | tail -1 | cut -d" " -f3 | cut -d ")" -f2 | cut -d "-" -f2)
             cLen=$(($cEnd-$cStart))
             #Keep the longest CDS
             if [ $cLen -gt $gLen ]; then
                 gLen=$cLen
-                gTag="$cTag"
+                gLong="$cTag"
             fi
             cLen=0
         done
     fi
     #Output longest CDS
-    grep -w "$gTag" "$tmpCDS" | sed 's/NEWLINE/\n/g' >> "$outLongCDS"
+    grep -w "$gLong" "$tmpCDS" | sed 's/NEWLINE/\n/g' >> "$outLongCDS"
 done < "$tmpList"
+echo "File with longest CDS has been generated!"
 
 #Clean up
 rm "$tmpCDS"
