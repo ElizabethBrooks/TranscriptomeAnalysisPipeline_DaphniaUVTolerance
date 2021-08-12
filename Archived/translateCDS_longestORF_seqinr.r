2852419d873e70ad28322642fe6b0fc7d8198460
Elizabeth Brooks <bamflappy@Elizabeths-MacBook-Air.local>
2021-08-12 00:11:20 -0400

    Testing updates to script to generate ka ks values

--- Archived/translateCDS_longestORF_seqinr.r
+++ Archived/translateCDS_longestORF_seqinr.r
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 #R script to translate cds to protein sequences
-#Usage: Rscript translateCDS_longestForwardORF_seqinr.r cdsPath
+#Usage: Rscript translateCDS_longestORF_seqinr.r cdsPath
 
 #if (!requireNamespace("BiocManager", quietly=TRUE))
 #   install.packages("BiocManager")
@@ -18,9 +18,11 @@ realcds <- read.fasta(file=args[1])[[1]]
 
 #Find longest ORF
 curORF=0
-lenfORF=0
 longORF=0
+lenfORF=0
+lenrORF=0
 longf=0
+longs="F"
 for (i in 0:2) {
 	#Translate to proteins for the current forward ORF
 	realpep <- translate(seq=realcds, frame=i, sens="F")
@@ -39,13 +41,35 @@ for (i in 0:2) {
 			curORF=curORF+1
 		}
 	}
+	#Translate to proteins for the current reverse ORF
+	realpep <- translate(seq=realcds, frame=i, sens="R")
+	#Check length of pep sequence
+	len=length(realpep)
+	#Check occurence of stop codons
+	numStops=str_count(realpep, "\\*")
+	#Loop over each pep in current ORF
+	for (j in len:1) {
+		if(numStops[j] == 1){ #Stop codon
+			if(curORF > lenrORF){ #Longest current ORF
+				lenrORF=curORF
+			}
+			curORF=0
+		}else{
+			curORF=curORF+1
+		}
+	}
 	#Keep longest ORF translation
-	if(lenfORF > longORF){
+	if(lenrORF > longORF){
+		longORF=lenrORF
+		longf=i
+		longs="R"
+	}else if(lenfORF > longORF){
 		longORF=lenfORF
 		longf=i
+		longs="F"
 	}
 }
 
 #Output longest ORF
-finalpep <- translate(seq=realcds, frame=longf, sens="F")
+finalpep <- translate(seq=realcds, frame=longf, sens=longs)
 cat(finalpep, sep="")
