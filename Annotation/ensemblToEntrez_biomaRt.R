1b0446af717866542f231c6f5fb1816bee9d31b1
Elizabeth Brooks <merimaat1@gmail.com>
2022-10-25 01:43:09 -0400

    Updated script to create gene to transcript map

--- Annotation/ensemblToEntrez_biomaRt.R
+++ Annotation/ensemblToEntrez_biomaRt.R
@@ -1,6 +1,7 @@
 
 # load libraries
 library("biomaRt")
+#listMarts()
 
 # specify the hsapiens mart
 ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
@@ -13,11 +14,16 @@ View(filters)
 attributes = listAttributes(ensembl)
 View(attributes)
 
+#Import normalized gene count data
+#inputTable <- read.csv(file=args[1], row.names="gene")[ ,args[2]:args[3]]
+inputTable <- read.csv(file="/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/DEGenotypes/glmQLF_normalizedCounts.csv", row.names="gene", header=TRUE)[ ,1:24]
+View(inputTable)
+
 # retrieve the Ensembl IDs
-ensIDs=c("XM_046580782.1")
+ensIDs=c("E9HGM4")
 
 # map the Ensembl to the EntrezGene IDs 
-getBM(attributes=c("ensembl_transcript_id", "entrezgene_id"), 
-      filters = "ensembl_transcript_id", 
+getBM(attributes=c("uniprot_gn_id", "entrezgene_id"), 
+      filters = "uniprot_gn_id", 
       values = ensIDs, 
       mart = ensembl)
---
+++ DataGeneration/generateGeneTransMap_KAP4_GenBank.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# script to create a gene to transcript map for the KAP4 genome using the GenBank 
+# transcript sequences file that have gene ID annotations
+# usage ex: bash generateGeneTransMap_KAP4_GenBank.sh
+
+# retrieve genome tag
+gTag=$(grep "genomeTag:" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeTag://g")
+
+# retrieve input path
+transcriptPath=$(grep "transcriptSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/transcriptSequences://g")
+
+# retrieve output map path
+mapPath=$(grep "geneTransMap:" ../InputData/outputPaths.txt | tr -d " " | sed "s/geneTransMap://g")
+
+# set file name
+mapPath=$mapPath"/"$gTag".gene_trans_map"
+
+# set outputs path
+path=$(dirname $mapPath)
+
+# retrieve transcript IDs
+cat $transcriptPath | grep ">" | cut -d " " -f 1 | cut -d ">" -f 2 > $path"/col1_cleaned.txt"
+
+# retrieve gene IDs
+cat $transcriptPath | grep ">" | cut -d "(" -f 2 | cut -d ")" -f 1 > $path"/col2_cleaned.txt"
+
+# create txt file with both sets of IDs as columns
+paste $path"/col1_cleaned.txt" $path"/col2_cleaned.txt" > $mapPath
+
+# clean up 
+rm $path"/col"*"_cleaned.txt"
