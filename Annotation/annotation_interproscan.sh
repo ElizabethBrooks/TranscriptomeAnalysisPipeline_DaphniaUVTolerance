7222dff122cc4501497b4e72a5333f20fe1ad260
Elizabeth Brooks <merimaat1@gmail.com>
2022-12-01 00:41:33 -0500

    Added interproscan annotation script

---
+++ Annotation/annotation_interproscan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N annotation_interproscan_jobOutput
+#$ -pe smp 24
+# script to perform annotation of protein sequences using interproscan
+# usage: qsub annotation_interproscan.sh
+# usage Ex: qsub annotation_interproscan.sh
+
+# required modules for ND CRC servers
+module load bio
+
+# retrieve protein sequences absolute path
+inputsPath=$(grep "proteinSequences:" ../InputData/inputPaths.txt | tr -d " " | sed "s/proteinSequences://g")
+# retrieve annotation outputs absolute path
+outputsPath=$(grep "annotation:" ../InputData/outputPaths.txt | tr -d " " | sed "s/annotation://g")
+outputsPath=$outputsPath"/annotation_interproscan"
+
+# retrieve software path
+softwarePath=$(grep "interproscan:" ../InputData/outputPaths.txt | tr -d " " | sed "s/interproscan://g")
+
+# move to software directory
+cd $softwarePath
+
+# run interpro scan on protein sequences
+# including GO terms and pathways
+./interproscan.sh -cpu 24 -goterms -pa -verbose -vtsv -i $inputsPath -b $outputsPath
