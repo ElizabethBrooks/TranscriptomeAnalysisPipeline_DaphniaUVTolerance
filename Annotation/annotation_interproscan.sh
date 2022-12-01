3e02a975d8d4e281436bacfa58f6d4355a0677f7
Elizabeth Brooks <merimaat1@gmail.com>
2022-12-01 02:41:05 -0500

    Adjusted number of cores for interproscan

--- Annotation/annotation_interproscan.sh
+++ Annotation/annotation_interproscan.sh
@@ -3,7 +3,7 @@
 #$ -m abe
 #$ -r n
 #$ -N annotation_interproscan_jobOutput
-#$ -pe smp 24
+#$ -pe smp 12
 # script to perform annotation of protein sequences using interproscan
 # usage: qsub annotation_interproscan.sh
 # usage Ex: qsub annotation_interproscan.sh
@@ -34,4 +34,4 @@ cd $softwarePath
 
 # run interpro scan on protein sequences
 # including GO terms and pathways
-./interproscan.sh -cpu 24 -goterms -pa -verbose -vtsv -i $inputsPath -b $outputsPath
+./interproscan.sh -cpu 12 -goterms -pa -verbose -vtsv -i $inputsPath -b $outputsPath
