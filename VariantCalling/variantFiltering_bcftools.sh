9b52f38300dcbcde5402a35dbc443ca1d27669a2
Elizabeth Brooks <merimaat1@gmail.com>
2023-01-25 22:18:41 -0500

    Updated output of variants counting for stats summary

--- VariantCalling/variantFiltering_bcftools.sh
+++ VariantCalling/variantFiltering_bcftools.sh
@@ -51,43 +51,41 @@ bcftools filter -i '%QUAL<1001' $inputsDir"/"$type"_calls.vcf.gz" | grep -v "#" 
 #Include sites with quality > 20 
 bcftools filter --threads 8 -i '%QUAL>20' $inputsDir"/"$type"_calls.vcf.gz" -Ob -o $outFolder"/"$type"_calls.flt-qual.bcf"
 echo "bcftools filter --threads 8 -i '%QUAL>20' "$inputsDir"/"$type"_calls.vcf.gz -Ob -o "$outFolder"/"$type"_calls.flt-qual.bcf" >> $inputOutFile
-echo "Sites with quality > 20: " >> $outputsFile
+echo "& including sites with quality > 20: " >> $outputsFile
 bcftools filter --threads 8 -i '%QUAL>20' $inputsDir"/"$type"_calls.vcf.gz" | grep -v "#" | wc -l >> $outputsFile
 
 #Include sites with average read depth > 10
 bcftools filter --threads 8 -i 'INFO/DP>10' $outFolder"/"$type"_calls.flt-qual.bcf" -Ob -o $outFolder"/"$type"_calls.flt-qualDP.bcf"
 echo "bcftools filter --threads 8 -i 'INFO/DP>10' "$outFolder"/"$type"_calls.flt-qual.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP.bcf" >> $inputOutFile
-echo "Sites with average read depth > 10: " >> $outputsFile
+echo "& including sites with average read depth > 10: " >> $outputsFile
 bcftools filter --threads 8 -i 'INFO/DP>10' $outFolder"/"$type"_calls.flt-qual.bcf" | grep -v "#" | wc -l >> $outputsFile
 
 #Exclude hetoerozygous sites
 bcftools filter --threads 8 -e 'GT="het"' $outFolder"/"$type"_calls.flt-qualDP.bcf" -Ob -o $outFolder"/"$type"_calls.flt-qualDP-homo.bcf"
 echo "bcftools filter --threads 8 -e 'GT=\"het\"' "$outFolder"/"$type"_calls.flt-qualDP.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf" >> $inputOutFile
-echo "Excluding hetoerozygous sites: " >> $outputsFile
+echo "& excluding hetoerozygous sites: " >> $outputsFile
 bcftools filter --threads 8 -e 'GT="het"' $outFolder"/"$type"_calls.flt-qualDP.bcf" | grep -v "#" | wc -l >> $outputsFile
 
 #Exclude sites homozygous for the reference
 bcftools filter --threads 8 -e 'GT="RR"' $outFolder"/"$type"_calls.flt-qualDP-homo.bcf" -Ob -o $outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf"
 echo "bcftools filter --threads 8 -e 'GT=\"RR\"' "$outFolder"/"$type"_calls.flt-qualDP-homo.bcf -Ob -o "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf" >> $inputOutFile
-echo "Excluding sites homozygous for the reference: " >> $outputsFile
+echo "& excluding sites homozygous for the reference: " >> $outputsFile
 bcftools filter --threads 8 -e 'GT="RR"' $outFolder"/"$type"_calls.flt-qualDP-homo.bcf" | grep -v "#" | wc -l >> $outputsFile
 
 #Filter adjacent indels within 2bp
 bcftools filter --threads 8 -G 2 $outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf" -Ob -o $outFolder"/"$type"_calls.flt-indels.bcf"
 echo "bcftools filter --threads 8 -G 2 "$outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf -Ob -o "$outFolder"/"$type"_calls.flt-indels.bcf" >> $inputOutFile
-echo "Excluding adjacent indels within 2bp: " >> $outputsFile
+echo "& excluding adjacent indels within 2bp: " >> $outputsFile
 bcftools filter --threads 8 -G 2 $outFolder"/"$type"_calls.flt-qualDP-homo-dif.bcf" | grep -v "#" | wc -l >> $outputsFile
 
 #Filter adjacent SNPs within 2bp
 bcftools filter --threads 8 -g 2 $outFolder"/"$type"_calls.flt-indels.bcf" -Ob -o $outFolder"/"$type"_calls.flt-SNPs.bcf"
 echo "bcftools filter --threads 8 -g 2 "$outFolder"/"$type"_calls.flt-indels.bcf -Ob -o "$outFolder"/"$type"_calls.flt-SNPs.bcf" >> $inputOutFile
-echo "Excluding adjacent SNPs within 2bp: " >> $outputsFile
+echo "& excluding adjacent SNPs within 2bp: " >> $outputsFile
 bcftools filter --threads 8 -g 2 $outFolder"/"$type"_calls.flt-indels.bcf" | grep -v "#" | wc -l >> $outputsFile
 
 #Turn on left alignment, normalize indels, and collapse multi allelic sites
 bcftools norm --threads 8 -m +any -f $genomeFile $outFolder"/"$type"_calls.flt-SNPs.bcf" -Ob -o $outFolder"/"$type"_calls.normCollapse.bcf"
 echo "bcftools norm --threads 8 -m +any -f "$genomeFile" "$outFolder"/"$type"_calls.flt-SNPs.bcf -Ob -o "$outFolder"/"$type"_calls.normCollapse.bcf" >> $inputOutFile
-
-#Check final number variants
-echo "Turn on left alignment, normalize indels, and collapse multi allelic sites: " >> $outputsFile
-bcftools filter -i '%QUAL<1001' $outFolder"/"$type"_calls.normCollapse.bcf" | grep -v "#" | wc -l >> $outputsFile
+echo "& with left alignment, normalized indels, and collapsed multi allelic sites: " >> $outputsFile
+bcftools norm --threads 8 -m +any -f $genomeFile $outFolder"/"$type"_calls.flt-SNPs.bcf" | grep -v "#" | wc -l >> $outputsFile
