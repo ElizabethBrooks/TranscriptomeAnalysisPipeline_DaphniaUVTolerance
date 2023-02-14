079aaa25ba9e89abb58eb21081122eb89b09afe9
Elizabeth Brooks <merimaat1@gmail.com>
2023-02-13 19:14:55 -0500

    Adjusted output stats for variant filtering

--- VariantCalling/variantFiltering_bcftools.sh
+++ VariantCalling/variantFiltering_bcftools.sh
@@ -45,10 +45,6 @@ bcftools --version > $inputOutFile
 
 #Check total variants
 echo "Total variants from reads: " > $outputsFile
-cat $inputsDir"/"$type"_calls.vcf.gz" | grep -v "#" | wc -l >> $outputsFile
-
-#Check sites with quality < 1001
-echo "& with quality < 1001: " > $outputsFile
 bcftools filter -i '%QUAL<1001' $inputsDir"/"$type"_calls.vcf.gz" | grep -v "#" | wc -l >> $outputsFile
 
 #Include sites with quality > 20 
