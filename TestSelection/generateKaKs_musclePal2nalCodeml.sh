44fa1f3d4290c2b5a17a1c1f0fb12d5bf580f5cc
Elizabeth Brooks <merimaat1@gmail.com>
2023-04-11 22:57:42 -0400

    Testing selction analysis script updates

--- TestSelection/generateKaKs_musclePal2nalCodeml.sh
+++ TestSelection/generateKaKs_musclePal2nalCodeml.sh
@@ -47,10 +47,10 @@ refPath=$(grep "genomeReference" $baseDir"/InputData/inputPaths.txt" | tr -d " "
 refTag=$(basename $refPath)
 
 # set reference pep path
-refPath=$inputsPath"/"$refTag"_longest_pep.fa"
+inRefNuc=$inputsPath"/"$refTag"_longest_pep.fa"
 
 # set consensus pep path
-inputsPath=$inputsPath"/"$type"_consensus_longest_pep.fa"
+inConNuc=$inputsPath"/"$type"_consensus_longest_pep.fa"
 
 # set results file path
 resultsFile=$outFolder"/kaksResults.csv"
@@ -60,11 +60,11 @@ echo "Generating MSA for protein sequences..."
 
 # prepare reference multiline pep fasta to retrieve seqs
 tmpRef=$outFolder"/Pulex_pep.tmp.fa"
-cat $refPath | sed 's/$/NEWLINE/g' | tr -d '\n' | sed 's/NEWLINE>/\n>/g' > $tmpRef
+cat $inRefNuc | sed 's/$/NEWLINE/g' | tr -d '\n' | sed 's/NEWLINE>/\n>/g' > $tmpRef
 
 # prepare input multiline pep fasta
-tmpSample=$outFolder"/OLYM_pep.tmp.fa"
-cat $inputsPath | sed 's/$/NEWLINE/g' | tr -d '\n' | sed 's/NEWLINE>/\n>/g' > $tmpSample
+tmpSample=$outFolder"/Olympics_pep.tmp.fa"
+cat $inConNuc | sed 's/$/NEWLINE/g' | tr -d '\n' | sed 's/NEWLINE>/\n>/g' > $tmpSample
 
 # save ka ks values to final results file
 resultsFile="$outFolder"/kaksResults.csv
@@ -73,35 +73,50 @@ echo "geneID  t  S  N  dNdS  dN  dS" > "$resultsFile"
 # loop over all genes in the reference
 while IFS= read -r line; do
 	# retrieve gene tag
-	gTag=$(echo $line | sed 's/NEWLINE/\n/g' | grep ">" | cut -d " " -f 1 | sed 's/>//g')
+	gTag=$(echo "$line" | sed 's/NEWLINE/\n/g' | grep ">" | cut -d " " -f 1 | sed 's/>//g')
 
 	# set tmp output pep file
 	gFile=$outFolder"/"$gTag"_Daphnia_pep.tmp.fa"
+	tmpRefNuc=$outFolder"/"$gTag"_tmpRefNuc_cds.fa"
+	tmpConNuc=$outFolder"/"$gTag"_tmpConNuc_cds.fa"
+	outAln=$outFolder"/"$gTag"_Daphnia_aligned_pep.tmp.fa"
 
-	# retrieve selected peptide sequences and convert back to multiline fasta format
+	# retrieve reference peptide sequences and convert back to multiline fasta format
 	grep "^>$gTag" $tmpRef | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>Pulex_$gTag/g" > $gFile
-	
+	grep "^>$gTag" $tmpRef | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>Pulex_$gTag/g" > $tmpRefNuc
+
+	# retrieve consensus peptide sequences and convert back to multiline fasta format
+	grep "^>$gTag" $tmpSample | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>Olympics_$gTag/g" >> $gFile
+	grep "^>$gTag" $tmpSample | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>Olympics_$gTag/g" > $tmpConNuc
+
 	# Status message
 	echo "Generating MSA for $gTag..."
 
-	# prepare multiline pep fasta to retrieve seqs
-	grep "^>$gTag" $tmpSample | sed 's/NEWLINE/\n/g' | sed "s/^>$gTag.*/>OLYM_$gTag/g" >> $gFile
-
-	# set file paths
-	outAln=$outFolder"/"$gTag"_Daphnia_aligned_pep.tmp.fa"
-	inAln=$outFolder"/"$gTag"_Daphnia_pep.fa"
-
 	# create MSA using muscle
 	# https://stackoverflow.com/questions/70769809/muscle-command-line-wrapper
-	muscle -in "$gFile" -out "$outAln"
+	muscle -in "$gFileCleaned" -out "$outAln"
 	#muscle -align "$gFile" -output "$outAln"
-
-	# replace X wildcard with stop codon *
-	sed 's/X/\*/g'  "$outAln" > "$inAln"
 
 	# clean up
 	rm $gFile
-	rm $outAln
+	rm $gFileCleaned
+
+	# status message
+	echo "Generating codon alignment for $gTag..."
+
+	# usage:  pal2nal.pl  pep.aln  nuc.fasta  [nuc.fasta...]  [options]
+	"$softwarePath"/pal2nal.pl "$outAln" "$tmpRefNuc" "$tmpConNuc" -output paml -nogap  >  $outFolder"/"$gTag".codon"
+	#pal2nal.pl "$outAln" "$tmpRefNuc" "$tmpConNuc" -output paml -nogap  >  $outFolder"/"$gTag".codon"
+
+	# clean up
+	rm $tmpRefNuc
+	rm $tmpConNuc
+
+	# status message
+	echo "Generating ka ks values for $gTag..."
+
+	# move to directory of inputs for codeml
+	cd $outFolder
 
 	# prepare tree file
 	echo "(>Pulex_$gTag, >Olympics_$gTag);" > $outFolder"/"$gTag".tree"
@@ -110,19 +125,6 @@ while IFS= read -r line; do
 	# seqtype = 1 for codon alignments
 	# runmode = -2 performs ML estimation of dS and dN in pairwise comparisons
 	cat $baseDir"/util/test.cnt" | sed "s/test\.codon/$gTag\.codon/g" | sed "s/test\.tree/$gTag\.tree/g" | sed "s/test\.codeml/$gTag\.codeml/g" > $outFolder"/"$gTag".cnt"
-
-	# status message
-	echo "Generating codon alignment for $gTag..."
-
-	# usage:  pal2nal.pl  pep.aln  nuc.fasta  [nuc.fasta...]  [options]
-	"$softwarePath"/pal2nal.pl "$inAln" "$tmpRefNuc" "$tmpConNuc" -output paml -nogap  >  $outFolder"/"$gTag".codon"
-	#pal2nal.pl "$inAln" "$tmpRefNuc" "$tmpConNuc" -output paml -nogap  >  $outFolder"/"$gTag".codon"
-
-	# move to directory of inputs for codeml
-	cd $outFolder
-
-	# status message
-	echo "Generating ka ks values for $gTag..."
 
 	# run codeml to retrieve ka ks values
 	# you can find the output of codeml in the .codeml file
