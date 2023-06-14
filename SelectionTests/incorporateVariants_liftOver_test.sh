f6eec0be3ee07e03cd92e5dabe5a963441902a57
Elizabeth Brooks <merimaat1@gmail.com>
2023-06-14 18:14:04 -0400

    Updated test script to incorportate variants

--- SelectionTests/incorporateVariants_liftOver_test.sh
+++ SelectionTests/incorporateVariants_liftOver_test.sh
@@ -11,7 +11,7 @@
 module load bio/2.0
 
 # set software directory
-softwarePath="afs/crc.nd.edu/user/e/ebrooks5/UTIL"
+softwarePath="/afs/crc.nd.edu/user/e/ebrooks5/UTIL"
 
 # set inputs absolute path
 inputsPath=$(grep "aligningGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligningGenome://g")
@@ -33,19 +33,22 @@ genomeFeatures=$(grep "genomeFeatures" ../InputData/inputPaths.txt | tr -d " " |
 outFolder=$inputsPath"/selectionTests_test"
 mkdir $outFolder
 
+# move to software directory
+cd $softwarePath
+
 # convert BCF to VCF
-inputBcf=$inputsPath"/variantsMerged/"$type"_calls.flt-norm.bcf"
-outputVcf=$outFolder"/"$type"_calls.flt-norm.vcf.gz"
-bcftools view $inputBcf -Oz -o $outputVcf
+#inputBcf=$inputsPath"/variantsMerged/"$type"_calls.flt-norm.bcf"
+#outputVcf=$outFolder"/"$type"_calls.flt-norm.vcf.gz"
+#bcftools view $inputBcf -Oz -o $outputVcf
 #bcftools view /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsMerged/filteredMapQ_calls.flt-norm.bcf -Oz -o /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz
 
 # index VCF
-bcftools index $outputVcf -t
+#bcftools index $outputVcf -t
 #bcftools index /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz -t
 
 # convert VCF to BED9+ with optional extra fields
 vcfBed=$outFolder"/"$type"_OLYM"
-./$softwarePath"/"vcfToBed $outputVcf $vcfBed
+./vcfToBed $outputVcf $vcfBed
 #./UTIL/vcfToBed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM
 
 # format bed file
@@ -57,25 +60,25 @@ vcfBed=$outFolder"/"$type"_OLYM"
 # retrieve chrom sizes
 #refTag=$(basename $genomeFile | sed 's/\.fna//g')
 #refSizes=$outFolder"/"$refTag".chrom.sizes"
-#./$softwarePath"/"faSize -detailed -tab $genomeFile > $refSizes
+#./faSize -detailed -tab $genomeFile > $refSizes
 #./UTIL/faSize -detailed -tab /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/GCF_021134715.1_ASM2113471v1_genomic.fna > /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes
 
 # convert bed format files to psl format
 #vcfPsl=$outFolder"/"$type"_OLYM.psl"
-#./$softwarePath"/"bedToPsl $refSizes $fmtBed $vcfPsl
+#./bedToPsl $refSizes $fmtBed $vcfPsl
 #./UTIL/bedToPsl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.fmt.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.psl
 
 # convert psl records to chain records
 #vcfChain=$outFolder"/"$type"_OLYM.chain"
-#./$softwarePath"/"pslToChain $vcfPsl $vcfChain
+#./pslToChain $vcfPsl $vcfChain
 #./UTIL/pslToChain /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.psl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.chain
 
 ## convert a GFF3 CIGAR file to a PSL file
-##./$softwarePath"/"gff3ToPsl [options] queryChromSizes targetChomSizes inGff3 out.psl
+##./gff3ToPsl [options] queryChromSizes targetChomSizes inGff3 out.psl
 ##./UTIL/gff3ToPsl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.psl
 
 ## tranform a psl format file to a bed format file
-##./$softwarePath"/"pslToBed [options] psl bed
+##./pslToBed [options] psl bed
 ##./UTIL/pslToBed
 
 # reformat chain file and correct indel sizes
@@ -116,5 +119,5 @@ vcfBed=$outFolder"/"$type"_OLYM"
 
 # move annotations from one assembly to another
 #noMap=$outFolder"/"$type"_OLYM.unMapped.txt"
-#./$softwarePath"/"liftOver -gff $genomeFeatures $fmtChain -bedPlus=9 $vcfBed $noMap
+#./liftOver -gff $genomeFeatures $fmtChain -bedPlus=9 $vcfBed $noMap
 #./UTIL/liftOver -gff /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.chain -bedPlus=9 /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.unmapped.bed
