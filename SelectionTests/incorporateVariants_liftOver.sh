f14d1c7b4dd4c9266a75e8dce0cd2792239deee2
Elizabeth Brooks <merimaat1@gmail.com>
2023-06-21 00:51:33 -0400

    Updated scripts to incorporate variants using liftover

--- SelectionTests/incorporateVariants_liftOver.sh
+++ SelectionTests/incorporateVariants_liftOver.sh
@@ -5,13 +5,13 @@
 #$ -N incorporateVariants_jobOutput
 
 # script to incorporate variants and update the gff
-# usage: qsub incorporateVariants_liftOver.sh
+# usage: qsub incorporateVariants_liftOver_test.sh
 
 # load necessary software
 module load bio/2.0
 
 # set software directory
-softwarePath="afs/crc.nd.edu/user/e/ebrooks5/UTIL"
+softwarePath="/afs/crc.nd.edu/user/e/ebrooks5/UTIL"
 
 # set inputs absolute path
 inputsPath=$(grep "aligningGenome:" ../InputData/outputPaths.txt | tr -d " " | sed "s/aligningGenome://g")
@@ -21,95 +21,68 @@ inputsPath=$inputsPath"/variantsCalled_samtoolsBcftools"
 type="filteredMapQ"
 
 # set inputs directory name
-inputsDir=$inputsPath"/variantsMerged_"$type
+inputsDir=$inputsPath"/variantsMerged"
 
 # retrieve genome features absolute path for alignment
 genomeFile=$(grep "genomeReference" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeReference://g")
+genomeTag=$(basename $genomeFile | sed 's/\.fna//g')
 
 # retrieve genome features absolute path for alignment
 genomeFeatures=$(grep "genomeFeatures" ../InputData/inputPaths.txt | tr -d " " | sed "s/genomeFeatures://g")
 
 # set output folder
 outFolder=$inputsPath"/selectionTests"
+mkdir $outFolder
+# check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $outFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
+
+# move to software directory
+cd $softwarePath
+
+# status message
+echo "Beginnning analysis..."
 
 # convert BCF to VCF
 inputBcf=$inputsPath"/variantsMerged/"$type"_calls.flt-norm.bcf"
 outputVcf=$outFolder"/"$type"_calls.flt-norm.vcf.gz"
 bcftools view $inputBcf -Oz -o $outputVcf
-#bcftools view /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsMerged/filteredMapQ_calls.flt-norm.bcf -Oz -o /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz
+#bcftools view /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsMerged/filteredMapQ_calls.flt-norm.bcf -Oz -o /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_calls.flt-norm.vcf.gz
 
 # index VCF
 bcftools index $outputVcf -t
-#bcftools index /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz -t
+#bcftools index /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_calls.flt-norm.vcf.gz -t
 
 # convert VCF to BED9+ with optional extra fields
-vcfBed=$outFolder"/"$type"_OLYM"
-./$softwarePath"/"vcfToBed $outputVcf $vcfBed
-#./UTIL/vcfToBed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_calls.flt-norm.vcf.gz /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM
+vcfBedPrefix=$outFolder"/"$type"_OLYM"
+./vcfToBed $outputVcf $vcfBedPrefix
+#./UTIL/vcfToBed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_calls.flt-norm.vcf.gz /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM
 
-# format bed file
-vcfBed=$outFolder"/"$type"_OLYM.bed"
-fmtBed=$outFolder"/"$type"_OLYM.fmt.bed"
-cat $vcfBed | -f 1-9 > $fmtBed
-#cat /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.bed | cut -f 1-9 > /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.fmt.bed
-
-# retrieve chrom sizes
-refTag=$(basename $genomeFile | sed 's/\.fna//g')
-refSizes=$outFolder"/"$refTag".chrom.sizes"
-./$softwarePath"/"faSize -detailed -tab $genomeFile > $refSizes
-#./UTIL/faSize -detailed -tab /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/GCF_021134715.1_ASM2113471v1_genomic.fna > /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes
-
-# convert bed format files to psl format
-vcfPsl=$outFolder"/"$type"_OLYM.psl"
-./$softwarePath"/"bedToPsl $refSizes $fmtBed $vcfPsl
-#./UTIL/bedToPsl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.fmt.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.psl
-
-# convert psl records to chain records
-vcfChain=$outFolder"/"$type"_OLYM.chain"
-./$softwarePath"/"pslToChain $vcfPsl $vcfChain
-#./UTIL/pslToChain /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.psl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.chain
-
-## convert a GFF3 CIGAR file to a PSL file
-##./$softwarePath"/"gff3ToPsl [options] queryChromSizes targetChomSizes inGff3 out.psl
-##./UTIL/gff3ToPsl /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/GCF_021134715.1_ASM2113471v1_genomic.psl
-
-## tranform a psl format file to a bed format file
-##./$softwarePath"/"pslToBed [options] psl bed
-##./UTIL/pslToBed
-
-# reformat chain file and correct indel sizes
-# map.chain has the old genome as the target and the new genome as the query
-tmpIndelChain=$outFolder"/"$type"_OLYM.flt-indel.chain"
-tmpSnpChain=$outFolder"/"$type"_OLYM.flt-snp.chain"
-fmtChain=$outFolder"/"$type"_OLYM.fmt.chain"
-cat $pslChain | grep "chain 0" > $tmpIndelChain
-cat $pslChain | grep "chain 1" > $tmpSnpChain
-
-# add snp chain info
-cat $tmpSnpChain > $fmtChain
-
-# chain score tName tSize tStrand tStart tEnd qName qSize qStrand qStart qEnd id
-# loop over each indel entry in the chain file
-while IFS= read -r line; do
-	# retrieve chain info
-	lineStart=$(echo $line | cut -d " " -f 1-5)
-	tEnd=$(echo $line | cut -d " " -f 7)
-	qName=$(echo $line | cut -d " " -f 8)
-	indelSize=$(echo $line | cut -d "/" -f 2 | cut -d " " -f 1 | wc -c)
-	indelSize=$(($indelSize-1))
-	qStrand=$(echo $line | cut -d " " -f 10)
-	id=$(echo $line | cut -d " " -f 13)
-	# update chain entries
-	tStart=$(($tEnd-1))
-	qSize=$indelSize
-	qStart=0
-	qEnd=$indelSize
-	# output updated chain line
-	echo "$lineStart $tStart $tEnd $qName $qSize $qStart $qEnd $id" >> $fmtChain
-	echo $qSize >> $fmtChain
-done < $tmpIndelChain
+## prepare gff file
+##genePredFile=$outFolder"/"$genomeTag".gp"
+##./ldHgGene -out=$genePredFile database table $genomeFeatures
+##./UTIL/ldHgGene -out=/scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/GCF_021134715.1.gp database table /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff
 
 # move annotations from one assembly to another
-noMap=$outFolder"/"$type"_OLYM.unMapped.txt"
-./$softwarePath"/"liftOver -gff $genomeFeatures $fmtChain -bedPlus=9 $vcfBed $noMap
-#./UTIL/liftOver -gff /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.chain -bedPlus=9 /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests/filteredMapQ_OLYM.unmapped.bed
+fmtChain=$inputsPath"/variantsConsensus/"$type"_consensus.chain"
+vcfBed=$vcfBedPrefix".bed"
+noMap=$outFolder"/"$type"_OLYM.unMapped.bed"
+./liftOver -gff $genomeFeatures $fmtChain -bedPlus=9 $vcfBed $noMap
+#./UTIL/liftOver -gff /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsConsensus/filteredMapQ_consensus.chain -bedPlus=9 /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM.unMapped.bed
+##./liftOver -genePred $genePredFile $fmtChain -bedPlus=9 $vcfBed $noMap
+##./UTIL/liftOver -genePred /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/GCF_021134715.1.gp /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsConsensus/filteredMapQ_consensus.chain -bedPlus=9 /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM.bed /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM.unMapped.bed
+
+# retrieve chrom sizes for checking liftOver output
+refSizes=$outFolder"/"$refTag".chrom.sizes"
+sampleGenome=$inputsPath"/variantsConsensus/"$type"_consensus.fa"
+sampleSizes=$outFolder"/"$type"_OLYM.chrom.sizes"
+./$softwarePath"/"faSize -detailed -tab $genomeFile > $refSizes
+./$softwarePath"/"faSize -detailed -tab $sampleGenome > $sampleSizes
+#./UTIL/faSize -detailed -tab /afs/crc.nd.edu/group/pfrenderlab/mendel/ebrooks/daphnia_pulex/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/GCF_021134715.1_ASM2113471v1_genomic.fna > /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/GCF_021134715.1_ASM2113471v1_genomic.chrom.sizes
+#./UTIL/faSize -detailed -tab /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/variantsConsensus/filteredMapQ_consensus.fa > /scratch365/ebrooks5/OLYM_dMelUV_analysis/KAP4_NCBI/variantsCalled_samtoolsBcftools/selectionTests_test/filteredMapQ_OLYM.chrom.sizes
+
+# status message
+echo "Analysis complete!"
+
