c3ea5085ec577612b04b5f51c0c4b60893a6efe2
Elizabeth Brooks <merimaat1@gmail.com>
2023-08-09 23:06:17 -0400

    Updated gene model plotting R script

--- GenomicVisualization/generateGroupedCoverage_filtered_fromGFF3.r
+++ GenomicVisualization/generateGroupedCoverage_filtered_fromGFF3.r
@@ -21,6 +21,9 @@ options(ucscChromosomeNames=FALSE)
 # set working directory
 setwd("/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/seqs")
 
+# set output path
+outPath = "/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/Bioinformatics/variantsCalled_samtoolsBcftools/geneModels"
+
 # Plotting Palettes
 # https://stackoverflow.com/questions/57153428/r-plot-color-combinations-that-are-colorblind-accessible
 # https://github.com/Nowosad/rcartocolor
@@ -29,12 +32,6 @@ plotColors <- carto_pal(12, "Safe")
 # forge the BSgenome data package
 #forgeBSgenomeDataPkg("/Users/bamflappy/Repos/TranscriptomeAnalysisPipeline_DaphniaUVTolerance/InputData/BSgenome.Dpulex.KAP4-seed", replace=TRUE)
 
-# set gene region name and coordinates
-# photolyase (LOC124188748) NC_060018.1:363588-367123
-geneLoc = "NC_060018.1"
-geneName = "LOC124188748"
-geneStart = 363588
-geneEnd = 367123
 
 # retrieve chromosome sequence
 #sTrack <- SequenceTrack(Dpulex)
@@ -44,25 +41,35 @@ sTrack <- SequenceTrack(Dpulex, chromosome = geneLoc)
 #plotTracks(sTrack, chromosome = geneLoc, from = geneStart, to = geneEnd)
 #plotTracks(sTrack, from = geneStart, to = geneEnd)
 
-#Retrieve input gff file path
-inputGFF = "/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/Bioinformatics/variantsCalled_samtoolsBcftools/geneModels/LOC124188748.gff"
+# set gene region name and coordinates
+# photolyase (LOC124188748) NC_060018.1:363588-367123
+#geneLoc = "NC_060018.1"
+#geneName = "LOC124188748"
+#geneStart = 363588
+#geneEnd = 367123
+# NFKB (LOC124190117) NC_060019.1:3336006-3343487
+#geneLoc = "NC_060019.1"
+#geneName = "LOC124190117"
+#geneStart = 3336006
+#geneEnd = 3343487
+# adrenodoxin (LOC124193197) NC_060020.1:3488210-3489206
+geneLoc = "NC_060020.1"
+geneName = "LOC124193197"
+geneStart = 3488210
+geneEnd = 3489206
+# mucin2 (LOC124202665) NC_060025.1:8795802-8799903
+#geneLoc = "NC_060025.1"
+#geneName = "LOC124202665"
+#geneStart = 8795802
+#geneEnd = 8799903
+
+## Important
+## geneName="XXXX"
+## cat /Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/ncbi_dataset/data/GCF_021134715.1/genomic.gff | grep -w "$geneName" > /Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/Bioinformatics/variantsCalled_samtoolsBcftools/geneModels/$geneName.gff
+#Retrieve input gene gff file path
+inputGFF = paste("/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/Bioinformatics/variantsCalled_samtoolsBcftools/geneModels/",geneName,".gff", sep = "")
 #Store gff3 file as TxDb object
 geneDB <- makeTxDbFromGFF(file=inputGFF, format="gff3", organism="Daphnia pulex")
-
-#Retrieve output path
-outPath = "/Users/bamflappy/PfrenderLab/OLYM_dMelUV/KAP4/NCBI/GCF_021134715.1/Bioinformatics/variantsCalled_samtoolsBcftools/geneModels"
-
-#Retrieve input bam files
-inputBamE05 = "/Volumes/My Passport/OLYM_dMelUV/alignments/E05/filteredMapQ_readGroups.bam"
-inputBamR2 = "/Volumes/My Passport/OLYM_dMelUV/alignments/R2/filteredMapQ_readGroups.bam"
-inputBamY05 = "/Volumes/My Passport/OLYM_dMelUV/alignments/Y05/filteredMapQ_readGroups.bam"
-inputBamY023 = "/Volumes/My Passport/OLYM_dMelUV/alignments/Y023/filteredMapQ_readGroups.bam"
-
-#Index input bam files, if they have not already been indexed
-#indexBam(inputBamE05)
-#indexBam(inputBamR2)
-#indexBam(inputBamY05)
-#indexBam(inputBamY023)
 
 #Create the gene region track for specified chromosome or scaffold
 geneTr <- GeneRegionTrack(geneDB, 
@@ -83,6 +90,18 @@ geneTr <- GeneRegionTrack(geneDB,
 #head(exon(geneTr))
 #head(symbol(geneTr))
 
+#Retrieve input bam files
+inputBamE05 = "/Volumes/My Passport/OLYM_dMelUV/alignments/E05/filteredMapQ_readGroups.bam"
+inputBamR2 = "/Volumes/My Passport/OLYM_dMelUV/alignments/R2/filteredMapQ_readGroups.bam"
+inputBamY05 = "/Volumes/My Passport/OLYM_dMelUV/alignments/Y05/filteredMapQ_readGroups.bam"
+inputBamY023 = "/Volumes/My Passport/OLYM_dMelUV/alignments/Y023/filteredMapQ_readGroups.bam"
+
+#Index input bam files, if they have not already been indexed
+#indexBam(inputBamE05)
+#indexBam(inputBamR2)
+#indexBam(inputBamY05)
+#indexBam(inputBamY023)
+
 #Create the alignment tracks
 #fill = "transparent"
 alTrE05 <- AlignmentsTrack(inputBamE05, isPaired = TRUE, name = "E05", type = "coverage", col = plotColors[5], fill = plotColors[11])
@@ -98,7 +117,7 @@ alTrY023 <- AlignmentsTrack(inputBamY023, isPaired = TRUE, name = "Y023", type =
 
 # plot all data tracks
 jpeg(file=paste(outPath,"/filteredCoverage_geneModel_",geneName,".jpeg", sep = ""), width=900, height=400)
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
+plotTracks(list(sTrack, geneTr, alTrE05, alTrR2, alTrY05, alTrY023), 
            #groups = c("E05", "R2", "Y05", "Y023"),
            from = geneStart,
            to = geneEnd, 
@@ -121,171 +140,18 @@ plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023),
 dev.off()
 
 # set coordinates for cds 1
-cdsStart = 364812
-cdsEnd = 365085
+#cdsStart = 364812
+#cdsEnd = 365085
 
 # plot cds 1
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 2
-cdsStart = 365154
-cdsEnd = 365279
-
-# plot cds 2
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 3
-cdsStart = 365353
-cdsEnd = 365555
-
-# plot cds 3
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 4
-cdsStart = 365632
-cdsEnd = 365686
-
-# plot cds 4
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 5
-cdsStart = 365756
-cdsEnd = 365859
-
-# plot cds 5
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 6
-cdsStart = 365930
-cdsEnd = 366128
-
-# plot cds 6
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 7
-cdsStart = 366201
-cdsEnd = 366353
-
-# plot cds 7
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 8
-cdsStart = 366418
-cdsEnd = 366457
-
-# plot cds 8
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 9
-cdsStart = 366520
-cdsEnd = 366642
-
-# plot cds 9
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
-
-# set coordinates for cds 10
-cdsStart = 366711
-cdsEnd = 367002
-
-# plot cds 10
-plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
-           from = cdsStart,
-           to = cdsEnd, 
-           chromosome = geneLoc,
-           exonAnnotation = "feature",
-           fontcolor.exon = 1,
-           fontsize.exon = 8,
-           thinBoxFeature = "UTR", 
-           collapse = FALSE,
-           alpha.title = 1,
-           showIndels=TRUE)
+#plotTracks(c(geneTr, sTrack, alTrE05, alTrR2, alTrY05, alTrY023), 
+           #from = cdsStart,
+           #to = cdsEnd, 
+           #chromosome = geneLoc,
+           #exonAnnotation = "feature",
+           #fontcolor.exon = 1,
+           #fontsize.exon = 8,
+           #thinBoxFeature = "UTR", 
+           #collapse = FALSE,
+           #alpha.title = 1,
+           #showIndels=TRUE)
